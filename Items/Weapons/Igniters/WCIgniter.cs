4f5de8a9bf473919f24ac7c7b99bf6930f5a9e13
Zenoviaa <calamityjewelstudios@gmail.com>
2022-06-13 07:50:15 -0400

    worldgenthings

--- Items/Weapons/Igniters/WCIgniter.cs
+++ Items/Weapons/Igniters/WCIgniter.cs
@@ -20,7 +20,7 @@ namespace Stellamod.Items.Weapons.Igniters
 		}
 		public override void SetDefaults()
 		{
-			Item.damage = 5;
+			Item.damage = 3;
 			Item.mana = 3;
 			Item.width = 40;
 			Item.height = 40;
---
+++ WoldGenThingy.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Input;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.WorldBuilding;
+using Terraria.GameContent.Generation;
+using Stellamod.Tiles;
+
+namespace Stellamod
+{
+	class WorldGenThingy : ModSystem
+	{
+		public static bool JustPressed(Keys key)
+		{
+			return Main.keyState.IsKeyDown(key) && !Main.oldKeyState.IsKeyDown(key);
+		}
+
+		public override void PostUpdateWorld()
+		{
+			if (JustPressed(Keys.D1))
+				TestMethod((int)Main.MouseWorld.X / 16, (int)Main.MouseWorld.Y / 16);
+		}
+
+		private void TestMethod(int x, int y)
+		{
+			Dust.QuickBox(new Vector2(x, y) * 16, new Vector2(x + 1, y + 1) * 16, 2, Color.YellowGreen, null);
+			//WorldGen.digTunnel(x, y, 1, 0.01f, 100, 4, false);
+			//WorldGen.TileRunner(x, y, WorldGen.genRand.Next(600, 700), WorldGen.genRand.Next(500, 600), ModContent.TileType<OvermorrowdirtTile>());
+			// Code to test placed here:
+			WorldGen.TileRunner(x, y, WorldGen.genRand.Next(600, 700), WorldGen.genRand.Next(500, 600), ModContent.TileType<OvermorrowdirtTile>());
+		}
+	}
+}
