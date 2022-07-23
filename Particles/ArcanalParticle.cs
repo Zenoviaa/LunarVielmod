48e334efacf766777828ea1f9237cc2fb92dff09
Zenoviaa <calamityjewelstudios@gmail.com>
2022-07-22 23:59:45 -0400

    thing

--- Particles/ArcanalParticle.cs
+++ Particles/ArcanalParticle.cs
@@ -34,7 +34,7 @@ namespace Stellamod.Particles
 		}
 		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color lightColor)
 		{
-			Texture2D tex = Request<Texture2D>("Stellamod/Particles/ArcanalParticle").Value;
+			Texture2D tex = Request<Texture2D>("Stellamod/Particles/magiccirlce").Value;
 			float alpha = timeLeft <= 20 ? 1f - 1f / 20f * (20 - timeLeft) : 1f;
 
 			if (alpha < 0f)
