93b47730d60e65f9f5c07b116ccff8c26ef87ce8
Zenoviaa <calamityjewelstudios@gmail.com>
2023-04-30 11:58:51 -0400

    sadness incarnated

---
+++ Items/Accessories/IllusionistBook.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Stellamod;
+using Stellamod.Items.Harvesting;
+using Stellamod.Items.Ores;
+
+namespace Stellamod.Items.Accessories
+{
+	public class IllusionistBook : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Book of Wooden Illusion");
+			Tooltip.SetDefault("Increased Regeneration!" +
+				"\n +3% damage" +
+				"\n Increases crit strike change by 5% ");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 24;
+			Item.height = 28;
+			Item.value = Item.buyPrice(10);
+			Item.rare = ItemRarityID.Green;
+			Item.accessory = true;
+
+
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.Wood, 50);
+			recipe.AddIngredient(ItemID.Cobweb, 30);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.GetDamage(DamageClass.Generic) *= 1.03f; // Increase ALL player damage by 100%
+			player.lifeRegen += 1;
+			player.GetCritChance(DamageClass.Generic) *= 1.05f;
+
+
+		}
+
+
+
+
+	}
+}
