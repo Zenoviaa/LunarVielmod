ba5ee9e8a93594287496a55e6b72e21a1c69dbef
Zenoviaa <calamityjewelstudios@gmail.com>
2023-05-04 23:19:29 -0400

    Frost Powder add

--- Items/Weapons/PowdersItem/FrostedPowder.cs
+++ Items/Weapons/PowdersItem/FrostedPowder.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Stellamod.Buffs;
+using Stellamod.Items.Harvesting;
 using Stellamod.Items.Materials;
+using Stellamod.Items.Ores;
 using Stellamod.Projectiles;
 using Stellamod.Projectiles.Powders;
+using Stellamod.Tiles;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
@@ -40,6 +43,23 @@ namespace Stellamod.Items.Weapons.PowdersItem
 			Item.UseSound = new SoundStyle("Stellamod/Assets/Sounds/iceshake");
 		}
 
-	
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<FrileBar>(), 12);
+			recipe.AddIngredient(ItemID.Silk, 5);
+			recipe.AddIngredient(ItemID.IceBlock, 50);
+			recipe.AddIngredient(ItemID.FallenStar, 10);
+			recipe.AddIngredient(ModContent.ItemType<Bagitem>(), 1);
+			recipe.AddIngredient(ModContent.ItemType<MorrowVine>(), 5);
+			recipe.AddIngredient(ModContent.ItemType<Fabric>(), 15);
+			recipe.AddTile(ModContent.TileType<AlcaologyTable>());
+			recipe.Register();
+			
+
+
+			recipe.Register();
+		}
 	}
 }
