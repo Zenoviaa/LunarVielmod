0143d069e6e37a02339cfb3c5fc180e1fa89acd4
Zenoviaa <calamityjewelstudios@gmail.com>
2023-05-04 21:28:07 -0400

    Lovestruck

---
+++ Items/Weapons/Igniters/LovestruckCard.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Buffs;
+using Stellamod.Items.Harvesting;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Ores;
+using Stellamod.Projectiles;
+using Stellamod.Projectiles.IgniterEx;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Weapons.Igniters
+{
+	internal class LovestruckCard : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Lovestruck Card Igniter");
+			Tooltip.SetDefault("Use with a combination of dusts to make spells :)" +
+				"\n Use a powder or dust and then use this type of weapon!");
+		}
+		public override void SetDefaults()
+		{
+			Item.damage = 3;
+			Item.mana = 3;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 50;
+			Item.useAnimation = 50;
+			Item.useStyle = ItemUseStyleID.Guitar;
+			Item.noMelee = true;
+			Item.knockBack = 0f;
+			Item.DamageType = DamageClass.Magic;
+			Item.value = 200;
+			Item.rare = ItemRarityID.Pink;
+			Item.UseSound = new SoundStyle("Stellamod/Assets/Sounds/clickk");
+			Item.autoReuse = true;
+			Item.shoot = ModContent.ProjectileType<IgniterStart>();
+			Item.autoReuse = true;
+			Item.crit = 50;
+			Item.shootSpeed = 20;
+		}
+		
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+
+			for (int i = 0; i < Main.npc.Length; i++)
+			{
+				NPC npc = Main.npc[i];
+				if (npc.active && npc.HasBuff<Dusted>())
+				{
+					Projectile.NewProjectile(npc.GetSource_FromThis(), npc.position, velocity, type, damage, knockback);
+					
+				}
+				
+				
+			}
+			return base.Shoot(player, source, position, velocity, type, damage, knockback);
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<FrileBar>(), 5);
+			recipe.AddIngredient(ItemID.FallenStar, 3);
+			recipe.AddIngredient(ItemID.LifeCrystal, 1);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+	}
+}
