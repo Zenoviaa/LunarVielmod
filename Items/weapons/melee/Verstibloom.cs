2c5be2ee746075d567a2aa619e9b38336473d98a
Zenoviaa <calamityjewelstudios@gmail.com>
2023-05-04 21:10:31 -0400

    buffs and nerfs

--- Items/weapons/melee/Verstibloom.cs
+++ Items/weapons/melee/Verstibloom.cs
@@ -22,7 +22,7 @@ namespace Stellamod.Items.Weapons.Melee
 		}
 		public override void SetDefaults()
 		{
-			Item.damage = 46;
+			Item.damage = 66;
 			Item.DamageType = DamageClass.Melee;
 			Item.width = 32;
 			Item.mana = 5;
