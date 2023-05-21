dbbb08b06f54fcfd2f9d90e9e414efb21ec180ab
Zenoviaa <calamityjewelstudios@gmail.com>
2023-05-20 23:40:17 -0400

    bore

---
+++ Tiles/Structures/AlcadizNGovheil/GovheilBore.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Items.Placeable;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+
+
+namespace Stellamod.Tiles.Structures.AlcadizNGovheil
+
+{
+	public class GovheilBore : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			// Properties
+			Main.tileTable[Type] = true;
+			Main.tileSolidTop[Type] = false;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = false;
+			Main.tileFrameImportant[Type] = true;
+			TileID.Sets.DisableSmartCursor[Type] = true;
+			TileID.Sets.IgnoredByNpcStepUp[Type] = true; // This line makes NPCs not try to step up this tile during their movement. Only use this for furniture with solid tops.
+			MineResist = 4f;
+			MinPick = 200;
+
+			DustType = DustID.SilverCoin;
+			AdjTiles = new int[] { TileID.Bookcases };
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+			TileObjectData.newTile.Height = 12;
+			TileObjectData.newTile.Width = 7;
+
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16};
+			TileObjectData.newTile.StyleWrapLimit = 2; //not really necessary but allows me to add more subtypes of chairs below the example chair texture
+			TileObjectData.newTile.StyleMultiplier = 2; //same as above
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.addTile(Type);
+			LocalizedText name = CreateMapEntryName();
+
+			// name.SetDefault("Hunter's Curtains");
+			AddMapEntry(new Color(8, 25, 35), name);
+		}
+		public override void NumDust(int x, int y, bool fail, ref int num)
+		{
+			num = fail ? 1 : 3;
+		}
+
+	}
+}
