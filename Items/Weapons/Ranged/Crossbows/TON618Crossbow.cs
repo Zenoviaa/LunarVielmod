d2da5d26de129e68b670d7b4c738457f6c1bada7
Zenovia <calamityjewelstudios@gmail.com>
2023-08-05 10:00:22 -0400

    Raodm localization

---
+++ Items/Weapons/Ranged/Crossbows/TON618Crossbow.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.Audio;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Stellamod.Projectiles.Crossbows;
+using Stellamod.Projectiles.Crossbows.Gemmed;
+using Stellamod.Projectiles.Crossbows.Magical;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Ores;
+
+namespace Stellamod.Items.Weapons.Ranged.Crossbows
+{
+
+    public class TON618Crossbow : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Wooden Crossbow"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            /* Tooltip.SetDefault("Use a small crossbow and shoot three bolts!"
+                + "\n'Triple Threat!'"); */
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 33;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 32;
+            Item.height = 25;
+            Item.useTime = 45;
+            Item.useAnimation = 45;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 2;
+            Item.rare = ItemRarityID.Blue;
+            Item.autoReuse = false;
+            Item.shootSpeed = 30f;
+            Item.shoot = ModContent.ProjectileType<TON618CrossbowHold>();
+            Item.scale = 0.8f;
+            Item.noMelee = true; // The projectile will do the damage and not the item
+            Item.value = Item.buyPrice(silver: 3);
+            Item.noUseGraphic = true;
+            Item.channel = true;
+       
+
+        }
+
+
+
+        
+
+
+    }
+}
