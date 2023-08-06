47591eb78cc3ebfc3ba8bab93db555e8526c2eac
Zenovia <calamityjewelstudios@gmail.com>
2023-08-06 01:15:05 -0400

    bean

--- Items/Weapons/Ranged/Crossbows/TON618Crossbow.cs
+++ Items/Weapons/Ranged/Crossbows/TON618Crossbow.cs
@@ -29,7 +29,7 @@ namespace Stellamod.Items.Weapons.Ranged.Crossbows
 
         public override void SetDefaults()
         {
-            Item.damage = 31;
+            Item.damage = 30;
             Item.DamageType = DamageClass.Ranged;
             Item.width = 32;
             Item.height = 25;
