c9e904ee51cc8b8e29ee3cb1367fa6ab73cb0643
Zenovia <calamityjewelstudios@gmail.com>
2023-08-09 09:24:48 -0400

    there you go oh hell nah

--- Items/Weapons/Ranged/HeatGlider.cs
+++ Items/Weapons/Ranged/HeatGlider.cs
@@ -21,7 +21,7 @@ namespace Stellamod.Items.Weapons.Ranged
         public int WinterboundArrow;
         public override void SetDefaults()
         {
-            Item.damage = 7;
+            Item.damage = 25;
             Item.width = 50;
             Item.height = 50;
             Item.useStyle = ItemUseStyleID.Shoot;
