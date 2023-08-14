b5840d2e30433867d5615673cd194821d3a740e3
Zenovia <calamityjewelstudios@gmail.com>
2023-08-13 20:18:55 -0400

    waa waa

--- Projectiles/IgniterEx/IgniterStart.cs
+++ Projectiles/IgniterEx/IgniterStart.cs
@@ -178,8 +178,8 @@ namespace Stellamod.Projectiles.IgniterEx
 					SoundEngine.PlaySound(new SoundStyle($"Stellamod/Assets/Sounds/GhostExcalibur1"));
 					Projectile.scale = 1.5f;
 					ShakeModSystem.Shake = 5;
-					npc.SimpleStrikeNPC(Projectile.damage * 7, 1, crit: false, Projectile.knockBack);
-					npc.SimpleStrikeNPC(Projectile.damage * 7, 1, crit: false, Projectile.knockBack);
+					npc.SimpleStrikeNPC(Projectile.damage * 10, 1, crit: false, Projectile.knockBack);
+					npc.SimpleStrikeNPC(Projectile.damage * 10, 1, crit: false, Projectile.knockBack);
 					float speedXa = -Projectile.velocity.X * Main.rand.NextFloat(.4f, .7f) + Main.rand.NextFloat(-8f, 8f);
 					float speedYa = -Projectile.velocity.Y * Main.rand.Next(0, 0) * 0.01f + Main.rand.Next(-20, 21) * 0.0f;
 					Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.position.X + speedXa, Projectile.position.Y + speedYa, speedXa * 0, speedYa * 0, ModContent.ProjectileType<CrystalBloom>(), (int)(Projectile.damage * 1.5), 0f, Projectile.owner, 0f, 0f);
