ad951dbfe30f692a6fe5a777448ce1fea807d80f
Zenovia <calamityjewelstudios@gmail.com>
2023-11-11 22:06:30 -0500

    hreen

--- Items/Weapons/Mage/Poya.cs
+++ Items/Weapons/Mage/Poya.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
+using Stellamod.Items.Harvesting;
+using Stellamod.Items.Materials;
 using Stellamod.Projectiles.Magic;
 using Stellamod.Projectiles.Spears;
 using Terraria;
@@ -41,6 +43,18 @@ namespace Stellamod.Items.Weapons.Mage
         public override Vector2? HoldoutOffset()
         {
             return new Vector2(-3f, -2f);
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.AddIngredient(ModContent.ItemType<AlcaricMush>(), 23);
+            recipe.AddIngredient(ModContent.ItemType<ConvulgingMater>(), 30);
+            recipe.AddIngredient(ModContent.ItemType<DarkEssence>(), 9);
+            recipe.AddIngredient(ModContent.ItemType<AlcadizMetal>(), 9);
+            recipe.AddIngredient(ModContent.ItemType<WickofSorcery>(), 1);
+            recipe.Register();
         }
     }
 }
