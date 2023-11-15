98460a0c65ada09f9ae2adc81be2081eb7bdf260
AzaleaThePhantomWitch <atych44@gmail.com>
2023-11-14 21:58:58 -0600

    wddw

---
+++ Items/Armors/Astrasilk - Copy/LunarianVoidBody.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Stellamod.Items.Materials;
+
+namespace Stellamod.Items.Armors.LunarianVoid
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class LunarianVoidBody : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Astrasilk Jacket");
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 30;
+            Item.value = Item.sellPrice(0, 0, 20, 0);
+            Item.rare = ItemRarityID.Blue;
+            Item.defense = 4;
+            Item.vanity = true;
+        }
+        public override void UpdateEquip(Player player)
+        {
+            player.GetCritChance(DamageClass.Magic) += 0.25f;
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<SpacialDistortionFragments>(), 14);
+            recipe.AddIngredient(ModContent.ItemType<AuroreanStarI>(), 3);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+
+    }
+}
