079dba2e862a2aac5ea87984786356fb50cec3c8
Zenovia <calamityjewelstudios@gmail.com>
2023-11-20 23:11:22 -0500

    green

---
+++ Items/Materials/ChiseledStoneBlock.cs
@@ -0,0 +1,56 @@
+﻿
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Materials
+{
+    public class ChiseledStoneBlock : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// Tooltip.SetDefault("Super silk!");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+			ItemID.Sets.ExtractinatorMode[Item.type] = Item.type;
+
+			// Some please convert this to lang files, I'm too lazy to do it
+			// Sorry Itorius, I feel you
+
+			// DisplayName.AddTranslation(GameCulture.German, "Beispielblock");
+			// Tooltip.AddTranslation(GameCulture.German, "Dies ist ein modded Block");
+			// DisplayName.AddTranslation(GameCulture.Italian, "Blocco di esempio");
+			// Tooltip.AddTranslation(GameCulture.Italian, "Questo è un blocco moddato");
+			// DisplayName.AddTranslation(GameCulture.French, "Bloc d'exemple");
+			// Tooltip.AddTranslation(GameCulture.French, "C'est un bloc modgé");
+			// DisplayName.AddTranslation(GameCulture.Spanish, "Bloque de ejemplo");
+			// Tooltip.AddTranslation(GameCulture.Spanish, "Este es un bloque modded");
+			// DisplayName.AddTranslation(GameCulture.Russian, "Блок примера");
+			// Tooltip.AddTranslation(GameCulture.Russian, "Это модифицированный блок");
+			// DisplayName.AddTranslation(GameCulture.Chinese, "例子块");
+			// Tooltip.AddTranslation(GameCulture.Chinese, "这是一个修改块");
+			// DisplayName.AddTranslation(GameCulture.Portuguese, "Bloco de exemplo");
+			// Tooltip.AddTranslation(GameCulture.Portuguese, "Este é um bloco modded");
+			// DisplayName.AddTranslation(GameCulture.Polish, "Przykładowy blok");
+			// Tooltip.AddTranslation(GameCulture.Polish, "Jest to modded blok");
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 12;
+			Item.height = 12;
+			Item.maxStack = 9999;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 10;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<Tiles.ChiseledStone>();
+		}
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes()
+		{
+
+		}
+	}
+}
