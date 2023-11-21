ad786672cd1e8b07cd0bb1888967ea605d722db8
AzaleaThePhantomWitch <atych44@gmail.com>
2023-11-20 21:38:09 -0600

    fbf

---
+++ Projectiles/Bow/LarveinGreatbowL.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Buffs;
+using Stellamod.Projectiles.Bow;
+using Stellamod.Trails;
+using Stellamod.UI.Systems;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Stellamod.Projectiles.Bow
+{
+    internal class LarveinGreatbowL : ModProjectile
+    {
+        bool Moved;
+
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Shadow Hand");
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 15;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 1;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.penetrate = 10;
+            Projectile.width = 39;
+            Projectile.height = 39;
+            Projectile.timeLeft = 60;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+        }
+        private float Red;
+        private float alphaCounter = 1;
+        public override void AI()
+        {
+            Projectile.velocity *= .96f;
+            Projectile.ai[1]++;
+            if (!Moved && Projectile.ai[1] >= 0)
+            {
+                float A = Main.rand.Next(0, 2);
+                if (A == 0)
+                {
+                    Red = 15;
+                }
+                else
+                {
+                    Red = 65;
+                }
+
+                Projectile.rotation = Projectile.velocity.ToRotation() + 1.57f + 3.14f;
+                Moved = true;
+            }
+            if (Projectile.ai[1] >= 20)
+            {
+                Projectile.tileCollide = true;
+            }
+            if (Projectile.alpha <= 255)
+            {
+                alphaCounter -= 0.02f;
+                Projectile.alpha += 7;
+            }
+            if (Projectile.alpha >= 255)
+            {
+
+            }
+
+
+            Projectile.rotation = Projectile.velocity.ToRotation() + 1.57f + 3.14f;
+        }
+        public override void OnKill(int timeLeft)
+        {
+
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture2D4 = Request<Texture2D>("Stellamod/Effects/Masks/DimLight").Value;
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(Red * alphaCounter), (int)(15f * alphaCounter), (int)(85f * alphaCounter), 0), Projectile.rotation, new Vector2(32, 32), 0.17f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(Red * alphaCounter), (int)(15f * alphaCounter), (int)(85f * alphaCounter), 0), Projectile.rotation, new Vector2(32, 32), 0.17f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(Red * alphaCounter), (int)(15f * alphaCounter), (int)(85f * alphaCounter), 0), Projectile.rotation, new Vector2(32, 32), 0.17f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(Red * alphaCounter), (int)(15f * alphaCounter), (int)(85f * alphaCounter), 0), Projectile.rotation, new Vector2(32, 32), 0.07f * (7 + 0.6f), SpriteEffects.None, 0f);
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+            Main.instance.LoadProjectile(Projectile.type);
+            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+
+            // Redraw the projectile with the color not influenced by light
+            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            for (int k = 0; k < Projectile.oldPos.Length; k++)
+            {
+                Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Color color = Projectile.GetAlpha(Color.Lerp(new Color(221, 172, 233), new Color(126, 96, 148), 1f / Projectile.oldPos.Length * k) * (1f - 1f / Projectile.oldPos.Length * k));
+                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+            }
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+
+            return true;
+        }
+        public override void PostDraw(Color lightColor)
+        {
+            Lighting.AddLight(Projectile.Center, Color.Pink.ToVector3() * 1.75f * Main.essScale);
+
+        }
+    }
+
+}
+
+
