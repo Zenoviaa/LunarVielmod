9db4e1c0886c8a0aabc21f3f474b0d2ab1a12ac5
Scolon <49799704+sscolon@users.noreply.github.com>
2023-12-01 21:25:16 -0500

    bubble dust

---
+++ Dusts/BubbleDust.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Dusts
+{
+	public class BubbleDust : ModDust
+	{
+		public override void OnSpawn(Dust dust)
+		{
+			dust.scale = 1f;
+			dust.alpha = 0;
+			dust.velocity *= 0.1f;
+			dust.frame = new Rectangle(0, 0, 14, 14);
+		}
+
+		public override bool Update(Dust dust)
+		{
+			Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f), 0.2f, 0.2f, 0.2f);
+			dust.noGravity = true;
+			dust.position += dust.velocity;
+			dust.velocity *= 0.92f;
+			dust.scale *= 0.98f;
+			dust.alpha += 12;
+			if (dust.scale < 0.5f)
+			{
+				dust.active = false;
+			}
+			return false;
+		}
+	}
+}
