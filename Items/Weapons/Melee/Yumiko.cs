5b99a887f9516977ea3c63901c384dd6f3f8cf1a
Zenovia <calamityjewelstudios@gmail.com>
2023-12-04 12:58:29 -0500

    buff before i go to studio

--- Items/Weapons/Melee/Yumiko.cs
+++ Items/Weapons/Melee/Yumiko.cs
@@ -58,7 +58,7 @@ namespace Stellamod.Items.Weapons.Melee
 		}
 		public override void SetDefaults()
 		{
-			Item.damage = 200;
+			Item.damage = 250;
 			Item.DamageType = DamageClass.Melee;
 			Item.width = 32;
 			Item.height = 32;
