cc428b6813a40a4ba3f0f924e0ad101ffaa70156
Zenovia <calamityjewelstudios@gmail.com>
2023-12-04 10:14:47 -0500

    just sprites

---
+++ Items/Weapons/Melee/Yumiko.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Items.Harvesting;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Ores;
+using Stellamod.Projectiles;
+using Stellamod.Projectiles.Slashers.Voyager;
+using Stellamod.Projectiles.Swords.Altride;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace Stellamod.Items.Weapons.Melee
+{
+	public class Yumiko : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+
+
+			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(1, 60));
+			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
+			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+			// DisplayName.SetDefault("Frost Swing");
+			/* Tooltip.SetDefault("Shoots one bone bolt to swirl and kill your enemies after attacking!" +
+			"\nHitting foes with the melee swing builds damage towards the swing of the weapon"); */
+		}
+		public override void SetDefaults()
+		{
+			Item.damage = 200;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 32;
+			Item.height = 32;
+			Item.useTime = 10;
+			Item.useAnimation = 100;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 15;
+			Item.rare = ItemRarityID.Blue;
+			Item.autoReuse = true;
+			Item.value = 100000;
+			Item.shoot = ModContent.ProjectileType<EverneanProj>();
+			Item.shootSpeed = 10f;
+			Item.noUseGraphic = true;
+			Item.noMelee = true;
+
+
+		}
+
+
+
+		public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+		{
+			// Draw the periodic glow effect behind the item when dropped in the world (hence PreDrawInWorld)
+			Texture2D texture = TextureAssets.Item[Item.type].Value;
+
+			Rectangle frame;
+
+			if (Main.itemAnimations[Item.type] != null)
+			{
+				// In case this item is animated, this picks the correct frame
+				frame = Main.itemAnimations[Item.type].GetFrame(texture, Main.itemFrameCounter[whoAmI]);
+			}
+			else
+			{
+				frame = texture.Frame();
+			}
+
+			Vector2 frameOrigin = frame.Size() / 2f;
+			Vector2 offset = new Vector2(Item.width / 2 - frameOrigin.X, Item.height - frame.Height);
+			Vector2 drawPos = Item.position - Main.screenPosition + frameOrigin + offset;
+
+			float time = Main.GlobalTimeWrappedHourly;
+			float timer = Item.timeSinceItemSpawned / 240f + time * 0.04f;
+
+			time %= 4f;
+			time /= 2f;
+
+			if (time >= 1f)
+			{
+				time = 2f - time;
+			}
+
+			time = time * 0.5f + 0.5f;
+
+			for (float i = 0f; i < 1f; i += 0.25f)
+			{
+				float radians = (i + timer) * MathHelper.TwoPi;
+
+				spriteBatch.Draw(texture, drawPos + new Vector2(0f, 8f).RotatedBy(radians) * time, frame, new Color(200, 70, 255, 77), rotation, frameOrigin, scale, SpriteEffects.None, 0);
+			}
+
+			for (float i = 0f; i < 1f; i += 0.34f)
+			{
+				float radians = (i + timer) * MathHelper.TwoPi;
+
+				spriteBatch.Draw(texture, drawPos + new Vector2(0f, 4f).RotatedBy(radians) * time, frame, new Color(255, 255, 255, 67), rotation, frameOrigin, scale, SpriteEffects.None, 0);
+			}
+
+			return true;
+		}
+	}
+}
