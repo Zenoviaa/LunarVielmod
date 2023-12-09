71a0dcaaeeec2e19b99c1ef630b81487bc3b63f4
Zenovia <calamityjewelstudios@gmail.com>
2023-12-08 23:36:33 -0500

    news

---
+++ Items/Consumables/CatacombsKey.cs
@@ -0,0 +1,43 @@
+﻿
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Consumables
+{
+    public class CatacombsKey : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Electronic Death Remote (EDR)");
+            // Tooltip.SetDefault("'that big red button probably will do something you’ll regret... \n Your conscience advises you to press it and see what happens!'");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 28;
+            Item.rare = ItemRarityID.Green;
+            Item.value = Item.sellPrice(0, 0, 0, 0);
+            Item.useAnimation = 45;
+            Item.useTime = 45;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.rare = ItemRarityID.Orange;
+        }
+
+        /*  public override void AddRecipes()
+          {
+              Recipe recipe = CreateRecipe();
+              recipe.AddIngredient(ModContent.ItemType<VirulentPlating>(), 30);
+              recipe.AddIngredient(ModContent.ItemType<LostScrap>(), 20);
+              recipe.AddIngredient(ModContent.ItemType<DreadFoil>(), 15);
+              recipe.AddTile(TileID.WorkBenches);
+              recipe.Register();
+          }
+
+  */
+
+
+    }
+}
