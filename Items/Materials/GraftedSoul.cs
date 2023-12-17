34b8c63d049509bbdc853a94faee620ea815b55c
Scolon <49799704+sscolon@users.noreply.github.com>
2023-12-16 23:54:19 -0500

    contaminated soul now animates in the world

--- Items/Materials/GraftedSoul.cs
+++ Items/Materials/GraftedSoul.cs
@@ -14,6 +14,7 @@ namespace Stellamod.Items.Materials
 		public override void SetStaticDefaults()
 		{ 
 			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(1, 60));
+			ItemID.Sets.AnimatesAsSoul[Item.type] = true;
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100; // How many items are needed in order to research duplication of this item in Journey mode. See https://terraria.gamepedia.com/Journey_Mode/Research_list for a list of commonly used research amounts depending on item type.
 		}
