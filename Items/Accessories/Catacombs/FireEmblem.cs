417f67efa5559aa00d1d6d11dea7ef2f9c7b7dc8
Scolon <49799704+sscolon@users.noreply.github.com>
2023-12-19 12:36:08 -0500

    fire emblem, no not that one-

--- Items/Accessories/Catacombs/FireEmblem.cs
+++ Items/Accessories/Catacombs/FireEmblem.cs
@@ -1,13 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Projectiles.IgniterExplosions;
+using Stellamod.UI.Systems;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Stellamod.Items.Accessories.Catacombs
 {
+    internal class FireEmblemPlayer : ModPlayer
+    {
+        public bool hasFireEmblem;
+        public override void ResetEffects()
+        {
+            hasFireEmblem = false;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (hasFireEmblem)
+            {
+                switch (Main.rand.Next(0, 4))
+                {
+                    case 0:
+                        target.AddBuff(BuffID.OnFire3, 120);
+                        break;
+                    case 1:
+                        target.AddBuff(BuffID.ShadowFlame, 120);
+                        break;
+                    case 2:
+                        target.AddBuff(BuffID.CursedInferno, 120);
+                        break;
+                    case 3:
+                        target.AddBuff(BuffID.Daybreak, 60);
+                        break;
+                }
+
+                //1/3 chance for crits to EXPLODE
+                if (hit.Crit && Main.rand.NextBool(4))
+                {
+                    ShakeModSystem.Shake = 10;
+                    SoundEngine.PlaySound(new SoundStyle($"Stellamod/Assets/Sounds/Kaboom"));
+                    Projectile.NewProjectile(Player.GetSource_FromThis(), target.Center, Vector2.Zero,
+                        ModContent.ProjectileType<FireBoom>(), damageDone / 2, hit.Knockback, Player.whoAmI);
+                }
+            }
+        }
+    }
+
     internal class FireEmblem : ModItem
     {
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 28;
+            Item.rare = ItemRarityID.LightRed;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<FireEmblemPlayer>().hasFireEmblem = true;
+        }
     }
 }
---
+++ Projectiles/IgniterExplosions/FireBoom.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Projectiles.IgniterExplosions
+{
+	public class FireBoom : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("FrostShotIN");
+			Main.projFrames[Projectile.type] = 15;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.localNPCHitCooldown = -1;
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.friendly = true;
+			Projectile.width = 512;
+			Projectile.height = 512;
+			Projectile.penetrate = -1;
+			Projectile.timeLeft = 30;
+			Projectile.scale = 1f;
+		}
+
+		public float Timer
+		{
+			get => Projectile.ai[0];
+			set => Projectile.ai[0] = value;
+		}
+		public override void AI()
+		{
+			
+			Vector3 RGB = new(0.89f, 2.53f, 2.55f);
+			// The multiplication here wasn't doing anything
+			Lighting.AddLight(Projectile.position, RGB.X, RGB.Y, RGB.Z);
+
+		}
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+			switch (Main.rand.Next(0, 4))
+			{
+				case 0:
+					target.AddBuff(BuffID.OnFire3, 120);
+					break;
+				case 1:
+					target.AddBuff(BuffID.ShadowFlame, 120);
+					break;
+				case 2:
+					target.AddBuff(BuffID.CursedInferno, 120);
+					break;
+				case 3:
+					target.AddBuff(BuffID.Daybreak, 60);
+					break;
+			}
+		}
+
+		public override bool PreAI()
+		{
+			Projectile.tileCollide = false;
+			if (++Projectile.frameCounter >= 2)
+			{
+				Projectile.frameCounter = 0;
+				if (++Projectile.frame >= 15)
+				{
+					Projectile.frame = 0;
+				}
+			}
+			return true;
+
+
+		}
+
+		public override Color? GetAlpha(Color lightColor)
+		{
+			return new Color(255, 255, 255, 0) * (1f - Projectile.alpha / 50f);
+		}
+
+
+	}
+
+}
