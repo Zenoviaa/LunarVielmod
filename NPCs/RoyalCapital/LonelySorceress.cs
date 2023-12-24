306263d323573d73514502bc5b64917025f7c54e
Zenovia <calamityjewelstudios@gmail.com>
2023-12-24 17:59:40 -0500

    wompwomp

---
+++ NPCs/RoyalCapital/LonelySorceress.cs
@@ -0,0 +1,352 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Stellamod.Assets.Biomes;
+using Stellamod.Helpers;
+using Stellamod.Items.Accessories;
+using Stellamod.Items.Accessories.Brooches;
+using Stellamod.Items.Armors.Vanity.Gia;
+using Stellamod.Items.Harvesting;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Ores;
+using Stellamod.Items.Placeable;
+using Stellamod.Items.Quest.Merena;
+using Stellamod.Items.Weapons.Igniters;
+using Stellamod.Items.Weapons.Mage;
+using Stellamod.Items.Weapons.Melee;
+using Stellamod.Items.Weapons.Melee.Safunais;
+using Stellamod.Items.Weapons.PowdersItem;
+using Stellamod.Items.Weapons.Ranged;
+using Stellamod.Items.Weapons.Summon;
+using Stellamod.Items.Weapons.Thrown;
+using Stellamod.Items.Weapons.Whips;
+using Stellamod.NPCs.Bosses.Fenix;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.Personalities;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using Terraria.Utilities;
+
+namespace Stellamod.NPCs.RoyalCapital
+{
+	// [AutoloadHead] and NPC.townNPC are extremely important and absolutely both necessary for any Town NPC to work at all.
+	
+	public class LonelySorceress : ModNPC
+	{
+		public int NumberOfTimesTalkedTo = 0;
+		public const string ShopName = "Shop";
+		public const string ShopName2 = "New Shop";
+		public override void SetStaticDefaults()
+		{
+			// DisplayName automatically assigned from localization files, but the commented line below is the normal approach.
+			// DisplayName.SetDefault("Example Person");
+			Main.npcFrameCount[Type] = 60; // The amount of frames the NPC has
+
+			NPCID.Sets.ActsLikeTownNPC[Type] = true;
+
+			//To reiterate, since this NPC isn't technically a town NPC, we need to tell the game that we still want this NPC to have a custom/randomized name when they spawn.
+			//In order to do this, we simply make this hook return true, which will make the game call the TownNPCName method when spawning the NPC to determine the NPC's name.
+			NPCID.Sets.SpawnsWithCustomName[Type] = true;
+
+
+			// Influences how the NPC looks in the Bestiary
+			NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers()
+			{
+				Velocity = 1f, // Draws the NPC in the bestiary as if its walking +1 tiles in the x direction
+				Direction = 1 // -1 is left and 1 is right. NPCs are drawn facing the left by default but ExamplePerson will be drawn facing the right
+							  // Rotation = MathHelper.ToRadians(180) // You can also change the rotation of an NPC. Rotation is measured in radians
+							  // If you want to see an example of manually modifying these when the NPC is drawn, see PreDraw
+			};
+
+
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+
+			// Set Example Person's biome and neighbor preferences with the NPCHappiness hook. You can add happiness text and remarks with localization (See an example in ExampleMod/Localization/en-US.lang).
+
+
+
+
+
+			; // < Mind the semicolon!
+		}
+
+		// Current state
+
+
+		// Current frame
+		public int frameCounter;
+		// Current frame's progress
+		public int frameTick;
+		// Current state's timer
+		public float timer;
+
+		// AI counter
+		public int counter;
+		public override void SetDefaults()
+		{
+			// Sets NPC to be a Town NPC
+			NPC.friendly = true; // NPC Will not attack player
+			NPC.width = 106;
+			NPC.height = 92;
+			NPC.aiStyle = -1;
+			NPC.damage = 90;
+			NPC.defense = 42;
+			NPC.lifeMax = 2000;
+			NPC.knockBackResist = 0.5f;
+			NPC.dontTakeDamageFromHostiles = true;
+
+
+		}
+		public override void FindFrame(int frameHeight)
+		{
+			NPC.frameCounter += 0.16f;
+			NPC.frameCounter %= Main.npcFrameCount[NPC.type];
+			int frame = (int)NPC.frameCounter;
+			NPC.frame.Y = frame * frameHeight;
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			bool npcAlreadyExists = false;
+			for (int i = 0; i < Main.maxNPCs; i++)
+			{
+				NPC npc = Main.npc[i];
+				if (npc.type == ModContent.NPCType<LonelySorceress>() || npc.type == ModContent.NPCType<Fenix>())
+				{
+					npcAlreadyExists = true;
+					break;
+				}
+			}
+
+			//Don't spawn the npc if it already exists
+			if (npcAlreadyExists)
+			{
+				return 0f;
+			}
+
+			//If any player is underground and has an example item in their inventory, the example bone merchant will have a slight chance to spawn.
+
+			if (spawnInfo.Player.InModBiome<AlcadziaBiome>())
+			{
+
+				return 10f;
+
+			}
+
+			//Else, the example bone merchant will not spawn if the above conditions are not met.
+			return 0f;
+		}
+
+
+		public override bool CanChat()
+		{
+			return true;
+		}
+
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+		{
+			// We can use AddRange instead of calling Add multiple times in order to add multiple items at once
+			bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+				// Sets the preferred biomes of this town NPC listed in the bestiary.
+				// With Town NPCs, you usually set this to what biome it likes the most in regards to NPC happiness.
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.VortexPillar,
+
+				// Sets your NPC's flavor text in the bestiary.
+				new FlavorTextBestiaryInfoElement("A strange lady, she seems to overflow with magical energy!"),
+
+				// You can add multiple elements if you really wanted to
+				// You can also use localization keys (see Localization/en-US.lang)
+				new FlavorTextBestiaryInfoElement("Lonely Feeble Sorceress")
+			});
+		}
+
+		// The PreDraw hook is useful for drawing things before our sprite is drawn or running code before the sprite is drawn
+		// Returning false will allow you to manually draw your NPC
+		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+		{
+			// This code slowly rotates the NPC in the bestiary
+			// (simply checking NPC.IsABestiaryIconDummy and incrementing NPC.Rotation won't work here as it gets overridden by drawModifiers.Rotation each tick)
+			if (NPCID.Sets.NPCBestiaryDrawOffset.TryGetValue(Type, out NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers))
+			{
+				drawModifiers.Rotation += 0.001f;
+
+				// Replace the existing NPCBestiaryDrawModifiers with our new one with an adjusted rotation
+				NPCID.Sets.NPCBestiaryDrawOffset.Remove(Type);
+				NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+			}
+
+			return true;
+
+		}
+		public override string GetChat()
+		{
+			WeightedRandom<string> chat = new WeightedRandom<string>();
+
+			int partyGirl = NPC.FindFirstNPC(NPCID.PartyGirl);
+			if (partyGirl >= 0 && Main.rand.NextBool(4))
+			{
+				chat.Add(Language.GetTextValue("I wanna be like her", Main.npc[partyGirl].GivenName));
+			}
+			// These are things that the NPC has a chance of telling you when you talk to it.
+			chat.Add(Language.GetTextValue("Mmm it's soo nice to meet you, wanna play with me :3"));
+			chat.Add(Language.GetTextValue("I'm all alone in this castle, if only someone could have fun with me :> "));
+			chat.Add(Language.GetTextValue("Mwhahahahaha I am the most evil sorceress on the planet!!!"));
+			chat.Add(Language.GetTextValue("Obviously I want you to stay! Can you just stay here for a while- no, forever? I mean it isn't too much of an ask. "), 5.0);
+			chat.Add(Language.GetTextValue("There were these two that came by, one of them looked like me and had fox ears too! I wonder what breed she is."), 0.4);
+			chat.Add(Language.GetTextValue("This place can be shared by only us two you know, you can be here for me :)"), 0.1);
+			chat.Add(Language.GetTextValue("Nobody really gets me you know, but I can see it in your eyes, we'll be made for each other :p"), 0.1);
+			chat.Add(Language.GetTextValue("I think if you want to, maybe we can spar??? It would be so fun with you!"), 0.1);
+			chat.Add(Language.GetTextValue("I think this place is accidently communistic..."), 0.1);
+			chat.Add(Language.GetTextValue("Where did that Carian tome go?"), 0.1);
+			chat.Add(Language.GetTextValue("Hii babe! Can we pleaseee fight? I haven't been able to do anything for sooo long and I just wanna have fun but nobody lets me do anything around here, so pretty pleasee"), 0.1);
+			chat.Add(Language.GetTextValue("Funny enough I'm actually older than I act, take a guess I bet you didn't answer 26 lol!!"), 0.1);
+
+			NumberOfTimesTalkedTo++;
+			if (NumberOfTimesTalkedTo >= 30)
+			{
+				//This counter is linked to a single instance of the NPC, so if ExamplePerson is killed, the counter will reset.
+				chat.Add(Language.GetTextValue("Shhh hush hun, we should try to spar instead :3"));
+			}
+
+			return chat; // chat is implicitly cast to a string.
+		}
+		public override void HitEffect(NPC.HitInfo hit)
+		{
+			int num = NPC.life > 0 ? 1 : 5;
+
+			for (int k = 0; k < num; k++)
+			{
+				Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.GreenBlood);
+			}
+		}
+
+
+
+
+		public override void AI()
+		{
+			NPC.TargetClosest();
+
+			NPC.spriteDirection = NPC.direction;
+
+			
+				Player target = Main.player[NPC.target];
+				Vector2 targetCenter = target.Center;
+				Vector2 targetHoverCenter = targetCenter + new Vector2(0, -48);
+				NPC.Center = Vector2.Lerp(NPC.Center, targetHoverCenter, 0.15f);
+
+				if (!target.InModBiome<AlcadziaBiome>())
+                {
+
+					NPC.velocity = Vector2.Lerp(NPC.velocity, new Vector2(0, 8), 0.025f);
+					NPC.EncourageDespawn(1);
+					NPC.noTileCollide = true;
+				}
+			
+		}
+
+
+		public override List<string> SetNPCNameList()
+		{
+			return new List<string>() {
+				"Lonely Feeble Sorceress",
+				"Lonely Feeble Sorceress"
+
+			};
+		}
+
+
+
+
+		public override void SetChatButtons(ref string button, ref string button2)
+		{ // What the chat buttons are when you open up the chat UI
+			
+			button = "Spar with your new wife";
+
+		}
+
+		public override void OnChatButtonClicked(bool firstButton, ref string shop)
+		{
+
+			if (firstButton)
+			{
+
+				Player target = Main.player[NPC.target];
+				NPC.alpha = 255;
+				var entitySource = NPC.GetSource_FromThis();
+				int index = NPC.NewNPC(entitySource, (int)target.Center.X, (int)target.Center.Y, ModContent.NPCType<Fenix>());
+				NPC.Kill();
+
+			}
+
+
+		}
+
+
+		public void ResetTimers()
+		{
+			timer = 0;
+			frameCounter = 0;
+			frameTick = 0;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		//	else if (Main.moonPhase < 4) {
+		// shop.item[nextSlot++].SetDefaults(ItemType<ExampleGun>());
+		//		shop.item[nextSlot].SetDefaults(ItemType<ExampleBullet>());
+		//	}
+		//	else if (Main.moonPhase < 6) {
+		// shop.item[nextSlot++].SetDefaults(ItemType<ExampleStaff>());
+		// 	}
+		//
+		// 	// todo: Here is an example of how your npc can sell items from other mods.
+		// 	// var modSummonersAssociation = ModLoader.TryGetMod("SummonersAssociation");
+		// 	// if (ModLoader.TryGetMod("SummonersAssociation", out Mod modSummonersAssociation)) {
+		// 	// 	shop.item[nextSlot].SetDefaults(modSummonersAssociation.ItemType("BloodTalisman"));
+		// 	// 	nextSlot++;
+		// 	// }
+		//
+		// 	// if (!Main.LocalPlayer.GetModPlayer<ExamplePlayer>().examplePersonGiftReceived && GetInstance<ExampleConfigServer>().ExamplePersonFreeGiftList != null) {
+		// 	// 	foreach (var item in GetInstance<ExampleConfigServer>().ExamplePersonFreeGiftList) {
+		// 	// 		if (Item.IsUnloaded) continue;
+		// 	// 		shop.item[nextSlot].SetDefaults(Item.Type);
+		// 	// 		shop.item[nextSlot].shopCustomPrice = 0;
+		// 	// 		shop.item[nextSlot].GetGlobalItem<ExampleInstancedGlobalItem>().examplePersonFreeGift = true;
+		// 	// 		nextSlot++;
+		// 	// 		//TODO: Have tModLoader handle index issues.
+		// 	// 	}
+		// 	// }
+		// }
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+}
