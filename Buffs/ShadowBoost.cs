172abbd48c54f133481ad7ff941c01340032e896
Scolon <49799704+sscolon@users.noreply.github.com>
2023-12-25 00:06:16 -0500

    eldritchian set, it's pretty goodd

---
+++ Buffs/ShadowBoost.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Buffs
+{
+
+    public class ShadowBoost : ModBuff
+    {
+        //This buff doesn't do anything, it just shows you how much time you got left.
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.pvpBuff[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+        }
+    }
+}
---
+++ Items/Armors/Eldritchian/EldritchianCloak.cs
@@ -0,0 +1,50 @@
+﻿using Stellamod.Items.Harvesting;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Ores;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Armors.Eldritchian
+{
+    // The AutoloadEquip attribute automatically attaches an equip texture to this item.
+    // Providing the EquipType.Body value here will result in TML expecting X_Arms.png, X_Body.png and X_FemaleBody.png sprite-sheet files to be placed next to the item's main texture.
+    [AutoloadEquip(EquipType.Body)]
+	public class EldritchianCloak : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			base.SetStaticDefaults();
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 34; // Width of the item
+			Item.height = 24; // Height of the item
+			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+			Item.rare = ItemRarityID.Pink; // The rarity of the item
+			Item.defense = 12; // The amount of defense the item will give when equipped
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			player.lifeRegen += 3;
+			player.GetAttackSpeed(DamageClass.Ranged) += 0.12f;
+			player.GetDamage(DamageClass.Ranged) += 0.08f;
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<EldritchSoul>(), 25);
+			recipe.AddIngredient(ItemID.SoulofMight, 10);
+			recipe.AddIngredient(ModContent.ItemType<LostScrap>(), 10);
+			recipe.AddIngredient(ModContent.ItemType<DreadFoil>(), 10);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.Register();
+		}
+	}
+}
