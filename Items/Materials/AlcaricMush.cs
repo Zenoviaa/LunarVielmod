62b69f14c68e7321fc6460c73d20aab5c897796e
Scolon <49799704+sscolon@users.noreply.github.com>
2023-12-26 18:49:14 -0500

    alcaric mush animation in world

--- Items/Materials/AlcaricMush.cs
+++ Items/Materials/AlcaricMush.cs
@@ -11,16 +11,14 @@ namespace Stellamod.Items.Materials
 	{
 		public override void SetStaticDefaults()
 		{
-			// Tooltip.SetDefault("Super silk!");
-			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
-			
-
 			// Some please convert this to lang files, I'm too lazy to do it
 			// Sorry Itorius, I feel you
 			// DisplayName.AddTranslation(GameCulture.Polish, "Przykładowy blok");
 			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(1, 60));
-		 // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
+
+			// Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
+			ItemID.Sets.AnimatesAsSoul[Item.type] = true;
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100; // How many items are needed in order to research duplication of this item in Journey mode. See https://terraria.gamepedia.com/Journey_Mode/Research_list for a list of commonly used research amounts depending on item type.
 		}
 		public override void SetDefaults()
