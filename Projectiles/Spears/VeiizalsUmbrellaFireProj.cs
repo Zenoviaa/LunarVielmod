95ee5458569439e3e96aa7bc9529518de470df63
AzaleaThePhantomWitch <atych44@gmail.com>
2024-01-21 03:29:29 -0600

    jtghrt

---
+++ Projectiles/Spears/VeiizalsUmbrellaFireProj.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Buffs;
+using Stellamod.Trails;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Stellamod.Projectiles.Spears
+{
+
+    internal class VeiizalsUmbrellaFireProj : ModProjectile
+    {
+        bool Moved;
+
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("The Irradiaspear");
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 25;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.scale = 0.8f;
+            Projectile.width = 20;
+            Projectile.height = 20;
+            Projectile.timeLeft = 260;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+        }
+        public override void AI()
+        {
+            Projectile.velocity *= 1.01f;
+            Projectile.ai[1]++;
+            if (!Moved && Projectile.ai[1] >= 0)
+            {
+                SoundEngine.PlaySound(SoundID.DD2_BetsyFireballShot, Projectile.position);
+
+                Projectile.spriteDirection = Projectile.direction;
+                Projectile.rotation = Projectile.velocity.ToRotation() + 1.57f + 3.14f;
+                for (int j = 0; j < 10; j++)
+                {
+                    Vector2 vector2 = Vector2.UnitX * -Projectile.width / 2f;
+                    vector2 += -Vector2.UnitY.RotatedBy(j * 3.141591734f / 6f, default) * new Vector2(8f, 16f);
+                    vector2 = vector2.RotatedBy(Projectile.rotation - 1.57079637f, default);
+                    int num8 = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.Firework_Red, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
+                    Main.dust[num8].scale = 1.3f;
+                    Main.dust[num8].noGravity = true;
+                    Main.dust[num8].position = Projectile.Center + vector2;
+                    Main.dust[num8].velocity = Projectile.velocity * 0.1f;
+                    Main.dust[num8].noLight = true;
+                    Main.dust[num8].velocity = Vector2.Normalize(Projectile.Center - Projectile.velocity * 3f - Main.dust[num8].position) * 1.25f;
+                }
+                Moved = true;
+            }
+            if (Projectile.ai[1] >= 20)
+            {
+                Projectile.tileCollide = true;
+            }
+
+            Projectile.spriteDirection = Projectile.direction;
+            Projectile.rotation += 0.18f;
+        }
+        public override void OnKill(int timeLeft)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+
+            }
+        }
+        private float alphaCounter = 1;
+        public override void PostDraw(Color lightColor)
+        {
+            Texture2D texture2D4 = Request<Texture2D>("Stellamod/Effects/Masks/DimLight").Value;
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(85f * alphaCounter), (int)(05f * alphaCounter), (int)(15f * alphaCounter), 0), Projectile.rotation, new Vector2(32, 32), 0.17f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(85f * alphaCounter), (int)(05f * alphaCounter), (int)(15f * alphaCounter), 0), Projectile.rotation, new Vector2(32, 32), 0.17f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(85f * alphaCounter), (int)(05f * alphaCounter), (int)(15f * alphaCounter), 0), Projectile.rotation, new Vector2(32, 32), 0.17f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(85f * alphaCounter), (int)(05f * alphaCounter), (int)(15f * alphaCounter), 0), Projectile.rotation, new Vector2(32, 32), 0.07f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Lighting.AddLight(Projectile.Center, Color.Red.ToVector3() * 1.75f * Main.essScale);
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            if (Main.rand.NextBool(5))
+            {
+                int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Firework_Red, 0f, 0f, 150, Color.MediumPurple, 1f);
+                Main.dust[dustnumber].velocity *= 0.3f;
+                Main.dust[dustnumber].noGravity = true;
+            }
+            SpriteEffects Effects = Projectile.spriteDirection != 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+            Main.instance.LoadProjectile(Projectile.type);
+            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+
+            // Redraw the projectile with the color not influenced by light
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+            Vector2 drawOrigin = new Vector2(TextureAssets.Projectile[Projectile.type].Value.Width * 0.5f, Projectile.height * 0.5f);
+            for (int k = 0; k < Projectile.oldPos.Length; k++)
+            {
+                Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Color color = Projectile.GetAlpha(Color.Lerp(new Color(255, 8, 55), new Color(99, 39, 51), 1f / Projectile.oldPos.Length * k) * (1f - 1f / Projectile.oldPos.Length * k));
+                Main.spriteBatch.Draw(TextureAssets.Projectile[Projectile.type].Value, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0f);
+            }
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+
+            return true;
+        }
+
+    }
+
+}
+
+
---
+++ Projectiles/Spears/VeiizalsUmbrellaProj.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Projectiles.Spears
+{
+    internal class VeiizalsUmbrellaProj : ModProjectile
+    {
+        protected virtual float HoldoutRangeMin => 24f;
+        protected virtual float HoldoutRangeMax => 96f;
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Star Sheith");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.CloneDefaults(ProjectileID.Spear); // Clone the default values for a vanilla spear. Spear specific values set for width, height, aiStyle, friendly, penetrate, tileCollide, scale, hide, ownerHitCheck, and melee.
+        }
+
+        public override void PostDraw(Color lightColor)
+        {
+            Lighting.AddLight(Projectile.Center, Color.DarkRed.ToVector3() * 1.75f * Main.essScale);
+            if (Main.rand.NextBool(5))
+            {
+                int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Firework_Red, 0f, 0f, 150, Color.LightGoldenrodYellow, 1f);
+                Main.dust[dustnumber].velocity *= 0.3f;
+            }
+        }
+
+
+        public override bool PreAI()
+        {
+            Player player = Main.player[Projectile.owner]; // Since we access the owner player instance so much, it's useful to create a helper local variable for this
+            int duration = player.itemAnimationMax; // Define the duration the projectile will exist in frames
+            player.heldProj = Projectile.whoAmI; // Update the player's held projectile id
+
+            // Reset projectile time left if necessary
+            if (Projectile.timeLeft > duration)
+            {
+                Projectile.timeLeft = duration;
+            }
+
+            Projectile.velocity = Vector2.Normalize(Projectile.velocity); // Velocity isn't used in this spear implementation, but we use the field to store the spear's attack direction.
+            float halfDuration = duration * 0.5f;
+            float progress;
+
+            // Here 'progress' is set to a value that goes from 0.0 to 1.0 and back during the item use animation.
+            if (Projectile.timeLeft < halfDuration)
+            {
+                progress = Projectile.timeLeft / halfDuration;
+            }
+            else
+            {
+                progress = (duration - Projectile.timeLeft) / halfDuration;
+            }
+
+            // Move the projectile from the HoldoutRangeMin to the HoldoutRangeMax and back, using SmoothStep for easing the movement
+            Projectile.Center = player.MountedCenter + Vector2.SmoothStep(Projectile.velocity * HoldoutRangeMin, Projectile.velocity * HoldoutRangeMax, progress);
+
+            // Apply proper rotation to the sprite.
+            if (Projectile.spriteDirection == -1)
+            {
+                // If sprite is facing left, rotate 45 degrees
+                Projectile.rotation += MathHelper.ToRadians(45f);
+            }
+            else
+            {
+                // If sprite is facing right, rotate 135 degrees
+                Projectile.rotation += MathHelper.ToRadians(135f);
+            }
+
+            return false; // Don't execute vanilla AI.
+        }
+
+    }
+}
+
+
