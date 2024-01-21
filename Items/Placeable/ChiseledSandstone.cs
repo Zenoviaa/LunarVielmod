72da4b4e0c37a103ad45193c0be0191e45b5d7bd
Zenovia <calamityjewelstudios@gmail.com>
2024-01-21 02:31:51 -0500

    done

---
+++ Items/Placeable/ChiseledSandstone.cs
@@ -0,0 +1,52 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Placeable
+{
+    public class ChiseledSandstone : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// Tooltip.SetDefault("Cathedite Block");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+			
+
+			// Some please convert this to lang files, I'm too lazy to do it
+			// Sorry Itorius, I feel you
+
+			// DisplayName.AddTranslation(GameCulture.German, "Beispielblock");
+			// Tooltip.AddTranslation(GameCulture.German, "Dies ist ein modded Block");
+			// DisplayName.AddTranslation(GameCulture.Italian, "Blocco di esempio");
+			// Tooltip.AddTranslation(GameCulture.Italian, "Questo è un blocco moddato");
+			// DisplayName.AddTranslation(GameCulture.French, "Bloc d'exemple");
+			// Tooltip.AddTranslation(GameCulture.French, "C'est un bloc modgé");
+			// DisplayName.AddTranslation(GameCulture.Spanish, "Bloque de ejemplo");
+			// Tooltip.AddTranslation(GameCulture.Spanish, "Este es un bloque modded");
+			// DisplayName.AddTranslation(GameCulture.Russian, "Блок примера");
+			// Tooltip.AddTranslation(GameCulture.Russian, "Это модифицированный блок");
+			// DisplayName.AddTranslation(GameCulture.Chinese, "例子块");
+			// Tooltip.AddTranslation(GameCulture.Chinese, "这是一个修改块");
+			// DisplayName.AddTranslation(GameCulture.Portuguese, "Bloco de exemplo");
+			// Tooltip.AddTranslation(GameCulture.Portuguese, "Este é um bloco modded");
+			// DisplayName.AddTranslation(GameCulture.Polish, "Przykładowy blok");
+			// Tooltip.AddTranslation(GameCulture.Polish, "Jest to modded blok");
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 12;
+			Item.height = 12;
+			Item.maxStack = Item.CommonMaxStack;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 10;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<Tiles.ChiseledSandstoneT>();
+		}
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		
+	}
+}
---
+++ Tiles/ChiseledSandstoneT.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Tiles
+{
+	public class ChiseledSandstoneT : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			RegisterItemDrop(ModContent.ItemType<Items.Placeable.ChiseledSandstone>());
+			DustType = DustID.AmberBolt;
+
+			MineResist = 2f;
+			
+
+			AddMapEntry(new Color(26, 14, 2));
+
+			// TODO: implement
+			// SetModTree(new Trees.ExampleTree());
+		}
+		public override void NumDust(int i, int j, bool fail, ref int num)
+		{
+			num = fail ? 1 : 3;
+		}
+		public override bool CanExplode(int i, int j) => true;
+		// TODO: implement
+		// public override void ChangeWaterfallStyle(ref int style) {
+		// 	style = mod.GetWaterfallStyleSlot("ExampleWaterfallStyle");
+		// }
+		//
+		// public override int SaplingGrowthType(ref int style) {
+		// 	style = 0;
+		// 	return TileType<ExampleSapling>();
+		// }
+	}
+}
