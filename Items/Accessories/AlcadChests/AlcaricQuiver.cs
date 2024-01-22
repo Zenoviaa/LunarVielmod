34ac29dac8c753025f92ac8af48b168bc57e822d
Scolon <49799704+sscolon@users.noreply.github.com>
2024-01-22 03:40:14 -0500

    alcad items

---
+++ Items/Accessories/AlcadChests/AlcaricQuiver.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Projectiles.Arrows;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Accessories.AlcadChests
+{
+    internal class AlcaricQuiverPlayer : ModPlayer
+    {
+        public bool hasQuiver;
+        public override void ResetEffects()
+        {
+            base.ResetEffects();
+            hasQuiver = false;
+        }
+
+        public override void ModifyShootStats(Item item, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            if (type == ProjectileID.WoodenArrowFriendly && hasQuiver)
+            {
+                SoundEngine.PlaySound(SoundID.Item176);
+                type = ModContent.ProjectileType<VoidArrow>();
+                damage += 5;
+                velocity *= 2f;
+            }
+        }
+    }
+
+    internal class AlcaricQuiver : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 40;
+            Item.height = 46;
+            Item.accessory = true;
+            Item.rare = ItemRarityID.Green;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            base.UpdateAccessory(player, hideVisual);
+            player.GetModPlayer<AlcaricQuiverPlayer>().hasQuiver = true;
+        }
+    }
+}
---
+++ Items/Accessories/AlcadChests/BlackRose.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Buffs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Accessories.AlcadChests
+{
+    internal class BlackRose : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 36;
+            Item.rare = ItemRarityID.Green;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (player.HasBuff(BuffID.ManaSickness))
+            {
+                int combatText = CombatText.NewText(player.getRect(), Color.Red, "10", true);
+                CombatText numText = Main.combatText[combatText];
+                numText.lifeTime = 60;
+             
+                player.ClearBuff(BuffID.ManaSickness);
+                player.statLife -= 10;
+            }
+        }
+    }
+}
