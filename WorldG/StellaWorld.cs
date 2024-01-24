dce072ef046062b10c259bdfa974bbdac717ebde
Zenovia <calamityjewelstudios@gmail.com>
2024-01-24 03:19:39 -0500

    wopmp

--- WorldG/StellaWorld.cs
+++ WorldG/StellaWorld.cs
@@ -1354,7 +1354,7 @@ namespace Stellamod.WorldG
 				while (!placed && attempts++ < 1000000)
 				{
 					// Select a place in the first 6th of the world, avoiding the oceans
-					int smx = WorldGen.genRand.Next(350, (Main.maxTilesX) - 150); // from 50 since there's a unaccessible area at the world's borders
+					int smx = WorldGen.genRand.Next(350, (Main.maxTilesX) - 350); // from 50 since there's a unaccessible area at the world's borders
 																						  // 50% of choosing the last 6th of the world
 																										  // Choose which side of the world to be on randomly
 					///if (WorldGen.genRand.NextBool())
