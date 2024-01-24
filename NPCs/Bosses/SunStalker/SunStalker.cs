30d5b7148ab348b678d3b8977e15c9c030fe51d1
Zenovia <calamityjewelstudios@gmail.com>
2024-01-24 03:55:50 -0500

    woas

--- NPCs/Bosses/SunStalker/SunStalker.cs
+++ NPCs/Bosses/SunStalker/SunStalker.cs
@@ -55,7 +55,7 @@ namespace Stellamod.NPCs.Bosses.SunStalker
             NPC.width = 100;
             NPC.height = 116;
             NPC.damage = 63;
-            NPC.defense = 14;
+            NPC.defense = 12;
             NPC.lifeMax = 1600;
             NPC.HitSound = SoundID.NPCHit28;
             NPC.DeathSound = SoundID.NPCDeath42;
