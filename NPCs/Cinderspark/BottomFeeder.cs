3589b9cd4fe745a55a3f54dd04368c2dfed1be05
AzaleaThePhantomWitch <atych44@gmail.com>
2024-02-01 21:48:26 -0600

    grg

--- NPCs/Cinderspark/BottomFeeder.cs
+++ NPCs/Cinderspark/BottomFeeder.cs
@@ -1,19 +1,30 @@
-﻿using Microsoft.Xna.Framework;
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Stellamod.Assets.Biomes;
+using Stellamod.Buffs;
+using Stellamod.Helpers;
+using Stellamod.Items.Consumables;
 using Stellamod.Items.Harvesting;
 using Stellamod.Items.Materials;
-using Stellamod.NPCs.Catacombs.Fire.BlazingSerpent;
+using Stellamod.Items.Weapons.Mage;
+using Stellamod.Items.Weapons.Melee;
+using Stellamod.Items.Weapons.Ranged;
+using Stellamod.Items.Weapons.Summon;
+using Stellamod.NPCs.Bosses.DreadMire;
+using Stellamod.NPCs.Bosses.DreadMire.Heart;
+using Stellamod.NPCs.Bosses.singularityFragment;
+using Stellamod.NPCs.Bosses.singularityFragment.Phase1;
+using Stellamod.NPCs.Bosses.Verlia;
 using Stellamod.Projectiles;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
 using Terraria.Audio;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
 
 namespace Stellamod.NPCs.Cinderspark
 {
@@ -47,7 +58,40 @@ namespace Stellamod.NPCs.Cinderspark
             int frame = (int)NPC.frameCounter;
             NPC.frame.Y = frame * frameHeight;
         }
-
+        Vector2 Drawoffset => new Vector2(0, NPC.gfxOffY) + Vector2.UnitX * NPC.spriteDirection * 0;
+        public virtual string GlowTexturePath => Texture + "_Glow";
+        private Asset<Texture2D> _glowTexture;
+        public Texture2D GlowTexture => (_glowTexture ??= (RequestIfExists<Texture2D>(GlowTexturePath, out var asset) ? asset : null))?.Value;
+        public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            float num108 = 4;
+            float num107 = (float)Math.Cos((double)(Main.GlobalTimeWrappedHourly % 1.4f / 1.4f * 6.28318548f)) / 2f + 0.5f;
+            float num106 = 0f;
+            Color color1 = Color.OrangeRed * num107 * .8f;
+            var effects = NPC.spriteDirection == -1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+            spriteBatch.Draw(
+                GlowTexture,
+                NPC.Center - Main.screenPosition + Drawoffset,
+                NPC.frame,
+                color1,
+                NPC.rotation,
+                NPC.frame.Size() / 2,
+                NPC.scale,
+                effects,
+                0
+            );
+            SpriteEffects spriteEffects3 = NPC.spriteDirection == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            Vector2 vector33 = new Vector2(NPC.Center.X, NPC.Center.Y) - Main.screenPosition + Drawoffset - NPC.velocity;
+            Color color29 = new Color(127 - NPC.alpha, 127 - NPC.alpha, 127 - NPC.alpha, 0).MultiplyRGBA(Color.LightBlue);
+            for (int num103 = 0; num103 < 1; num103++)
+            {
+                Color color28 = color29;
+                color28 = NPC.GetAlpha(color28);
+                color28 *= 1f - num107;
+                Vector2 vector29 = NPC.Center + (num103 / (float)num108 * 6.28318548f + NPC.rotation + num106).ToRotationVector2() * (4f * num107 + 2f) - Main.screenPosition + Drawoffset - NPC.velocity * num103;
+                Main.spriteBatch.Draw(GlowTexture, vector29, NPC.frame, color28, NPC.rotation, NPC.frame.Size() / 2f, NPC.scale, spriteEffects3, 0f);
+            }
+        }
         public override void AI()
         {
             NPC.TargetClosest();
