75c6dfa1d71317e1a1b76e1d0bf8d89f17a8ea2b
Scolon <49799704+sscolon@users.noreply.github.com>
2024-02-02 05:44:36 -0500

    cinder enemies progress

--- Projectiles/TestProj.cs
+++ Projectiles/TestProj.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ParticleLibrary;
 using Stellamod.Helpers;
+using Stellamod.Particles;
 using Stellamod.Trails;
+using System.Collections.Generic;
+using System;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
@@ -8,54 +13,129 @@ using Terraria.ModLoader;
 
 namespace Stellamod.Projectiles
 {
-    internal class TestProj : ModProjectile
+    internal class TestProj : ModProjectile, IPixelPrimitiveDrawer
     {
-        public override void SetStaticDefaults()
-        {
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 80;
-            ProjectileID.Sets.TrailingMode[Projectile.type] = 3;
-        }
+        internal PrimitiveTrail BeamDrawer;
+        public ref float Time => ref Projectile.ai[0];
+        public NPC Owner => Main.npc[(int)Projectile.ai[1]];
+        public const float LaserLength = 2400f;
+
 
         public override void SetDefaults()
         {
-            Projectile.extraUpdates = 16;
-            Projectile.width = 20;
-            Projectile.height = 20;
+            Projectile.width = Projectile.height = 700;
             Projectile.friendly = true;
-            Projectile.hostile = false;
-            Projectile.tileCollide = true;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
             Projectile.penetrate = -1;
             Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = -1;
-        }
-
-        public float WidthFunction(float completionRatio)
-        {
-            float baseWidth = Projectile.scale * Projectile.width * 0.5f;
-            return MathHelper.SmoothStep(baseWidth, 3.5f, completionRatio);
-        }
-
-        public Color ColorFunction(float completionRatio)
-        {
-            return Color.Lerp(Color.OrangeRed, Color.Transparent, completionRatio);
-        }
-
-        public override bool PreDraw(ref Color lightColor)
-        {
-            DrawHelper.DrawSimpleTrail(Projectile, WidthFunction, ColorFunction, TrailRegistry.VortexTrail);
-            DrawHelper.DrawAdditiveAfterImage(Projectile, Color.OrangeRed, Color.Transparent, ref lightColor);
-            return base.PreDraw(ref lightColor);
+            Projectile.localNPCHitCooldown = 15;
+            Projectile.timeLeft = 600;
+            Projectile.alpha = 255;
+            CooldownSlot = ImmunityCooldownID.Bosses;
         }
 
         public override void AI()
         {
-            Projectile.rotation = Projectile.velocity.ToRotation();
-            Vector3 RGB = new(1.00f, 0.37f, 0.30f);
-            // The multiplication here wasn't doing anything
-            Lighting.AddLight(Projectile.position, RGB.X, RGB.Y, RGB.Z);
+            Player player = Main.player[Projectile.owner];
+            if (player.noItems || player.CCed || player.dead || !player.active)
+                Projectile.Kill();
 
-            int dust = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f);
-            Main.dust[dust].scale = 0.6f;
+            Vector2 playerCenter = player.RotatedRelativePoint(player.MountedCenter, true);
+            float swordRotation = 0f;
+            if (Main.myPlayer == Projectile.owner)
+            {
+                player.ChangeDir(Projectile.direction);
+                swordRotation = (Main.MouseWorld - player.Center).ToRotation();
+                if (!player.channel)
+                    Projectile.Kill();
+            }
+
+            Projectile.velocity = swordRotation.ToRotationVector2();
+            Projectile.spriteDirection = player.direction;
+            if (Projectile.spriteDirection == 1)
+                Projectile.rotation = Projectile.velocity.ToRotation();
+            else
+                Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.Pi;
+
+            // Fade in.
+            Projectile.alpha = Utils.Clamp(Projectile.alpha - 25, 0, 255);
+
+            Projectile.scale = MathF.Sin(Time / 600f * MathHelper.Pi) * 3f;
+            if (Projectile.scale > 1f)
+                Projectile.scale = 1f;
+
+
+            // And create bright light.
+            Lighting.AddLight(Projectile.Center, Color.DarkBlue.ToVector3() * 1.5f);
+
+
+
+            CreateDustAtBeginning();
+
+            Time++;
         }
+
+        public void CreateDustAtBeginning()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Dust fire = Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(10f, 10f), DustID.SpectreStaff);
+                fire.velocity = -Vector2.UnitY * Main.rand.NextFloat(1.5f, 3.25f);
+                fire.velocity *= Main.rand.NextBool(2).ToDirectionInt();
+                fire.scale = 1f + fire.velocity.Length() * 0.1f;
+                fire.color = Color.Lerp(Color.White, Color.GreenYellow, Main.rand.NextFloat());
+                fire.noGravity = false;
+            }
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            float _ = 0f;
+            float width = Projectile.width * 0.8f;
+            Vector2 start = Projectile.Center;
+            Vector2 end = start + Projectile.velocity * (LaserLength - 80f);
+            return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), start, end, width, ref _);
+        }
+
+        public float WidthFunction(float completionRatio)
+        {
+            return Projectile.width * Projectile.scale * 1.3f;
+        }
+
+        public override bool ShouldUpdatePosition() => false;
+
+        public Color ColorFunction(float completionRatio)
+        {
+            Color color = Color.Lerp(Color.Green, Color.GreenYellow, 0.65f);
+            return color * Projectile.Opacity * MathF.Pow(Utils.GetLerpValue(0f, 0.1f, completionRatio, true), 3f);
+        }
+
+        public override bool PreDraw(ref Color lightColor) => false;
+
+        public void DrawPixelPrimitives(SpriteBatch spriteBatch)
+        {
+            BeamDrawer ??= new PrimitiveTrail(WidthFunction, ColorFunction, null, true, TrailRegistry.LaserShader);
+
+            Color middleColor = Color.Lerp(Color.White, Color.LightSkyBlue, 0.6f);
+            Color middleColor2 = Color.Lerp(Color.LightBlue, Color.AliceBlue, 0.5f);
+            Color finalColor = Color.Lerp(middleColor, middleColor2, Time / 600);
+
+            TrailRegistry.LaserShader.UseColor(Color.LightSkyBlue);
+            TrailRegistry.LaserShader.SetShaderTexture(TrailRegistry.BeamTrail);
+
+            List<float> originalRotations = new();
+            List<Vector2> points = new();
+            for (int i = 0; i <= 8; i++)
+            {
+                points.Add(Vector2.Lerp(Projectile.Center, Projectile.Center + Projectile.velocity * LaserLength, i / 8f));
+                originalRotations.Add(MathHelper.PiOver2);
+            }
+
+            BeamDrawer.DrawPixelated(points, -Main.screenPosition, 32);
+            Main.spriteBatch.ExitShaderRegion();
+        }
+
+        public override bool? CanDamage() => Time >= 60f;
     }
 }
