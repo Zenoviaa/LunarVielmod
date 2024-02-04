4df38bc8835cdee59ea5688d5165f257590e21e1
Zenovia <calamityjewelstudios@gmail.com>
2024-02-03 19:50:39 -0500

    we

--- Items/Quest/Zui/EmptyCollectorsBag.cs
+++ Items/Quest/Zui/EmptyCollectorsBag.cs
@@ -13,7 +13,7 @@ namespace Stellamod.Items.Quest.Zui
             Item.height = 20; // The item texture's height
 
             Item.maxStack = 99; // The item's max stack value
-            Item.value = Item.buyPrice(silver: 50); // The value of the item in copper coins. Item.buyPrice & Item.sellPrice are helper methods that returns costs in copper coins based on platinum/gold/silver/copper arguments provided to it.
+            Item.value = Item.sellPrice(silver: 1); // The value of the item in copper coins. Item.buyPrice & Item.sellPrice are helper methods that returns costs in copper coins based on platinum/gold/silver/copper arguments provided to it.
             Item.rare = ItemRarityID.Quest;
             Item.questItem = true;
         }
