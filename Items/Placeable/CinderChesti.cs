ba26774800a02f86a3a43b2f443be04fec9d8927
Zenovia <calamityjewelstudios@gmail.com>
2024-02-03 19:43:33 -0500

    eina

---
+++ Items/Placeable/CinderChesti.cs
@@ -0,0 +1,38 @@
+﻿using Stellamod.Tiles.Abyss.Aurelus;
+using Stellamod.Tiles;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Placeable
+{
+	public class CinderChesti : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("Gilded Chest");
+			// Tooltip.SetDefault("A chest dedicated to the feral warriors of the morrow");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 26;
+			Item.height = 22;
+			Item.maxStack = 99;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.value = 500;
+			Item.createTile = ModContent.TileType<CinderChest>();
+			Item.placeStyle = 1; // Use this to place the chest in its locked style
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+
+	}
+}
