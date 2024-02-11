6e9a557e426f12b701d0435a30dc788a9debc762
Zenovia <calamityjewelstudios@gmail.com>
2024-02-10 23:09:56 -0500

    v

---
+++ Projectiles/BrackettProj.cs
@@ -0,0 +1,79 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Projectiles
+{
+	public class BrackettProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("Plantius");
+			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;
+			ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.CloneDefaults(ProjectileID.FrostDaggerfish);
+			AIType = ProjectileID.FrostDaggerfish;
+			Projectile.penetrate = -1;
+			Projectile.width = 32;
+			Projectile.height = 32;
+		}
+
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+		{
+			if (Main.rand.NextBool(2))
+				target.AddBuff(BuffID.Poisoned, 180);
+		}
+
+		public override bool PreAI()
+		{
+			if (Main.rand.NextBool(3))
+			{
+				Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Dirt);
+				Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.JunglePlants);
+			}
+
+			return true;
+		}
+
+        public override void AI()
+        {
+			Projectile.rotation += 0.1f;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+		{
+			Main.instance.LoadProjectile(Projectile.type);
+			Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+
+			// Redraw the projectile with the color not influenced by light
+			Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+			for (int k = 0; k < Projectile.oldPos.Length; k++)
+			{
+				Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+				Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+				Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+			}
+
+			return false;
+		}
+
+		public override void OnKill(int timeLeft)
+		{
+			for (int i = 0; i < 15; i++)
+			{
+				SoundEngine.PlaySound(SoundID.Dig, Projectile.Center);
+				Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Dirt);
+				Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.SilverCoin);
+			}
+		}
+	}
+}
