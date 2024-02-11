e99a907af95e5f40a0436c92c6e36a0cb0c531a3
Scolon <49799704+sscolon@users.noreply.github.com>
2024-02-11 04:55:06 -0500

    vacuum drill

---
+++ Items/Weapons/Ranged/VacuumDrill.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Stellamod.NPCs.Catacombs.Trap.Sparn;
+using Stellamod.NPCs.Catacombs.Water.WaterCogwork;
+using Stellamod.Projectiles;
+using Stellamod.Projectiles.Gun;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Weapons.Ranged
+{
+    public class VacuumDrill : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            /* Tooltip.SetDefault("Meatballs" +
+				"\nDo not be worried, this mushes reality into bit bits and then shoots it!" +
+				"\nYou can never miss :P"); */
+            // DisplayName.SetDefault("Teraciz");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 62;
+            Item.height = 32;
+            Item.scale = 0.9f;
+            Item.rare = ItemRarityID.Green;
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = false;
+            Item.UseSound = SoundID.DD2_LightningAuraZap;
+
+            // Weapon Properties
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 19;
+            Item.knockBack = 0;
+            Item.noMelee = true;
+
+            // Gun Properties
+            Item.shoot = ModContent.ProjectileType<VacuumDrillHold>();
+            Item.shootSpeed = 15f;
+            Item.channel = true;
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        // This method lets you adjust position of the gun in the player's hands. Play with these values until it looks good with your graphics.
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(2f, -2f);
+        }
+    }
+}
---
+++ Projectiles/Gun/VacuumLightningBolt.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ParticleLibrary;
+using Stellamod.Particles;
+using Stellamod.Trails;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Projectiles.Gun
+{
+    internal class VacuumLightningBolt : ModProjectile, IPixelPrimitiveDrawer
+    {
+        private Vector2[] _lightningArcPos = new Vector2[1]; 
+        public const int Trail_Width = 24;
+        public override void SetStaticDefaults()
+        {
+            // Sets the amount of frames this minion has on its spritesheet
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 24;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.friendly = true;
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.timeLeft = int.MaxValue;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 10;
+            Projectile.tileCollide = false;
+        }
+
+        public override void AI()
+        {
+   
+            Player player = Main.player[Projectile.owner];
+            if (Main.myPlayer == Projectile.owner)
+            {
+                if (!player.channel)
+                    Projectile.Kill();
+            }
+
+            Vector2 playerCenter = player.RotatedRelativePoint(player.MountedCenter, true);
+            float swordRotation = 0f;
+            if (Main.myPlayer == Projectile.owner)
+            {
+                player.ChangeDir(Projectile.direction);
+                swordRotation = (Main.MouseWorld - player.Center).ToRotation();
+                if (!player.channel)
+                    Projectile.Kill();
+            }
+
+            alphaCounter = VectorHelper.Osc(0.5f, 1.00f, 3);
+            Projectile.velocity = swordRotation.ToRotationVector2();
+            Projectile.Center = playerCenter + Projectile.velocity * 1f;// customization of the hitbox position
+            //Dunno if this is needed but whatever
+            Projectile.rotation = Projectile.velocity.ToRotation();
+
+            _lightningArcPos = CalculateLightningArc();
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            //Electrifying!!!! nEMIES!!!
+            target.AddBuff(BuffID.Electrified, 120);
+            SoundEngine.PlaySound(SoundID.DD2_LightningBugZap);
+
+            for (int i = 0; i < 8; i++)
+            {
+                Vector2 speed = Main.rand.NextVector2CircularEdge(1, 1);
+                var d = Dust.NewDustPerfect(Projectile.Center, DustID.Electric, speed, Scale: 1.5f);
+                d.noGravity = true;
+            }
+        }
+
+        public float WidthFunction(float completionRatio)
+        {
+            float baseWidth = Projectile.scale * Trail_Width;
+            return MathHelper.SmoothStep(baseWidth / 2, baseWidth, completionRatio);
+        }
+
+        public Color ColorFunction(float completionRatio)
+        {
+            Color startColor = Color.Cyan;
+            Color endColor = Color.Transparent;
+            return Color.Lerp(startColor, endColor, completionRatio);
+        }
+
+        private Vector2[] CalculateLightningArc()
+        {
+            float teleportDistance = 192;
+            Vector2 currentPosition = Projectile.position;
+            List<Vector2> positions = new List<Vector2>();
+            positions.Add(currentPosition);
+            for (int i = 0; i < 24; i++)
+            {
+                Vector2 direction = Projectile.velocity.SafeNormalize(Vector2.Zero);
+                direction = direction.RotatedByRandom(MathHelper.ToRadians(60));
+                float distance = Main.rand.NextFloat(20, 45);
+                Vector2 newPosition = currentPosition + direction * distance;
+                currentPosition = newPosition;
+                positions.Add(currentPosition);
+
+
+                float distanceFromTarget = 700;
+                Vector2 targetCenter = currentPosition;
+                bool foundTarget = false;
+                // This code is required either way, used for finding a target
+                for (int j = 0; j < Main.maxNPCs; j++)
+                {
+                    NPC npc = Main.npc[j];
+                    if (npc.CanBeChasedBy())
+                    {
+                        float between = Vector2.Distance(npc.Center, currentPosition);
+                        bool closest = Vector2.Distance(currentPosition, targetCenter) > between;
+                        bool inRange = between < distanceFromTarget;
+                        bool lineOfSight = Collision.CanHitLine(currentPosition, Projectile.width, Projectile.height, npc.position, npc.width, npc.height);
+                        if (((closest && inRange) || !foundTarget) && lineOfSight)
+                        {
+                            distanceFromTarget = between;
+                            targetCenter = npc.Center;
+                            foundTarget = true;
+                        }
+                    }
+                }
+
+                if (!foundTarget)
+                {
+                    float distanceToMouse = Vector2.Distance(currentPosition, Main.MouseWorld);
+                    if (distanceToMouse < teleportDistance)
+                    {
+                        positions.Add(Main.MouseWorld);
+                        positions.Add(Main.MouseWorld);
+                        break;
+                    }
+                }else if (foundTarget && distanceFromTarget < teleportDistance)
+                {
+                    positions.Add(targetCenter);
+                    positions.Add(targetCenter);
+                    break;
+                }
+            }
+
+
+            return positions.ToArray();
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            //This damages everything in the trail
+            Vector2[] positions = _lightningArcPos;
+            float collisionPoint = 0;
+            for (int i = 1; i < positions.Length; i++)
+            {
+                Vector2 position = positions[i];
+                Vector2 previousPosition = positions[i - 1];
+                if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), position, previousPosition, Trail_Width, ref collisionPoint))
+                    return true;
+            }
+            return base.Colliding(projHitbox, targetHitbox);
+        }
+
+        internal PrimitiveTrail BeamDrawer;
+
+        public float alphaCounter = 1;
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture2D4 = ModContent.Request<Texture2D>("Stellamod/Effects/Masks/DimLight").Value;
+            Vector2 drawPos = _lightningArcPos[_lightningArcPos.Length - 1];
+            Vector2 drawOrigin = new Vector2(32, 32);
+            for(int i = 0; i < 3; i++)
+            {
+                Main.spriteBatch.Draw(texture2D4, drawPos - Main.screenPosition, null, new Color((int)(255f * alphaCounter), (int)(255f * alphaCounter), (int)(255f * alphaCounter), 0), Projectile.rotation,
+                    drawOrigin, 1.4f, SpriteEffects.None, 0f);
+            }
+
+            return base.PreDraw(ref lightColor);
+        }
+
+        public void DrawPixelPrimitives(SpriteBatch spriteBatch)
+        {
+            BeamDrawer ??= new PrimitiveTrail(WidthFunction, ColorFunction, null, true, TrailRegistry.LaserShader);
+
+            TrailRegistry.LaserShader.UseColor(Color.LightCyan);
+            TrailRegistry.LaserShader.SetShaderTexture(TrailRegistry.BeamTrail);
+
+            BeamDrawer.DrawPixelated(_lightningArcPos, -Main.screenPosition, _lightningArcPos.Length);
+            Main.spriteBatch.ExitShaderRegion();
+        }
+    }
+}
