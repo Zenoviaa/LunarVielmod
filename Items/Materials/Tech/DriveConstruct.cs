9729cf3397e5ad8d6f1008ea05b76f4570a06dd6
Zenovia <calamityjewelstudios@gmail.com>
2024-02-11 03:33:48 -0500

    da

--- Items/Materials/Tech/DriveConstruct.cs
+++ Items/Materials/Tech/DriveConstruct.cs
@@ -17,7 +17,7 @@ namespace Stellamod.Items.Materials.Tech
             Item.width = 20;
             Item.height = 20;
             Item.maxStack = Item.CommonMaxStack;
-            Item.value = Item.buyPrice(0, 1, 50, 0);
+            Item.value = Item.buyPrice(0, 4, 50, 0);
             Item.rare = ItemRarityID.Blue;
             Item.useTime = 10;
             Item.useAnimation = 10;
