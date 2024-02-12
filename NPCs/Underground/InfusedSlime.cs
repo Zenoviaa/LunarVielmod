f6e6130f5fb15d8d5ced93467268b2f7f6b34354
Scolon <49799704+sscolon@users.noreply.github.com>
2024-02-11 20:26:28 -0500

    Infused Slime

---
+++ NPCs/Underground/InfusedSlime.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Helpers;
+using Stellamod.Projectiles;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+
+namespace Stellamod.NPCs.Underground
+{
+    internal class InfusedSlime : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 23;
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 86;
+            NPC.height = 48;
+            NPC.damage = 42;
+            NPC.defense = 8;
+            NPC.lifeMax = 128;
+            NPC.HitSound = new SoundStyle("Stellamod/Assets/Sounds/Gintze_Hit") with { PitchVariance = 0.1f };
+            NPC.DeathSound = new SoundStyle("Stellamod/Assets/Sounds/Gintze_Death") with { PitchVariance = 0.1f };
+            NPC.value = 563f;
+            NPC.knockBackResist = .45f;
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            NPC.frameCounter += 0.4f;
+            NPC.frameCounter %= Main.npcFrameCount[NPC.type];
+            int frame = (int)NPC.frameCounter;
+            NPC.frame.Y = frame * frameHeight;
+        }
+
+        private ref float AI_Timer => ref NPC.ai[0];
+        private ref float AI_Walk_Direction => ref NPC.ai[1];
+
+        private void AI_Movement(float walkDirection)
+        {
+            walkDirection = Math.Sign(walkDirection);
+            float targetX = walkDirection * 1;
+            float accel = 0.2f;
+            if (targetX <= -1)
+            {
+                if (NPC.velocity.X > targetX)
+                {
+                    NPC.velocity.X -= accel;
+                }
+                else
+                {
+                    NPC.velocity.X += accel;
+                }
+
+            }
+            else if (targetX >= 1)
+            {
+                if (NPC.velocity.X < targetX)
+                {
+                    NPC.velocity.X += accel;
+                }
+                else
+                {
+                    NPC.velocity.X -= accel;
+                }
+            }
+            else
+            {
+                NPC.velocity.X *= 0.9f;
+            }
+        }
+
+        public override void AI()
+        {
+            Player target = Main.player[NPC.target];
+            NPC.TargetClosest();
+            if (NPC.collideX)
+            {
+                Collision.StepUp(ref NPC.position, ref NPC.velocity, NPC.width, NPC.height, ref NPC.stepSpeed, ref NPC.gfxOffY);
+            }
+
+            if (NPC.HasValidTarget && Collision.CanHitLine(NPC.position, NPC.width, NPC.height, target.position, target.width, target.height))
+            {
+                float walkDirection = NPC.DirectionTo(target.Center).X;
+                AI_Movement(walkDirection);
+                AI_Timer++;
+                if (AI_Timer == 60)
+                {
+                    SoundEngine.PlaySound(SoundID.DD2_LightningAuraZap);
+                }
+                else if (AI_Timer > 60 && AI_Timer < 120)
+                {
+                    Dust dust = Dust.NewDustDirect(NPC.Center, NPC.width, NPC.height, DustID.Electric);
+                    dust.velocity *= -1f;
+                    dust.scale *= 0.5f;
+                    dust.noGravity = true;
+                    Vector2 vector2_1 = new Vector2(Main.rand.Next(-80, 81), Main.rand.Next(-80, 81));
+                    vector2_1.Normalize();
+                    Vector2 vector2_2 = vector2_1 * (Main.rand.Next(50, 100) * 0.04f);
+                    dust.velocity = vector2_2;
+                    vector2_2.Normalize();
+                    Vector2 vector2_3 = vector2_2 * 34f;
+                    dust.position = NPC.Center - vector2_3;
+
+                    //Telegraph
+                    NPC.FaceTarget();
+
+                }
+                else if (AI_Timer >= 120)
+                {
+                    if (StellaMultiplayer.IsHost)
+                    {
+                        Vector2 velocity = NPC.DirectionTo(target.Center) * 10;
+                        Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, velocity,
+                                ModContent.ProjectileType<InfusedSlimeBolt>(), 15, 1, Owner: Main.myPlayer);
+                    }
+
+                    SoundEngine.PlaySound(SoundID.DD2_LightningBugZap);
+                    AI_Timer = 0;
+                }
+            } 
+            else
+            {
+                float walkDirection = AI_Walk_Direction;
+                AI_Movement(walkDirection);
+                if (Main.rand.NextBool(300))
+                {
+                    AI_Walk_Direction = Main.rand.Next(-1, 2);
+                    NPC.netUpdate = true;
+                }
+            }
+
+            Visuals();
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            //You can't be in the surface and underground at the same time so this should work
+            //0.05f should make it 20 less common than normal spawns.
+            return (SpawnCondition.Cavern.Chance * 0.5f);
+        }
+
+        private void Visuals()
+        {
+            if (Main.rand.NextBool(16))
+            {
+                Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.Electric);
+            }
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            Vector3 huntrianColorXyz = DrawHelper.HuntrianColorOscillate(
+                Color.White.ToVector3(),
+                Color.WhiteSmoke.ToVector3(),
+                new Vector3(3, 3, 3), 0);
+
+            DrawHelper.DrawDimLight(NPC, huntrianColorXyz.X, huntrianColorXyz.Y, huntrianColorXyz.Z, Color.White, Color.WhiteSmoke, 0);
+            Lighting.AddLight(screenPos, Color.White.ToVector3() * 1.0f * Main.essScale);
+            return base.PreDraw(spriteBatch, screenPos, drawColor);
+        }
+    }
+}
---
+++ Projectiles/InfusedSlimeBolt.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Trails;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Projectiles
+{
+    internal class InfusedSlimeBolt : ModProjectile, IPixelPrimitiveDrawer
+    {
+        public override void SetStaticDefaults()
+        {
+            // Sets the amount of frames this minion has on its spritesheet
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 18;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.friendly = false;
+            Projectile.hostile = true;
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.timeLeft = 180;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+        }
+
+        private ref float AI_Timer => ref Projectile.ai[0];
+        public override void AI()
+        {
+            AI_Timer++;
+        
+            if (AI_Timer % 2 == 0)
+            {
+                //Randomly teleport to make the jagged effect
+                Vector2 direction = Projectile.velocity.SafeNormalize(Vector2.Zero);
+                direction = direction.RotatedByRandom(MathHelper.ToRadians(210));
+                float distance = Main.rand.NextFloat(2, 8);
+                Projectile.Center = Projectile.Center + direction * distance;
+                Projectile.netUpdate = true;
+            }
+
+            //Dunno if this is needed but whatever
+            Projectile.rotation = Projectile.velocity.ToRotation();
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            //Electrifying!!!! nEMIES!!!
+            target.AddBuff(BuffID.Electrified, 120);
+            SoundEngine.PlaySound(SoundID.DD2_LightningBugZap);
+
+            for (int i = 0; i < 8; i++)
+            {
+                Vector2 speed = Main.rand.NextVector2CircularEdge(1, 1);
+                var d = Dust.NewDustPerfect(Projectile.Center, DustID.Electric, speed, Scale: 1.5f);
+                d.noGravity = true;
+            }
+        }
+        public float WidthFunction(float completionRatio)
+        {
+            float baseWidth = Projectile.scale * 8;
+            return MathHelper.SmoothStep(baseWidth, 3.5f, completionRatio);
+        }
+
+        public Color ColorFunction(float completionRatio)
+        {
+            Color startColor = Color.Cyan;
+            Color endColor = Color.Transparent;
+            return Color.Lerp(startColor, endColor, completionRatio);
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            //This damages everything in the trail
+            Vector2[] positions = Projectile.oldPos;
+            float collisionPoint = 0;
+            for (int i = 1; i < positions.Length; i++)
+            {
+                Vector2 position = positions[i];
+                Vector2 previousPosition = positions[i - 1];
+                if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), position, previousPosition, 1, ref collisionPoint))
+                    return true;
+            }
+            return base.Colliding(projHitbox, targetHitbox);
+        }
+
+        internal PrimitiveTrail BeamDrawer;
+        public void DrawPixelPrimitives(SpriteBatch spriteBatch)
+        {
+            BeamDrawer ??= new PrimitiveTrail(WidthFunction, ColorFunction, null, true, TrailRegistry.LaserShader);
+
+            TrailRegistry.LaserShader.UseColor(Color.LightCyan);
+            TrailRegistry.LaserShader.SetShaderTexture(TrailRegistry.BeamTrail);
+
+            BeamDrawer.DrawPixelated(Projectile.oldPos, -Main.screenPosition, 32);
+            Main.spriteBatch.ExitShaderRegion();
+        }
+    }
+}
