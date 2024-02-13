ae50ef1021d5b0fec81c4029c54b1e47b123bc65
Zenovia <calamityjewelstudios@gmail.com>
2024-02-12 22:32:16 -0500

    takeoffyourskin

---
+++ Tiles/DrakonicManor/LabsS.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+
+
+namespace Stellamod.Tiles.DrakonicManor
+{
+	public class LabsS : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			// Properties
+			Main.tileTable[Type] = true;
+			Main.tileSolidTop[Type] = false;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = false;
+			Main.tileFrameImportant[Type] = true;
+			TileID.Sets.DisableSmartCursor[Type] = true;
+			TileID.Sets.IgnoredByNpcStepUp[Type] = true; // This line makes NPCs not try to step up this tile during their movement. Only use this for furniture with solid tops.
+			MineResist = 4f;
+			MinPick = 200;
+
+			DustType = DustID.InfernoFork;
+			AdjTiles = new int[] { TileID.Bookcases };
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+			TileObjectData.newTile.Height = 13;
+			TileObjectData.newTile.Width = 34;
+
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, };
+			TileObjectData.newTile.StyleWrapLimit = 2; //not really necessary but allows me to add more subtypes of chairs below the example chair texture
+			TileObjectData.newTile.StyleMultiplier = 2; //same as above
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.addTile(Type);
+			LocalizedText name = CreateMapEntryName();
+
+			// name.SetDefault("GrassBigS");
+			AddMapEntry(new Color(200, 10, 100), name);
+		}
+		public override bool CanExplode(int i, int j) => false;
+		public override void NumDust(int x, int y, bool fail, ref int num)
+		{
+			num = fail ? 1 : 3;
+		}
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+
+		}
+	}
+}
