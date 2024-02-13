6c1c38131bdef54a7faabb9046fca8bc3f9ef0f3
Zenovia <calamityjewelstudios@gmail.com>
2024-02-12 21:46:39 -0500

    done

---
+++ Items/Placeable/Laboratory/GLabCrystal.cs
@@ -0,0 +1,25 @@
+﻿using Stellamod.Tiles.Structures.AlcadizNGovheil;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Placeable.Laboratory
+{
+    public class GLabCrystal : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("CurtainLeft");
+			// Tooltip.SetDefault("Curtain");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+		public override void SetDefaults()
+		{
+			Item.DefaultToPlaceableTile(ModContent.TileType<LabCrystal>());
+			Item.value = 150;
+			Item.maxStack = 9999;
+			Item.width = 38;
+			Item.height = 24;
+		}
+	}
+}
