5f9ca1703b130b9f28fd8ba3b927a9c3f1d5d0b8
Scolon <49799704+sscolon@users.noreply.github.com>
2024-02-14 15:42:05 -0500

    normalized boss relics

---
+++ Items/Placeable/BossRelicItem.cs
@@ -0,0 +1,29 @@
+﻿using Stellamod.Tiles.Furniture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace Stellamod.Items.Placeable
+{
+    public abstract class BossRelicItem : ModItem
+    {
+        public abstract int TileType { get; }
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(TileType);
+            Item.value = 150;
+            Item.maxStack = 20;
+            Item.width = 30;
+            Item.height = 44;
+
+            Item.rare = ItemRarityID.Master;
+            Item.master = true; // This makes sure that "Master" displays in the tooltip, as the rarity only changes the item name color
+            Item.value = Item.buyPrice(0, 5);
+        }
+    }
+}
---
+++ Items/Placeable/BossRelicsItems.cs
@@ -0,0 +1,18 @@
+﻿using Stellamod.Tiles.Furniture;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Placeable
+{
+    public class DaedusBossRel : BossRelicItem { public override int TileType => ModContent.TileType<DaedusBossRelic>(); }
+    public class DreadBossRel : BossRelicItem { public override int TileType => ModContent.TileType<DreadBossRelic>(); }
+    public class FenixBossRel : BossRelicItem { public override int TileType => ModContent.TileType<FenixBossRelic>(); }
+    public class IrradiaBossRel : BossRelicItem { public override int TileType => ModContent.TileType<IrradiaBossRelic>(); }
+    public class JackBossRel : BossRelicItem { public override int TileType => ModContent.TileType<JackBossRelic>(); }
+    public class SOMBossRel : BossRelicItem { public override int TileType => ModContent.TileType<SOMBossRelic>(); }
+    public class SunsBossRel : BossRelicItem { public override int TileType => ModContent.TileType<SunsBossRelic>(); }
+    public class SyliaBossRel : BossRelicItem { public override int TileType => ModContent.TileType<SyliaBossRelic>(); }
+    public class VeriBossRel : BossRelicItem { public override int TileType => ModContent.TileType<VeriBossRelic>(); }
+    public class VerliBossRel : BossRelicItem { public override int TileType => ModContent.TileType<VerliBossRelic>(); }
+    public class GintzeBossRel : BossRelicItem { public override int TileType => ModContent.TileType<GintzeBossRelic>(); }
+    public class ZuiBossRel : BossRelicItem { public override int TileType => ModContent.TileType<ZuiBossRelic>(); }
+}
