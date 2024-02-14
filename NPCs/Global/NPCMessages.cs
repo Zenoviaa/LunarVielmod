48479c9e886739cf5ba740d1445a9941ebb03307
Scolon <49799704+sscolon@users.noreply.github.com>
2024-02-14 17:54:25 -0500

    npc messages

--- NPCs/Global/NPCMessages.cs
+++ NPCs/Global/NPCMessages.cs
@@ -11,7 +11,21 @@ namespace Stellamod.NPCs.Global
     {
         public override void OnKill(NPC npc)
         {
-            if(npc.type == NPCID.WallofFlesh && !Main.hardMode)
+            if(npc.type == NPCID.Plantera && !NPC.downedPlantBoss)
+            {
+                string text = "Zui has something to offer you...";
+                Color color = Color.Gold;
+                if (Main.netMode == NetmodeID.Server)
+                {
+                    NetworkText nText = NetworkText.FromLiteral(text);
+                    ChatHelper.BroadcastChatMessage(nText, color);
+                }
+                else
+                {
+                    Main.NewText(text, color.R, color.G, color.B);
+                }
+            }
+            else if(npc.type == NPCID.WallofFlesh && !Main.hardMode)
             {
                 string text = "The Abysm and Virulent stir...";
                 Color color = Color.Red;
