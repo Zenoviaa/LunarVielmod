200a411099890b229b0e8d9cefafb9e657e806cc
Scolon <49799704+sscolon@users.noreply.github.com>
2024-02-19 16:25:06 -0500

    catacombs key drop change

---
+++ NPCs/Global/NPCBagEdits.cs
@@ -0,0 +1,19 @@
+﻿using Stellamod.Items.Consumables;
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.NPCs.Global
+{
+    internal class NPCBagEdits : GlobalItem
+    {
+        public override void ModifyItemLoot(Item item, ItemLoot itemLoot)
+        {
+            if(item.type == ItemID.WallOfFleshBossBag)
+            {
+                itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<CatacombsKey>()));
+            }
+        }
+    }
+}
