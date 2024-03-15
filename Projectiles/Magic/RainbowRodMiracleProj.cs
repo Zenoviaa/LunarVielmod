73f870b3412678aab9a307674f75e163bc1e7318
Scolon <49799704+sscolon@users.noreply.github.com>
2024-03-15 05:30:15 -0400

    added shredder

---
+++ Projectiles/Magic/RainbowRodMiracleProj.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Trails;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Projectiles.Magic
+{
+    internal class RainbowRodMiracleProj : ModProjectile, IPixelPrimitiveDrawer
+    {
+        public override void OnSpawn(IEntitySource source)
+        {
+            for (int i = 0; i < Projectile.oldPos.Length; i++)
+            {
+                Projectile.oldPos[i] = Projectile.position;
+            }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            // Sets the amount of frames this minion has on its spritesheet
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 32;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.friendly = true;
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.timeLeft = 180;
+            Projectile.penetrate = -1;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 10;
+        }
+
+
+        private void AI_Movement(Vector2 targetCenter, float moveSpeed, float accel = 1f)
+        {
+            //This code should give quite interesting movement
+            //Accelerate to being on top of the player
+
+            float distX = targetCenter.X - Projectile.Center.X;
+            if (Projectile.Center.X < targetCenter.X && Projectile.velocity.X < moveSpeed)
+            {
+                Projectile.velocity.X += accel;
+            }
+            else if (Projectile.Center.X > targetCenter.X && Projectile.velocity.X > -moveSpeed)
+            {
+                Projectile.velocity.X -= accel;
+            }
+
+            //Accelerate to being above the player.
+            float distY = targetCenter.Y - Projectile.Center.Y;
+            if (Projectile.Center.Y < targetCenter.Y && Projectile.velocity.Y < moveSpeed)
+            {
+                Projectile.velocity.Y += accel;
+            }
+            else if (Projectile.Center.Y > targetCenter.Y && Projectile.velocity.Y > -moveSpeed)
+            {
+                Projectile.velocity.Y -= accel;
+            }
+        }
+
+        public override void AI()
+        {
+            if(Projectile.owner == Main.myPlayer)
+            {
+                float speed = Projectile.ai[0];
+                AI_Movement(Main.MouseWorld, speed);
+                Projectile.netUpdate = true;
+            }
+
+            //Dunno if this is needed but whatever
+            Projectile.rotation = Projectile.velocity.ToRotation();
+        }
+
+        public float WidthFunction(float completionRatio)
+        {
+            float baseWidth = Projectile.scale * 16;
+            return MathHelper.SmoothStep(baseWidth, 3.5f, completionRatio);
+        }
+
+        public Color ColorFunction(float completionRatio)
+        {
+            Color startColor = Color.White;
+            Color endColor = Color.Transparent;
+            return Color.Lerp(startColor, endColor, completionRatio);
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            return false;
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            //This damages everything in the trail
+            Vector2[] positions = Projectile.oldPos;
+            float collisionPoint = 0;
+            for (int i = 1; i < positions.Length; i++)
+            {
+                Vector2 position = positions[i];
+                Vector2 previousPosition = positions[i - 1];
+                if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), position, previousPosition, 12, ref collisionPoint))
+                    return true;
+            }
+            return base.Colliding(projHitbox, targetHitbox);
+        }
+
+        internal PrimitiveTrail BeamDrawer;
+        public void DrawPixelPrimitives(SpriteBatch spriteBatch)
+        {
+            BeamDrawer ??= new PrimitiveTrail(WidthFunction, ColorFunction, null, true, TrailRegistry.LaserShader);
+
+            TrailRegistry.LaserShader.UseColor(Color.Black);
+            TrailRegistry.LaserShader.SetShaderTexture(TrailRegistry.BeamTrail);
+
+            BeamDrawer.DrawPixelated(Projectile.oldPos, -Main.screenPosition, 32);
+            Main.spriteBatch.ExitShaderRegion();
+        }
+    }
+}
