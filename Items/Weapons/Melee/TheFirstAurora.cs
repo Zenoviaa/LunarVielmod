2b0ee57b8c7bbeee3ca07628b9a8db4f68e811a3
Zenovia <calamityjewelstudios@gmail.com>
2024-03-18 04:08:51 -0400

    More stuff

---
+++ Items/Weapons/Melee/TheFirstAurora.cs
@@ -0,0 +1,90 @@
+﻿
+using Microsoft.Xna.Framework;
+using Stellamod.Helpers;
+using Stellamod.Items.Harvesting;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Ores;
+using Stellamod.Projectiles;
+using Stellamod.Projectiles.Slashers;
+using Stellamod.Projectiles.Slashers.ThefirstAurora;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Weapons.Melee
+{
+    public class TheFirstAurora : ModItem
+    {
+        public int AttackCounter = 1;
+        public int combowombo = 0;
+
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("The Auroran"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            /* Tooltip.SetDefault("Sends shockwaves through the air" +
+                "\nHitting enemies with sword will increase speed!" +
+                "\nDivergency Inspired!"); */
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 20;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 0;
+            Item.height = 0;
+            Item.useTime = 100;
+            Item.useAnimation = 100;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 12;
+            Item.value = 10000;
+            Item.noMelee = true;
+
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<AuroraSProj>();
+            Item.shootSpeed = 1f;
+            Item.noUseGraphic = true;
+            Item.value = Item.sellPrice(0, 2, 50, 0);
+            Item.rare = ItemRarityID.Blue;
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            if (player.GetModPlayer<MyPlayer>().SwordCombo >= 0)
+            {
+                type = ModContent.ProjectileType<AuroraSProj>();
+
+            }
+            if (player.GetModPlayer<MyPlayer>().SwordCombo >= 4)
+            {
+                type = ModContent.ProjectileType<AuroraSProj>();
+                SoundEngine.PlaySound(SoundID.Item34, player.position);
+            }
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+
+            int dir = AttackCounter;
+            if (player.direction == 1)
+            {
+                player.GetModPlayer<CorrectSwing>().SwingChange = AttackCounter;
+            }
+            else
+            {
+                player.GetModPlayer<CorrectSwing>().SwingChange = AttackCounter * -1;
+
+            }
+            AttackCounter = -AttackCounter;
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, 1, dir);
+           // Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<AuroranBullet>(), damage * 2, knockback, player.whoAmI, 1, dir);
+          //  Projectile.NewProjectile(source, position, velocity * 0.8f, ModContent.ProjectileType<AuroranBullet2>(), damage * 2, knockback, player.whoAmI, 1, dir);
+           // Projectile.NewProjectile(source, position, velocity * 1.2f, ModContent.ProjectileType<AuroranBullet3>(), damage * 2, knockback, player.whoAmI, 1, dir);
+            return false;
+        }
+
+        
+    }
+}
---
+++ Trails/IDrawing.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+
+namespace Stellamod.Trails
+{
+    public interface IDrawing
+    {
+        void Draw(Vector2[] arr, float uvAdd = 0f, float uvMultiplier = 1f);
+    }
+    public static class TrailHelper
+    {
+
+        public static float CalcProgress(int length, int i)
+        {
+            return 1f - 1f / length * i;
+        }
+
+        public static int TrailLength(this Projectile projectile)
+        {
+            return ProjectileID.Sets.TrailCacheLength[projectile.type];
+        }
+        public static void GetDrawInfo(this NPC npc, out Texture2D texture, out Vector2 offset, out Rectangle frame, out Vector2 origin, out int trailLength)
+        {
+            texture = TextureAssets.Npc[npc.type].Value;
+            offset = npc.Size / 2f;
+            frame = npc.frame;
+            origin = frame.Size() / 2f;
+            trailLength = NPCID.Sets.TrailCacheLength[npc.type];
+        }
+
+        public static void GetDrawInfo(this Projectile projectile, out Texture2D texture, out Vector2 offset, out Rectangle frame, out Vector2 origin, out int trailLength)
+        {
+            texture = TextureAssets.Projectile[projectile.type].Value;
+            offset = projectile.Size / 2f;
+            frame = projectile.Frame();
+            origin = frame.Size() / 2f;
+            trailLength = NPCID.Sets.TrailCacheLength[projectile.type];
+
+        }
+        public static Rectangle Frame(this Projectile projectile)
+        {
+            return TextureAssets.Projectile[projectile.type].Value.Frame(1, Main.projFrames[projectile.type], 0, projectile.frame);
+        }
+
+    }
+}
