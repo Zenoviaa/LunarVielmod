0a9e8a1c9a5b9b93b9f236537e7f44401cb2da61
Scolon <49799704+sscolon@users.noreply.github.com>
2024-03-17 23:01:01 -0400

    drive round

---
+++ Items/Ammo/DriveRound.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Stellamod.Projectiles.Ammo;
+using Stellamod.Items.Materials.Tech;
+
+namespace Stellamod.Items.Ammo
+{
+    internal class DriveRound : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 99;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 24; // The damage for projectiles isn't actually 12, it actually is the damage combined with the projectile and the item together.
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 8;
+            Item.height = 8;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.consumable = true; // This marks the item as consumable, making it automatically be consumed when it's used as ammunition, or something else, if possible.
+            Item.knockBack = 1.5f;
+            Item.value = 10;
+            Item.rare = ItemRarityID.LightPurple;
+            Item.shoot = ModContent.ProjectileType<DriveRoundProj>(); // The projectile that weapons fire when using this item as ammunition.
+            Item.shootSpeed = 16f; // The speed of the projectile.
+            Item.ammo = AmmoID.Bullet; // The ammo class this ammo belongs to.
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+        {
+            CreateRecipe(100)
+                .AddIngredient(ItemID.MusketBall, 100)
+                .AddIngredient(ModContent.ItemType<MetallicOmniSource>(), 2)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
