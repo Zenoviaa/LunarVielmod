e4d114f9972d55e4063fbb5a83e1ecd1e4c78fc3
Zenovia <calamityjewelstudios@gmail.com>
2024-03-18 22:40:56 -0400

    v

--- Items/Weapons/Melee/Greatswords/SingularDive.cs
+++ Items/Weapons/Melee/Greatswords/SingularDive.cs
@@ -82,56 +82,7 @@ namespace Stellamod.Items.Weapons.Melee.Greatswords
 
 		
 
-		public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
-		{
-			// Draw the periodic glow effect behind the item when dropped in the world (hence PreDrawInWorld)
-			Texture2D texture = TextureAssets.Item[Item.type].Value;
-
-			Rectangle frame;
-
-			if (Main.itemAnimations[Item.type] != null)
-			{
-				// In case this item is animated, this picks the correct frame
-				frame = Main.itemAnimations[Item.type].GetFrame(texture, Main.itemFrameCounter[whoAmI]);
-			}
-			else
-			{
-				frame = texture.Frame();
-			}
-
-			Vector2 frameOrigin = frame.Size() / 2f;
-			Vector2 offset = new Vector2(Item.width / 2 - frameOrigin.X, Item.height - frame.Height);
-			Vector2 drawPos = Item.position - Main.screenPosition + frameOrigin + offset;
-
-			float time = Main.GlobalTimeWrappedHourly;
-			float timer = Item.timeSinceItemSpawned / 240f + time * 0.04f;
-
-			time %= 4f;
-			time /= 2f;
-
-			if (time >= 1f)
-			{
-				time = 2f - time;
-			}
-
-			time = time * 0.5f + 0.5f;
-
-			for (float i = 0f; i < 1f; i += 0.25f)
-			{
-				float radians = (i + timer) * MathHelper.TwoPi;
-
-				spriteBatch.Draw(texture, drawPos + new Vector2(0f, 8f).RotatedBy(radians) * time, frame, new Color(200, 70, 255, 77), rotation, frameOrigin, scale, SpriteEffects.None, 0);
-			}
-
-			for (float i = 0f; i < 1f; i += 0.34f)
-			{
-				float radians = (i + timer) * MathHelper.TwoPi;
-
-				spriteBatch.Draw(texture, drawPos + new Vector2(0f, 4f).RotatedBy(radians) * time, frame, new Color(255, 255, 255, 67), rotation, frameOrigin, scale, SpriteEffects.None, 0);
-			}
-
-			return true;
-		}
+		
 
 		public override bool AltFunctionUse(Player player)
 		{
