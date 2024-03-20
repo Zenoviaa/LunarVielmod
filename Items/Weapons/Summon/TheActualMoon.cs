f404e9c0e2001e37a4c05618d71abf79d6e5e0c5
Scolon <49799704+sscolon@users.noreply.github.com>
2024-03-20 05:31:03 -0400

    THE MOON

---
+++ Items/Weapons/Summon/TheActualMoon.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Buffs.Whipfx;
+using Stellamod.Helpers;
+using Stellamod.Items.Materials;
+using Stellamod.Projectiles.Whips;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Weapons.Summon
+{
+    internal class TheActualMoon : ModItem
+    {
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(AuroreanStarballDebuff.TagDamage);
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+
+            // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+            var line = new TooltipLine(Mod, "", "");
+            line = new TooltipLine(Mod, "Alcarishasd", "Orb Weapon Type")
+            {
+                OverrideColor = ColorFunctions.OrbWeaponType
+            };
+            tooltips.Add(line);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 48;
+            Item.damage = 62;
+            Item.DamageType = DamageClass.Summon;
+            Item.knockBack = 8;
+            Item.useTime = 4;
+            Item.useAnimation = 4;
+            Item.useStyle = ItemUseStyleID.RaiseLamp;
+            Item.value = Item.sellPrice(0, 1, 0, 0);
+            Item.rare = ItemRarityID.Blue;
+
+            // These below are needed for a minion weapon
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.autoReuse = true;
+
+            // No buffTime because otherwise the item tooltip would say something like "1 minute duration"
+            Item.shoot = ModContent.ProjectileType<TheActualMoonProj>();
+            Item.shootSpeed = 1;
+        }
+
+        public override void UpdateInventory(Player player)
+        {
+            base.UpdateInventory(player);
+            if(player.HeldItem.type == ModContent.ItemType<TheActualMoon>() && 
+                player.ownedProjectileCounts[ModContent.ProjectileType<TheActualMoonProj>()]==0)
+            {
+                var projectile = Projectile.NewProjectileDirect(player.GetSource_FromThis(), player.Center, Vector2.Zero, 
+                    ModContent.ProjectileType<TheActualMoonProj>(), Item.damage, Item.knockBack, player.whoAmI);
+                projectile.originalDamage = Item.damage;
+            }
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            for(int i = 0; i < Main.projectile.Length; i++)
+            {
+                if (Main.projectile[i].type == ModContent.ProjectileType<TheActualMoonProj>() && Main.projectile[i].owner == player.whoAmI)
+                {
+                    Main.projectile[i].ai[0]++;
+                    break;
+                }
+            }
+
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(1)
+                 .AddIngredient(ModContent.ItemType<BlankOrb>(), 1)
+                 .AddIngredient(ModContent.ItemType<PearlescentScrap>(), 15)
+                 .AddTile(TileID.Anvils)
+                 .Register();
+
+        }
+    }
+}
---
+++ Items/Weapons/Summon/TheActualMoonProj.cs
@@ -0,0 +1,371 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Buffs.Whipfx;
+using Stellamod.Dusts;
+using Stellamod.Helpers;
+using Stellamod.Projectiles.IgniterExplosions;
+using Stellamod.Trails;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Weapons.Summon
+{
+    internal class TheActualMoonProj : ModProjectile
+    {
+        public enum ActionState
+        {
+            Orbit,
+            Swing_1,
+            Swing_2,
+            Swing_3
+        }
+
+        public const float Swing_Time = 45 * Swing_Speed_Multiplier;
+        public const float Swing_Time_2 = 90 * Swing_Speed_Multiplier;
+        public const float Final_Swing_Distance = 252;
+        public const float Combo_Time = 8;
+        public const int Swing_Speed_Multiplier = 4;
+
+        Player Owner => Main.player[Projectile.owner];
+
+        ref float ComboCounter => ref Projectile.ai[0];
+        public ActionState State
+        {
+            get => (ActionState)Projectile.ai[1];
+            set => Projectile.ai[1] = (float)value;
+        }
+        ref float Timer => ref Projectile.ai[2];
+        float SwingTime;
+        float EasedProgress;
+        Vector2 SwingStart;
+        Vector2 SwingTarget;
+        Vector2 SwingVelocity;
+        int DustTimer;
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 64;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 72;
+            Projectile.height = 72;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.minion = true;
+            Projectile.DamageType = DamageClass.Summon;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = int.MaxValue;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.extraUpdates = Swing_Speed_Multiplier;
+        }
+
+        public override bool? CanDamage()
+        {
+            //Only deal damage while swinging
+            return State != ActionState.Orbit;
+        }
+
+        public override void AI()
+        {
+            //Kill yourself if not holding the item
+            if(Owner.HeldItem.type != ModContent.ItemType<TheActualMoon>())
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            switch (State)
+            {
+                case ActionState.Orbit:
+                    Orbit();
+                    break;
+                case ActionState.Swing_1:
+                    Swing1();
+                    break;
+                case ActionState.Swing_2:
+                    Swing2();
+                    break;
+                case ActionState.Swing_3:
+                    Swing3();
+                    break;
+            }
+        }
+
+        private void Orbit()
+        {
+            //Orbiting
+            float orbitSpeed = 5;
+            float orbitDistance = 48;
+            float orbitProgress = VectorHelper.Osc(0, 1, orbitSpeed);
+            float easedProgress = Easing.InOutCubic(orbitProgress);
+
+            //Hovering
+            float hoverSpeed = 5;
+            float hoverDistance = 24;
+            float yOffset = VectorHelper.Osc(-hoverDistance, hoverDistance, hoverSpeed);
+
+            Vector2 offset = new Vector2(orbitDistance, yOffset);
+            Vector2 startVector = Owner.Center - offset;
+            Vector2 endVector = Owner.Center + offset;
+
+            //Lerp
+            Vector2 targetCenter = Vector2.Lerp(startVector, endVector, easedProgress);
+            Projectile.Center = Vector2.Lerp(Projectile.Center, targetCenter, 0.12f / Swing_Speed_Multiplier);
+            if (ComboCounter >= 1)
+            {
+                switch (Main.rand.Next(2))
+                {
+                    case 0:
+                        SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/SoftSummon"), Projectile.position);
+                        break;
+                    case 1:
+                        SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/SoftSummon2"), Projectile.position);
+                        break;
+                }
+
+                Reset();
+                SwingVelocity = Owner.DirectionTo(SwingTarget);
+                SwingStart = Owner.Center;
+                SwingTarget = Main.MouseWorld;
+                SwingTime = Swing_Time;
+                State = ActionState.Swing_1;
+                ComboCounter = 0;
+                Timer = 0;
+            }
+        }
+
+        private void Reset()
+        {
+            EasedProgress = 0;
+            for(int i = 0; i < Projectile.localNPCImmunity.Length; i++)
+            {
+                Projectile.localNPCImmunity[i] = 0;
+            }
+        }
+
+        private void SwingDusts()
+        {
+            DustTimer++;
+            if (DustTimer >= 4 * Swing_Speed_Multiplier)
+            {
+                DustTimer = 0;
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height,
+                    ModContent.DustType<GunFlash>(), newColor: new Color(69, 43, 149), Scale: 0.8f);
+            }
+        }
+
+        private void Swing1()
+        {
+            SwingDusts();
+            Timer++;
+
+            float progress = Timer / SwingTime;
+            EasedProgress = Easing.SpikeOrb(progress);
+            float rot = MathHelper.Lerp(-MathHelper.Pi, 0, EasedProgress);
+
+            Vector2 start = SwingStart;
+            Vector2 end = SwingTarget.RotatedBy(rot, SwingStart);
+            Vector2 lerpPosition = Vector2.Lerp(start, end, EasedProgress);
+
+            Projectile.Center = Vector2.Lerp(Projectile.Center, lerpPosition, 0.54f / Swing_Speed_Multiplier);
+            if (Timer > SwingTime)
+            {
+                if (ComboCounter >= 1)
+                {
+                    switch (Main.rand.Next(2))
+                    {
+                        case 0:
+                            SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/SoftSummon"), Projectile.position);
+                            break;
+                        case 1:
+                            SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/SoftSummon2"), Projectile.position);
+                            break;
+                    }
+
+                    Reset();
+                    SwingVelocity = Owner.DirectionTo(SwingTarget);
+                    float distance = 180;
+                    SwingStart = Owner.Center + SwingVelocity.RotatedByRandom(MathHelper.TwoPi) * distance;
+                    SwingTarget = Main.MouseWorld;
+                    SwingTime = Swing_Time;
+                    State = ActionState.Swing_2;
+                    ComboCounter = 0;
+                    Timer = 0;
+                }
+                else if (Timer > SwingTime + Combo_Time)
+                {
+                    ComboCounter = 0;
+                    Timer = 0;
+                    State = ActionState.Orbit;
+                }
+            }
+        }
+
+        private void Swing2()
+        {
+            SwingDusts();
+            Timer++;
+
+
+            float progress = Timer / SwingTime;
+            EasedProgress = Easing.SpikeOrb(progress);
+            float rot = MathHelper.Lerp(MathHelper.Pi, 0, EasedProgress);
+
+            Vector2 start = SwingStart;
+            Vector2 end = SwingTarget.RotatedBy(rot, SwingStart);
+            Vector2 lerpPosition = Vector2.Lerp(start, end, EasedProgress);
+
+            Projectile.Center = Vector2.Lerp(Projectile.Center, lerpPosition, 0.54f / Swing_Speed_Multiplier);
+            if (Timer > SwingTime)
+            {
+                if (ComboCounter >= 1)
+                {
+                    switch (Main.rand.Next(2))
+                    {
+                        case 0:
+                            SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/StarFlower2") { Pitch = -0.5f }, Projectile.position);
+                            break;
+                        case 1:
+                            SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/StarFlower2") { Pitch = -0.5f }, Projectile.position);
+                            break;
+                    }
+
+                    Reset();
+                    SwingVelocity = Owner.DirectionTo(SwingTarget);
+                    SwingStart = Projectile.Center;
+                    SwingTarget = Main.MouseWorld;
+                    SwingTime = Swing_Time_2;
+                    State = ActionState.Swing_3;
+                    ComboCounter = 0;
+                    Timer = 0;
+                }
+                else if (Timer > SwingTime + Combo_Time)
+                {
+                    ComboCounter = 0;
+                    Timer = 0;
+                    State = ActionState.Orbit;
+                }
+            }
+        }
+
+        private void Swing3()
+        {
+            SwingDusts();
+            Timer++;
+            float progress = Timer / SwingTime;
+            EasedProgress = Easing.SpikeOrb(progress);
+
+            Vector2 start = Owner.Center;
+            Vector2 end = SwingTarget + (SwingTarget - start).SafeNormalize(Vector2.Zero) * Final_Swing_Distance;
+            Vector2 lerpPosition = Vector2.Lerp(start, end, EasedProgress);
+
+            Projectile.Center = Vector2.Lerp(Projectile.Center, lerpPosition, 0.54f / Swing_Speed_Multiplier);
+            if (Timer > SwingTime)
+            {
+                ComboCounter = 0;
+                Timer = 0;
+                State = ActionState.Orbit;
+            }
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            base.OnHitNPC(target, hit, damageDone);
+            target.AddBuff(ModContent.BuffType<AuroreanStarballDebuff>(), 240);
+            switch (State)
+            {
+                case ActionState.Swing_1:
+                case ActionState.Swing_2:
+                    for (int i = 0; i < 4; i++)
+                    {
+                        Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 
+                            ModContent.DustType<GunFlash>(), newColor: new Color(85, 112, 188), Scale: 0.8f);
+                    }
+
+                    switch (Main.rand.Next(3))
+                    {
+                        case 0:
+                            SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/Astalaiya1") { PitchVariance = 0.15f }, Projectile.position);
+                            break;
+                        case 1:
+                            SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/Astalaiya2") { PitchVariance = 0.15f }, Projectile.position);
+                            break;
+                        case 2:
+                            SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/Astalaiya3") { PitchVariance = 0.15f }, Projectile.position);
+                            break;
+                    }
+                    break;
+
+                case ActionState.Swing_3:
+                    
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<StarsBoom>(),
+                        Projectile.damage, Projectile.knockBack, Projectile.owner);
+
+                    target.SimpleStrikeNPC(Projectile.damage, hit.HitDirection);
+                    target.SimpleStrikeNPC(Projectile.damage, hit.HitDirection);
+
+                    //Funny Screenshake
+                    Main.LocalPlayer.GetModPlayer<MyPlayer>().ShakeAtPosition(Projectile.Center, 1024f, 64);
+                    SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/StarFlower3") { PitchVariance = 0.15f }, Projectile.position);
+                    break;
+            }            
+        }
+
+        public TrailRenderer SwordSlash;
+        public TrailRenderer SwordSlash2;
+        public TrailRenderer SwordSlash3;
+
+        public override bool PreDraw(ref Color lightColor)
+        {  
+            var TrailTex = ModContent.Request<Texture2D>("Stellamod/Effects/Primitives/Trails/StarTrail").Value;
+            var TrailTex2 = ModContent.Request<Texture2D>("Stellamod/Effects/Primitives/Trails/StringTrail").Value;
+            var TrailTex3 = ModContent.Request<Texture2D>("Stellamod/Effects/Primitives/Trails/CrystalTrail").Value;
+            Color color = Color.Multiply(new(1.50f, 1.75f, 3.5f, 0), 200);
+            if (SwordSlash == null)
+            {
+                SwordSlash = new TrailRenderer(TrailTex, TrailRenderer.DefaultPass,
+                    (p) => Vector2.Lerp(new Vector2(150), new Vector2(128), p), 
+                    (p) => new Color(230, 255, 255, 125) * (1f - p));
+                SwordSlash.drawOffset = Projectile.Size / 2f;
+            }
+            if (SwordSlash2 == null)
+            {
+                SwordSlash2 = new TrailRenderer(TrailTex2, TrailRenderer.DefaultPass, 
+                    (p) => Vector2.Lerp(new Vector2(75), new Vector2(75), p), 
+                    (p) => new Color(247, 178, 239, 125) * (1f - p));
+                SwordSlash2.drawOffset = Projectile.Size / 2f;
+            }
+            if (SwordSlash3 == null)
+            {
+                SwordSlash3 = new TrailRenderer(TrailTex3, TrailRenderer.DefaultPass,
+                    (p) => Vector2.Lerp(new Vector2(30), new Vector2(30), p),
+                    (p) => new Color(69, 93, 149, 125) * (1f - p));
+                SwordSlash3.drawOffset = Projectile.Size / 2f;
+            }
+
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Texture, null, null, null, null, null, Main.GameViewMatrix.ZoomMatrix);
+
+
+            SwordSlash.Draw(Projectile.oldPos, Projectile.oldRot);
+            SwordSlash2.Draw(Projectile.oldPos, Projectile.oldRot);
+            SwordSlash3.Draw(Projectile.oldPos, Projectile.oldRot);
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin();
+
+
+            
+            DrawHelper.DrawAdditiveAfterImage(Projectile, new Color(85, 112, 188) * 0.3f, Color.Transparent, ref lightColor);
+            return base.PreDraw(ref lightColor);
+        }
+    }
+}
