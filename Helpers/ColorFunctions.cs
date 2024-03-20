94d1c34b96bb8a1299d50d9964b74dbf138bf2ab
Zenovia <calamityjewelstudios@gmail.com>
2024-03-20 06:19:05 -0400

    as

--- Helpers/ColorFunctions.cs
+++ Helpers/ColorFunctions.cs
@@ -7,7 +7,7 @@ namespace Stellamod.Helpers
     {
         public static Color AcidFlame => new Color(24, 142, 61);
         public static Color MiracleVoid => new Color(60, 0 , 118);
-        public static Color OrbWeaponType => new Color(0, Main.DiscoG, 150);
+        public static Color OrbWeaponType => new Color(0, Main.DiscoG, 150, 0f);
 
         public static Color SteinWeaponType => new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB, 0f);
 
