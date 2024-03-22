623b909921ec3172aca367383083a5daf3f4820c
Zenovia <calamityjewelstudios@gmail.com>
2024-03-22 05:09:01 -0400

    fake af

---
+++ Tiles/IlluriaDirtWall.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.Xna.Framework;
+using Stellamod.Dusts;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Tiles
+{
+	public class IlluriaDirtWall : ModWall
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.wallHouse[Type] = false;
+
+			DustType = ModContent.DustType<Sparkle>();
+			RegisterItemDrop(ModContent.ItemType<Items.Materials.IlluriaDirtWallBlock>());
+			AddMapEntry(new Color(200, 200, 200));
+		}
+		public override void NumDust(int i, int j, bool fail, ref int num)
+		{
+			num = fail ? 1 : 3;
+		}
+		public override bool CanExplode(int i, int j) => true;
+	}
+}
