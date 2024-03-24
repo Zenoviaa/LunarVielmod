e3860b324101ff94672a9f26dd281d0a4835686f
Zenovia <calamityjewelstudios@gmail.com>
2024-03-24 15:58:44 -0400

    sa

--- Items/Weapons/Melee/Curlistine.cs
+++ Items/Weapons/Melee/Curlistine.cs
@@ -12,7 +12,7 @@ namespace Stellamod.Items.Weapons.Melee
     public class Curlistine : ClassSwapItem
     {
         //Alternate class you want it to change to
-        public override DamageClass AlternateClass => DamageClass.Throwing;
+        public override DamageClass AlternateClass => DamageClass.Magic;
 
         //Defaults for the other class
         public override void SetClassSwappedDefaults()
