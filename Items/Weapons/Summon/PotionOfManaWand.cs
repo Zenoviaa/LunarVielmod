b8dc3f46a06d8a703fac098de1508ab1de106a84
Scolon <49799704+sscolon@users.noreply.github.com>
2024-03-29 22:50:14 -0400

    potion of life/mana wand balancing and visual

--- Items/Weapons/Summon/PotionOfManaWand.cs
+++ Items/Weapons/Summon/PotionOfManaWand.cs
@@ -46,7 +46,7 @@ namespace Stellamod.Items.Weapons.Summon
 			Item.height = 20;
 			Item.rare = ItemRarityID.Blue;
 			Item.value = Terraria.Item.sellPrice(0, 10, 80, 0);
-			Item.damage = 4; // Sets the Item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+			
 			Item.DamageType = DamageClass.Summon;
 			Item.mana = 20;
 			Item.useTime = 30; // The Item's use time in ticks (60 ticks == 1 second.)
