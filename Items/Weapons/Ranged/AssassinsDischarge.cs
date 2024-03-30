398c65092f8896520dc967cfa9f8a25cb2034cb4
Zenovia <calamityjewelstudios@gmail.com>
2024-03-29 22:27:12 -0400

    asss

--- Items/Weapons/Ranged/AssassinsDischarge.cs
+++ Items/Weapons/Ranged/AssassinsDischarge.cs
@@ -11,7 +11,7 @@ namespace Stellamod.Items.Weapons.Ranged
     {
         public override void SetDefaults()
         {
-            Item.damage = 13;
+            Item.damage = 11;
             Item.width = 50;
             Item.height = 50;
             Item.useStyle = ItemUseStyleID.Shoot;
