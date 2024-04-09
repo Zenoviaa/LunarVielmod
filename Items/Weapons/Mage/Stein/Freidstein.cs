fbe2bad7f1913396952ff23730fabb0d0f7f30b0
Zenovia <calamityjewelstudios@gmail.com>
2024-04-09 03:15:56 -0400

    as

--- Items/Weapons/Mage/Stein/Freidstein.cs
+++ Items/Weapons/Mage/Stein/Freidstein.cs
@@ -37,7 +37,7 @@ namespace Stellamod.Items.Weapons.Mage.Stein
 		}
 		public override void SetDefaults()
 		{
-			Item.damage = 34;
+			Item.damage = 40;
 			Item.DamageType = DamageClass.Magic;
 			Item.width = 0;
 			Item.height = 0;
