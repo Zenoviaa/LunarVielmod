763a5ecdb9644ee7c92e8255102a6611ccd97590
Zenovia <calamityjewelstudios@gmail.com>
2024-04-09 02:46:17 -0400

    Finished Steins

--- Items/Weapons/Mage/Stein/Iknoctstein.cs
+++ Items/Weapons/Mage/Stein/Iknoctstein.cs
@@ -36,7 +36,7 @@ namespace Stellamod.Items.Weapons.Mage.Stein
 		}
 		public override void SetDefaults()
 		{
-			Item.damage = 62;
+			Item.damage = 70;
 			Item.DamageType = DamageClass.Magic;
 			Item.width = 0;
 			Item.height = 0;
