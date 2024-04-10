1b127f552fc80b415644845236718c01eaf917f0
Scolon <49799704+sscolon@users.noreply.github.com>
2024-04-10 04:12:26 -0400

    added polaris

---
+++ Items/Weapons/Ranged/Polaris.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Stellamod.Helpers;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Items.Materials;
+using Microsoft.Xna.Framework;
+using Stellamod.Projectiles.Gun;
+
+namespace Stellamod.Items.Weapons.Ranged
+{
+    internal class Polaris : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Wooden Crossbow"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            /* Tooltip.SetDefault("Use a small crossbow and shoot three bolts!"
+                + "\n'Triple Threat!'"); */
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            ItemID.Sets.ItemNoGravity[Item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 3333;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 32;
+            Item.height = 25;
+            Item.useTime = 45;
+            Item.useAnimation = 45;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 2;
+            Item.rare = ModContent.RarityType<NiiviSpecialRarity>();
+            Item.autoReuse = false;
+            Item.shootSpeed = 30f;
+            Item.shoot = ModContent.ProjectileType<PolarisHold>();
+            Item.scale = 0.8f;
+            Item.noMelee = true; // The projectile will do the damage and not the item
+            Item.value = Item.buyPrice(silver: 12);
+            Item.noUseGraphic = true;
+            Item.channel = true;
+        }
+
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<PureHeart>(), 1);
+            recipe.AddIngredient(ItemID.FragmentVortex, 12);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.Register();
+        }
+
+
+        public override void PostUpdate()
+        {
+            Lighting.AddLight(Item.Center, Color.WhiteSmoke.ToVector3() * 0.55f * Main.essScale); // Makes this item glow when thrown out of inventory.
+        }
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            DrawHelper.DrawGlowInInventory(Item, spriteBatch, position, ColorFunctions.Niivin);
+            return true;
+        }
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            DrawHelper.DrawGlow2InWorld(Item, spriteBatch, ref rotation, ref scale, whoAmI);
+            return true;
+        }
+
+        public override void Update(ref float gravity, ref float maxFallSpeed)
+        {
+            //The below code makes this item hover up and down in the world
+            //Don't forget to make the item have no gravity, otherwise there will be weird side effects
+            float hoverSpeed = 5;
+            float hoverRange = 0.2f;
+            float y = VectorHelper.Osc(-hoverRange, hoverRange, hoverSpeed);
+            Vector2 position = new Vector2(Item.position.X, Item.position.Y + y);
+            Item.position = position;
+        }
+    }
+}
---
+++ Projectiles/Gun/PolarisLaserProj.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Dusts;
+using Stellamod.Helpers;
+using Stellamod.NPCs.Bosses.Sylia;
+using Stellamod.Projectiles.IgniterExplosions;
+using Stellamod.Trails;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace Stellamod.Projectiles.Gun
+{
+    internal class PolarisLaserProj : ModProjectile,
+        IPixelPrimitiveDrawer
+    {
+        //Don't change the sample points, 3 is good enough
+        private const int NumSamplePoints = 3;
+
+        private const float MaxBeamLength = 2400f;
+
+        public float BeamLength;
+        public List<Vector2> BeamPoints;
+        internal PrimitiveTrail BeamDrawer;
+
+        //No texture for this
+        public override string Texture => TextureRegistry.EmptyTexture;
+
+        ref float Size => ref Projectile.ai[0];
+        float Timer;
+        public override void SetDefaults()
+        {
+            Projectile.width = 32;
+            Projectile.height = 16;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.tileCollide = true;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 60;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 25;
+            BeamPoints = new List<Vector2>();
+        }
+
+        public override void AI()
+        {
+            float targetBeamLength = PerformBeamHitscan();
+            BeamLength = targetBeamLength;
+            Timer++;
+            if(Timer == 1)
+            {
+                switch (Main.rand.Next(2))
+                {
+                    case 0:
+                        SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/M38F30Bomb1"), Projectile.position);
+                        break;
+                    case 1:
+                        SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/M38F30Bomb2"), Projectile.position);
+                        break;
+                }
+
+                for (int i = 0; i < 14; i++)
+                {
+                    Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<GlowDust>(), (Vector2.One * Main.rand.Next(1, 5)).RotatedByRandom(19.0), 0, Color.LightSkyBlue, 1f).noGravity = true;
+                }
+
+                Vector2 direction = Projectile.velocity.SafeNormalize(Vector2.Zero);
+                Vector2 explosionCenter = Projectile.Center + direction * BeamLength;
+                Main.LocalPlayer.GetModPlayer<MyPlayer>().ShakeAtPosition(explosionCenter, 1024f, 32f);
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), explosionCenter, Vector2.Zero,
+                    ModContent.ProjectileType<SiriusBoom>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            return false;
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            float _ = 0f;
+            float width = Projectile.width * 0.8f * Size;
+            Vector2 start = Projectile.Center;
+
+            Vector2 direction = Projectile.velocity.SafeNormalize(Vector2.Zero);
+            Vector2 end = start + direction * (BeamLength - 80f);
+            return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), start, end, width, ref _);
+        }
+
+        private float PerformBeamHitscan()
+        {
+            // By default, the hitscan interpolation starts at the Projectile's center.
+            // If the host Prism is fully charged, the interpolation starts at the Prism's center instead.
+            Vector2 samplingPoint = Projectile.Center;
+
+            // Perform a laser scan to calculate the correct length of the beam.
+            // Alternatively, if you want the beam to ignore tiles, just set it to be the max beam length with the following line.
+            // return MaxBeamLength;
+            float[] laserScanResults = new float[NumSamplePoints];
+
+
+            Vector2 direction = Projectile.velocity.SafeNormalize(Vector2.Zero);
+            Collision.LaserScan(samplingPoint, direction, 0 * Projectile.scale, MaxBeamLength, laserScanResults);
+            float averageLengthSample = 0f;
+            for (int i = 0; i < laserScanResults.Length; ++i)
+            {
+                averageLengthSample += laserScanResults[i];
+            }
+            averageLengthSample /= NumSamplePoints;
+            return averageLengthSample;
+        }
+
+
+        public float WidthFunction(float completionRatio)
+        {
+            float osc = VectorHelper.Osc(0.75f, 1f);
+
+            float width = (float)Projectile.timeLeft / 60f;
+            return (Projectile.width * Projectile.scale) * osc * width * Size;
+        }
+
+        public Color ColorFunction(float completionRatio)
+        {
+            Color color = Color.Lerp(Color.LightCyan, Color.White, VectorHelper.Osc(0, 1));
+            return color;
+        }
+
+        public override bool PreDraw(ref Color lightColor) => false;
+        public override bool ShouldUpdatePosition() => false;
+        public void DrawPixelPrimitives(SpriteBatch spriteBatch)
+        {
+            BeamDrawer ??= new PrimitiveTrail(WidthFunction, ColorFunction, null, true, TrailRegistry.LaserShader);
+
+            TrailRegistry.LaserShader.UseColor(Color.White);
+            TrailRegistry.LaserShader.SetShaderTexture(TrailRegistry.BeamTrail);
+
+            //Put in the points
+            //This is just a straight beam that collides with tiles
+            BeamPoints.Clear();
+            Vector2 direction = Projectile.velocity.SafeNormalize(Vector2.Zero);
+            for (int i = 0; i <= 8; i++)
+            {
+                BeamPoints.Add(Vector2.Lerp(Projectile.Center, Projectile.Center + direction * BeamLength, i / 8f));
+            }
+
+            BeamDrawer.DrawPixelated(BeamPoints, -Main.screenPosition, 32);
+            Main.spriteBatch.ExitShaderRegion();
+        }
+    }
+}
