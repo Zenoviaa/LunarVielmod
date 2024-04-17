ca9e41204a6dc7ca8f734bc3abdc09b81ea342c3
Scolon <49799704+sscolon@users.noreply.github.com>
2024-04-17 16:33:03 -0400

    tree over 3

---
+++ Tiles/Ambient/TreeOver3.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Stellamod.Tiles.Ambient
+{
+    internal class TreeOver3 : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+            TileObjectData.newTile.Height = 7;
+            TileObjectData.newTile.Width = 11;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16, 16, 16, 16 };
+            TileObjectData.addTile(Type);
+            LocalizedText name = CreateMapEntryName();
+            AddMapEntry(new Color(200, 200, 200), name);
+        }
+    }
+}
