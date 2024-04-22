3dbb513f679e3a5e7fc0f99d783772020025801d
Scolon <49799704+sscolon@users.noreply.github.com>
2024-04-22 02:44:33 -0400

    black shader

--- NPCs/Bosses/Sylia/SyliaAIPhase2Transition.cs
+++ NPCs/Bosses/Sylia/SyliaAIPhase2Transition.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using ParticleLibrary;
+using Stellamod.Helpers;
 using Stellamod.NPCs.Bosses.Sylia.Projectiles;
 using Stellamod.Particles;
 using Stellamod.UI.Systems;
 using Terraria;
 using Terraria.Audio;
+using Terraria.Graphics.Effects;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -17,10 +19,16 @@ namespace Stellamod.NPCs.Bosses.Sylia
             AttackCycle = 0;
             Timer++;
             Main.LocalPlayer.GetModPlayer<MyPlayer>().FocusOn(NPC.Center, 1f);
+            FilterManager filterManager = Terraria.Graphics.Effects.Filters.Scene;
+            filterManager[ShaderRegistry.Screen_Black].GetShader()
+             .UseProgress(1f - (Timer / 240));
+            filterManager.Activate(ShaderRegistry.Screen_Black);
 
             //Kill the barrier
             if (Timer == 1)
             {
+   
+
                 DrawMagicCircle = true;
                 SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/SyliaTransition"));
                 for (int i = 0; i < Main.maxProjectiles; i++)
