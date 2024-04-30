c3cd4adacbef20bdea14ab2d11c4932fb85b84b4
Zenovia <calamityjewelstudios@gmail.com>
2024-04-30 04:24:45 -0400

    Wayyy too good

--- Helpers/TextureRegistry.cs
+++ Helpers/TextureRegistry.cs
@@ -8,5 +8,7 @@
         public static string NormalNoise1 => "Stellamod/Textures/NormalNoise1";
         public static string ZuiEffect => "Stellamod/Effects/Masks/ZuiEffect";
         public static string VoxTexture3 => "Stellamod/Assets/Effects/VoxTexture3";
+
+        public static string VoxTexture4 => "Stellamod/Assets/Effects/VoxTexture5";
     }
 }
