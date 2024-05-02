2eac32566eabb49d34d0e0e61a62836b36c23845
Scolon <49799704+sscolon@users.noreply.github.com>
2024-05-01 21:37:04 -0400

    add irradia axe

---
+++ NPCs/Bosses/IrradiaNHavoc/Projectiles/IrradiaAxeFlameArrow.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Trails;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+
+namespace Stellamod.NPCs.Bosses.IrradiaNHavoc.Projectiles
+{
+    internal class IrradiaAxeFlameArrow : ModProjectile
+    {
+        bool Moved;
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Heat Arrow");
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 12;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if(Main.myPlayer == Projectile.owner)
+            {
+                if (Main.rand.NextBool(2))
+                    target.AddBuff(BuffID.OnFire, 180);
+            }
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 9;
+            Projectile.height = 17;
+            Projectile.knockBack = 12.9f;
+            Projectile.aiStyle = 1;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.hostile = true;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = 180;
+            AIType = ProjectileID.Bullet;
+        }
+
+        public override void AI()
+        {
+            Projectile.ai[1]++;
+            if (!Moved && Projectile.ai[1] >= 0)
+            {
+                Projectile.spriteDirection = Projectile.direction;
+                Projectile.rotation = Projectile.velocity.ToRotation() + 1.57f + 3.14f;
+                for (int j = 0; j < 10; j++)
+                {
+                    Vector2 vector2 = Vector2.UnitX * -Projectile.width / 2f;
+                    vector2 += -Vector2.UnitY.RotatedBy(j * 3.141591734f / 6f, default) * new Vector2(8f, 16f);
+                    vector2 = vector2.RotatedBy(Projectile.rotation - 1.57079637f, default);
+                    int num8 = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.GoldCoin, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
+                    Main.dust[num8].scale = 1.3f;
+                    Main.dust[num8].noGravity = true;
+                    Main.dust[num8].position = Projectile.Center + vector2;
+                    Main.dust[num8].velocity = Projectile.velocity * 0.1f;
+                    Main.dust[num8].noLight = true;
+                    Main.dust[num8].velocity = Vector2.Normalize(Projectile.Center - Projectile.velocity * 3f - Main.dust[num8].position) * 1.25f;
+                }
+                Moved = true;
+            }
+        }
+        public override void PostDraw(Color lightColor)
+        {
+            Texture2D texture2D4 = Request<Texture2D>("Stellamod/Effects/Masks/DimLight").Value;
+            if (Main.rand.NextBool(5))
+            {
+                int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GoldCoin, 0f, 0f, 150, Color.White, 1f);
+                Main.dust[dustnumber].velocity *= 0.3f;
+            }
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(85f * 1), (int)(45f * 1), (int)(15f * 1), 0), Projectile.rotation, new Vector2(32, 32), 0.17f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(85f * 1), (int)(45f * 1), (int)(15f * 1), 0), Projectile.rotation, new Vector2(32, 32), 0.17f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, new Color((int)(85f * 1), (int)(45f * 1), (int)(15f * 1), 0), Projectile.rotation, new Vector2(32, 32), 0.07f * (7 + 0.6f), SpriteEffects.None, 0f);
+            Lighting.AddLight(Projectile.Center, Color.Yellow.ToVector3() * 1.0f * Main.essScale);
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            Main.LocalPlayer.GetModPlayer<MyPlayer>().ShakeAtPosition(Projectile.Center, 512f, 32f);
+            for (int i = 0; i < 20; i++)
+            {
+                Dust.NewDustPerfect(Projectile.Center, DustID.GoldCoin, (Vector2.One * Main.rand.Next(1, 5)).RotatedByRandom(25.0), 0, default, 1f).noGravity = false;
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                int num = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GoldCoin, 0f, -2f, 0, default, 1.5f);
+                Main.dust[num].noGravity = true;
+                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                {
+                    Main.dust[num].velocity = Projectile.DirectionTo(Main.dust[num].position) * 6f;
+                }
+            }
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+
+        public PrimDrawer TrailDrawer { get; private set; } = null;
+        public float WidthFunction(float completionRatio)
+        {
+            float baseWidth = Projectile.scale * Projectile.width * 1.0f;
+            return MathHelper.SmoothStep(baseWidth, 0.35f, completionRatio);
+        }
+        public Color ColorFunction(float completionRatio)
+        {
+            return Color.Lerp(Color.LightYellow, Color.Orange, completionRatio) * 0.7f;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            if (Main.rand.NextBool(5))
+            {
+                int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.CopperCoin, 0f, 0f, 150, Color.White, 1f);
+                Main.dust[dustnumber].velocity *= 0.3f;
+                Main.dust[dustnumber].noGravity = true;
+            }
+            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, new Vector2(texture.Width / 2, texture.Height / 2), 1f, Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0f);
+            TrailDrawer ??= new PrimDrawer(WidthFunction, ColorFunction, GameShaders.Misc["VampKnives:BasicTrail"]);
+            GameShaders.Misc["VampKnives:BasicTrail"].SetShaderTexture(TrailRegistry.SmallWhispyTrail);
+            TrailDrawer.DrawPrims(Projectile.oldPos, Projectile.Size * 0.5f - Main.screenPosition, 155);
+
+            return false;
+        }
+    }
+}
