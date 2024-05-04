7f53a00cbe5af1a180bc4d3dbb81de769d02f1a3
Zenovia <calamityjewelstudios@gmail.com>
2024-05-04 03:20:15 -0400

    woopa

--- Assets/Biomes/VeilBackgroundStyle.cs
+++ Assets/Biomes/VeilBackgroundStyle.cs
@@ -30,7 +30,7 @@ namespace Stellamod.Backgrounds
 
 		public override int ChooseFarTexture()
 		{
-			return BackgroundTextureLoader.GetBackgroundSlot("Stellamod/Assets/Textures/Backgrounds/FableBiomeBackgroundFlat");
+			return BackgroundTextureLoader.GetBackgroundSlot("Stellamod/Assets/Textures/Backgrounds/IceyBiomeBackground");
 			
 		}
 
