56b5eba660eb8e29eebf365fd0c248b58ede1694
Zenovia <calamityjewelstudios@gmail.com>
2024-05-06 23:00:22 -0400

    gonw

---
+++ Projectiles/Powders/MushyPowderProj.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using ParticleLibrary;
+using Stellamod.Buffs;
+using Stellamod.Buffs.Dusteffects;
+using Stellamod.Particles;
+using Stellamod.Projectiles.IgniterExplosions;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Projectiles.Powders
+{
+	public class MushyPowderProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("Powdered Flame");
+
+		}
+		public override void SetDefaults()
+		{
+			Projectile.width = 30;
+			Projectile.height = 30;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+			Projectile.timeLeft = 45;
+			Projectile.ignoreWater = true;
+		}
+		public override void AI()
+		{
+
+			Projectile.velocity *= 0.96f;
+			for (int j = 0; j < 1; j++)
+			{
+				Vector2 speed = Main.rand.NextVector2Circular(0.5f, 0.5f);
+				ParticleManager.NewParticle(Projectile.Center, speed * 4, ParticleManager.NewInstance<LeafParticle>(), Color.RosyBrown, Main.rand.NextFloat(0.2f, 0.8f));
+
+
+			}
+		}
+		public override bool PreAI()
+		{
+			Projectile.tileCollide = false;
+			int dust = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f);
+			Main.dust[dust].scale = 1.4f;
+			int dust2 = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.OrangeTorch, 0f, 0f);
+			Main.dust[dust2].scale = 0.8f;
+
+
+			return true;
+		}
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+		{
+			Player player = Main.player[Projectile.owner];
+			player.AddBuff(ModContent.BuffType<UseIgniter>(), 720);
+			target.AddBuff(ModContent.BuffType<Dusted>(), 720);
+			target.AddBuff(ModContent.BuffType<MushyBuff>(), 720);
+			base.OnHitNPC(target, hit, damageDone);
+		}
+	}
+}
