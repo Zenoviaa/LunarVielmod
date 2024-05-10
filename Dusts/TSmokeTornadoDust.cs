a5bc86b081e4d0d1a939bf2e77ab1c4f3afa0003
Scolon <49799704+sscolon@users.noreply.github.com>
2024-05-10 04:45:53 -0400

    tornado visual

--- Dusts/TSmokeTornadoDust.cs
+++ Dusts/TSmokeTornadoDust.cs
@@ -4,7 +4,7 @@ using Terraria.ModLoader;
 
 namespace Stellamod.Dusts
 {
-	public class TSmokeDust : ModDust
+	public class TSmokeTornadoDust : ModDust
 	{
         public override void OnSpawn(Dust dust)
         {
@@ -12,35 +12,28 @@ namespace Stellamod.Dusts
             dust.scale *= Main.rand.NextFloat(0.8f, 2f);
             dust.frame = new Rectangle(0, Main.rand.Next(2) * 32, 32, 32);
             dust.rotation = Main.rand.NextFloat(6.28f);
+            dust.alpha = 100;
+        //    dust.scale = 0.3f;
         }
 
         public override Color? GetAlpha(Dust dust, Color lightColor)
         {
             var gray = new Color(25, 25, 25);
             Color black = Color.Black;
-            Color ret;
-
-            if (dust.alpha < 120)
-                ret = Color.Lerp(dust.color, gray, dust.alpha / 120f);
-            else if (dust.alpha < 180)
-                ret = Color.Lerp(gray, black, (dust.alpha - 120) / 60f);
-            else
-                ret = black;
-
+            Color ret = dust.color;
+            dust.color = Color.Lerp(dust.color, Color.Black, 0.005f);
             return ret * ((255 - dust.alpha) / 255f);
         }
 
         public override bool Update(Dust dust)
         {
-            dust.velocity = dust.velocity.RotatedBy(MathHelper.PiOver4 / 16);
-            dust.velocity *= 0.98f;
-            dust.velocity.X *= 0.95f;
-            dust.velocity.Y *= 0.97f;
+            dust.velocity = dust.velocity.RotatedBy(MathHelper.PiOver4 / 4);
+            dust.position.Y -= 10 * dust.scale * 0.25f;
             dust.color *= 0.98f;
 
             if (dust.alpha > 100)
             {
-                dust.scale *= 0.975f;
+                dust.scale *= 1.05f;
                 dust.alpha += 2;
             }
             else
