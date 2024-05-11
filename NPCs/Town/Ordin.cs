c4043915a658cf71d4b4453f379760059eceab47
Scolon <49799704+sscolon@users.noreply.github.com>
2024-05-11 13:13:54 -0400

    added vixyl

--- NPCs/Town/Ordin.cs
+++ NPCs/Town/Ordin.cs
@@ -378,11 +378,15 @@ namespace Stellamod.NPCs.Town
 					{
 						shopCustomPrice = 3,
 						shopSpecialCurrency = Stellamod.MOCCurrencyID // omit this line if shopCustomPrice should be in regular coins.
-					});
+					})
+					.Add(new Item(ModContent.ItemType<Vixyl>())
+                     {
+                         shopCustomPrice = 3,
+                         shopSpecialCurrency = Stellamod.MOBCurrencyID // omit this line if shopCustomPrice should be in regular coins.
+                     });
 
-					
 
-			npcShop.Register(); // Name of this shop tab
+            npcShop.Register(); // Name of this shop tab
 
 
 
---
+++ Projectiles/Slashers/Vixyl/VixylSlashProj.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Helpers;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Projectiles.Slashers.Vixyl
+{
+    internal class VixylSlashProj : ModProjectile
+    {
+        private int _frameCounter;
+        private int _frameTick;
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 10;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 300;
+            Projectile.height = 300;
+            Projectile.tileCollide = false;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 30;
+            Projectile.localNPCHitCooldown = 3;
+            Projectile.usesLocalNPCImmunity = true;
+        }
+
+        public float Timer
+        {
+            get => Projectile.ai[0];
+            set => Projectile.ai[0] = value;
+        }
+
+        public override bool PreAI()
+        {
+            if (++_frameTick >= 1)
+            {
+                _frameTick = 0;
+                if (++_frameCounter >= 10)
+                {
+                    _frameCounter = 0;
+                }
+            }
+            return true;
+        }
+
+        public override void AI()
+        {
+            Player owner = Main.player[Projectile.owner];
+            Projectile.Center = owner.Center;
+
+            //Lighting
+
+            Vector3 RGB = new(0.89f, 2.53f, 2.55f);
+            // The multiplication here wasn't doing anything
+            Lighting.AddLight(Projectile.position, RGB.X, RGB.Y, RGB.Z);
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return new Color(200, 200, 200, 0) * (1f - Projectile.alpha / 50f);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+            Vector2 drawPosition = Projectile.Center - Main.screenPosition;
+
+            float width = 500;
+            float height = 500;
+            Vector2 origin = new Vector2(width / 2, height / 2);
+            int frameSpeed = 1;
+            int frameCount = 10;
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            spriteBatch.Draw(texture, drawPosition,
+                texture.AnimationFrame(ref _frameCounter, ref _frameTick, frameSpeed, frameCount, false),
+                (Color)GetAlpha(lightColor), 0f, origin, 1.5f, SpriteEffects.None, 0f);
+            return false;
+        }
+    }
+}
