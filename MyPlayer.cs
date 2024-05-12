b7efa04935f924579769dfd734c9c169881666af
Zenovia <calamityjewelstudios@gmail.com>
2024-05-12 04:05:57 -0400

    green sun started

--- MyPlayer.cs
+++ MyPlayer.cs
@@ -2055,7 +2055,98 @@ namespace Stellamod
 
 
 
-			if (ModContent.GetInstance<LunarVeilClientConfig>().ParticlesToggle == true && (ZoneCinder || ZoneDrakonic))
+
+
+            bool spawnAuroreanParticles2 = EventWorld.GreenSun && ZoneAcid && (player.ZoneOverworldHeight || player.ZoneSkyHeight);
+            if (ModContent.GetInstance<LunarVeilClientConfig>().ParticlesToggle == true && spawnAuroreanParticles2)
+            {
+                Main.windPhysicsStrength = 50;
+                GoldenRingCooldown++;
+                GoldenSparkleCooldown++;
+                RayCooldown++;
+
+                for (int j = 0; j < 1; j++)
+                {
+                    RandomOrig3 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-900f, 900f), (Main.rand.NextFloat(-600f, 600f)));
+                    RandomOrig2 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1600f, 1600f), (Main.rand.NextFloat(-900f, 900f)));
+                    RandomOrig = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1800f, 1800f), (Main.rand.NextFloat(-1200f, 1200f)));
+
+                    Vector2 speed = Main.rand.NextVector2Circular(1f, 1f);
+                    Vector2 speed2 = Main.rand.NextVector2Circular(0.1f, 0.1f);
+                    ParticleManager.NewParticle(Player.Center - RandomOrig, speed2 * 3, ParticleManager.NewInstance<FabledParticle2>(), Color.Orange, Main.rand.NextFloat(0.2f, 0.8f));
+                }
+
+                for (int j = 0; j < 1; j++)
+                {
+                    RandomOrig3 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-900f, 900f), (Main.rand.NextFloat(-600f, 600f)));
+                    RandomOrig2 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1600f, 1600f), (Main.rand.NextFloat(-900f, 900f)));
+                    RandomOrig = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1800f, 1800f), (Main.rand.NextFloat(-1200f, 1200f)));
+
+                    Vector2 speed = Main.rand.NextVector2Circular(1f, 1f);
+                    Vector2 speed2 = Main.rand.NextVector2Circular(0.1f, 0.1f);
+                    ParticleManager.NewParticle(Player.Center - RandomOrig3, speed * 0.5f, ParticleManager.NewInstance<FabledParticle2>(), Color.HotPink, Main.rand.NextFloat(0.2f, 0.8f));
+                }
+
+                for (int j = 0; j < 1; j++)
+                {
+                    RandomOrig3 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-900f, 900f), (Main.rand.NextFloat(-600f, 600f)));
+                    RandomOrig2 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1600f, 1600f), (Main.rand.NextFloat(-900f, 900f)));
+                    RandomOrig = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1800f, 1800f), (Main.rand.NextFloat(-1200f, 1200f)));
+
+                    Vector2 speed = Main.rand.NextVector2Circular(1f, 1f);
+                    Vector2 speed2 = Main.rand.NextVector2Circular(0.1f, 0.1f);
+                    ParticleManager.NewParticle(Player.Center - RandomOrig2, speed * 2, ParticleManager.NewInstance<MoonTrailParticle2>(), Color.RoyalBlue, Main.rand.NextFloat(0.2f, 0.8f));
+                }
+
+                for (int j = 0; j < 1; j++)
+                {
+                    RandomOrig3 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-900f, 900f), (Main.rand.NextFloat(-600f, 600f)));
+                    RandomOrig2 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1600f, 1600f), (Main.rand.NextFloat(-900f, 900f)));
+                    RandomOrig = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1800f, 1800f), (Main.rand.NextFloat(-1200f, 1200f)));
+
+                    Vector2 speed = Main.rand.NextVector2Circular(1f, 1f);
+                    Vector2 speed2 = Main.rand.NextVector2Circular(0.1f, 0.1f);
+                    ParticleManager.NewParticle(Player.Center - RandomOrig, speed2 * 3, ParticleManager.NewInstance<FabledParticle3>(), Color.RoyalBlue, Main.rand.NextFloat(0.2f, 0.8f));
+                }
+
+                for (int j = 0; j < 1; j++)
+                {
+                    RandomOrig3 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-900f, 900f), (Main.rand.NextFloat(-600f, 600f)));
+                    RandomOrig2 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1600f, 1600f), (Main.rand.NextFloat(-900f, 900f)));
+                    RandomOrig = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1800f, 1800f), (Main.rand.NextFloat(-1200f, 1200f)));
+
+                    Vector2 speed = Main.rand.NextVector2Circular(1f, 1f);
+                    Vector2 speed2 = Main.rand.NextVector2Circular(0.1f, 0.1f);
+                    ParticleManager.NewParticle(Player.Center - RandomOrig2, speed * 2, ParticleManager.NewInstance<GoldRingParticle2>(), Color.RoyalBlue, Main.rand.NextFloat(0.2f, 0.8f));
+                }
+
+                for (int j = 0; j < 1; j++)
+                {
+                    RandomOrig3 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-900f, 900f), (Main.rand.NextFloat(-600f, 600f)));
+                    RandomOrig2 = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1600f, 1600f), (Main.rand.NextFloat(-900f, 900f)));
+                    RandomOrig = new Vector2(Player.width / 2, Player.height / 2) + new Vector2(Main.rand.NextFloat(-1800f, 1800f), (Main.rand.NextFloat(-1200f, 1200f)));
+
+                    Vector2 speed = Main.rand.NextVector2Circular(1f, 1f);
+                    Vector2 speed2 = Main.rand.NextVector2Circular(0.1f, 0.1f);
+                    ParticleManager.NewParticle(Player.Center - RandomOrig3, speed * 0.5f, ParticleManager.NewInstance<FabledParticle3>(), Color.RoyalBlue, Main.rand.NextFloat(0.2f, 0.8f));
+                }
+
+              
+
+               
+
+
+                GoldenRingCooldown++;
+                GoldenSparkleCooldown++;
+               
+            }
+
+
+
+
+
+
+            if (ModContent.GetInstance<LunarVeilClientConfig>().ParticlesToggle == true && (ZoneCinder || ZoneDrakonic))
 			{
 				Main.windPhysicsStrength = 50;
 				Main.UseHeatDistortion = true;
