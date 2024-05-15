d3dcdde0c4ad27a509477d9fa09181f54978ae0e
Zenovia <calamityjewelstudios@gmail.com>
2024-05-15 15:26:14 -0400

    two reworked weapons

--- Items/Weapons/Melee/Gintelze.cs
+++ Items/Weapons/Melee/Gintelze.cs
@@ -1,6 +1,16 @@
-﻿
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Buffs;
+using Stellamod.Helpers;
+using Stellamod.NPCs.Bosses.Niivi.Projectiles;
+using Stellamod.NPCs.Catacombs.Trap.Sparn;
+using Stellamod.NPCs.Catacombs.Water.WaterCogwork;
+using Stellamod.Projectiles;
+using Stellamod.Projectiles.Chains;
+using Stellamod.Projectiles.Gun;
+using Stellamod.Projectiles.Test;
 using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -8,41 +18,54 @@ using Terraria.ModLoader;
 namespace Stellamod.Items.Weapons.Melee
 {
     public class Gintelze : ModItem
-	{
-		public override void SetStaticDefaults()
-		{
-			// Tooltip.SetDefault("This sword feels warm, what is this Materials?"); // The (English) text shown below your weapon's name.
+    {
+        private int _dir = 1;
+        public override void SetStaticDefaults()
+        {
+            /* Tooltip.SetDefault("Meatballs" +
+				"\nDo not be worried, this mushes reality into bit bits and then shoots it!" +
+				"\nYou can never miss :P"); */
+            // DisplayName.SetDefault("Teraciz");
 
-			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-		}
-		public override void SetDefaults()
-		{
-			Item.width = 40; // The item texture's width.
-			Item.height = 40; // The item texture's height.
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
 
-			Item.useStyle = ItemUseStyleID.Swing; // The useStyle of the Item.
-			Item.useTime = 20; // The time span of using the weapon. Remember in terraria, 60 frames is a second.
-			Item.useAnimation = 20; // The time span of the using animation of the weapon, suggest setting it the same as useTime.
-			Item.autoReuse = true; // Whether the weapon can be used more than once automatically by holding the use button.
+        public override void SetDefaults()
+        {
+            Item.width = 62;
+            Item.height = 32;
+            Item.scale = 0.9f;
+            Item.rare = ItemRarityID.Green;
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true;
+            Item.UseSound = new SoundStyle("Stellamod/Assets/Sounds/Balls");
 
-			Item.DamageType = DamageClass.Melee; // Whether your item is part of the melee class.
-			Item.damage = 13; // The damage your item deals.
-			Item.knockBack = 12; // The force of knockback of the weapon. Maximum is 20
-			Item.crit = 62; // The critical strike chance the weapon has. The player, by default, has a 4% critical strike chance.
+            // Weapon Properties
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 19;
+            Item.knockBack = 0;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
 
-			Item.value = Item.buyPrice(gold: 1); // The value of the weapon in copper coins.
-            Item.rare = ItemRarityID.Blue;
-            Item.UseSound = SoundID.Item1; // The sound when the weapon is being used.
-		}
-		public override void MeleeEffects(Player player, Rectangle hitbox)
-		{
-			if (Main.rand.NextBool(1))
-			{
-				// Emit dusts when the sword is swung
-				Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.SilverCoin);
-			}
-		}
-		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
-	
-	}
+            // Gun Properties
+            Item.shoot = ModContent.ProjectileType<GintelzeSwingProj>();
+            Item.shootSpeed = 5;
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        // This method lets you adjust position of the gun in the player's hands. Play with these values until it looks good with your graphics.
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(2f, -2f);
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            _dir = -_dir;
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, ai1: _dir);
+            return false;
+        }
+    }
 }
--- Items/Weapons/Ranged/MagnusMagnum.cs
+++ Items/Weapons/Ranged/MagnusMagnum.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -36,5 +38,66 @@ namespace Stellamod.Items.Weapons.Ranged
 			return new Vector2(-2, 0);
 		}
 
-	}
+
+
+
+        private int _comboCounter;
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            float rot = velocity.ToRotation();
+            float spread = 0.4f;
+
+            Vector2 offset = new Vector2(1.5f, -0.1f * player.direction).RotatedBy(rot);
+
+            _comboCounter++;
+            if (_comboCounter > 100)
+            {
+                for (int k = 0; k < 7; k++)
+                {
+                    Vector2 direction = offset.RotatedByRandom(spread);
+                    Dust.NewDustPerfect(position + offset * 43, ModContent.DustType<Dusts.GlowDust>(), new Vector2(0, 0), 125, new Color(50, 80, 240), 1);
+                    Dust.NewDustPerfect(player.Center + offset * 43, ModContent.DustType<Dusts.TSmokeDust>(), Vector2.UnitY * -2 + offset.RotatedByRandom(spread), 150, Color.DarkBlue * 0.5f, Main.rand.NextFloat(0.5f, 1));
+                    Dust.NewDustPerfect(player.Center + offset * 43, ModContent.DustType<Dusts.TSmokeDust>(), Vector2.UnitY * -2 + offset.RotatedByRandom(spread), 150, new Color(60, 55, 50) * 0.5f, Main.rand.NextFloat(0.5f, 1));
+                    Dust.NewDustPerfect(position + offset * 43, ModContent.DustType<Dusts.GlowDust>(), direction * Main.rand.NextFloat(8), 125, Color.DarkBlue, Main.rand.NextFloat(0.5f, 0.8f));
+                }
+                SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/MiniPistol2"));
+                Item.useTime = 31;
+                Item.useAnimation = 31;
+                _comboCounter = 0;
+            }
+            if (_comboCounter > 75)
+            {
+                Dust.NewDustPerfect(player.Center + offset * 43, ModContent.DustType<Dusts.TSmokeDust>(), Vector2.UnitY * -2 + offset.RotatedByRandom(spread), 150, Color.IndianRed * 0.5f, Main.rand.NextFloat(0.5f, 1));
+            }
+
+     
+
+            for (int p = 0; p < 1; p++)
+            {
+                // Rotate the velocity randomly by 30 degrees at max.
+                Vector2 newVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(7));
+                newVelocity *= 1f - Main.rand.NextFloat(0.3f);
+                Projectile.NewProjectileDirect(source, position, newVelocity, type, damage, knockback, player.whoAmI);
+            }
+
+            Main.LocalPlayer.GetModPlayer<MyPlayer>().ShakeAtPosition(player.Center, 1024f, 6f);
+
+
+            //Dust Burst Towards Mouse
+
+
+            for (int k = 0; k < 7; k++)
+            {
+                Vector2 direction = offset.RotatedByRandom(spread);
+
+
+                Dust.NewDustPerfect(position + offset * 43, ModContent.DustType<Dusts.GlowDust>(), direction * Main.rand.NextFloat(8), 125, new Color(150, 50, 240), Main.rand.NextFloat(0.2f, 0.5f));
+            }
+
+            Dust.NewDustPerfect(position + offset * 43, ModContent.DustType<Dusts.GlowDust>(), new Vector2(0, 0), 125, new Color(100, 80, 240), 1);
+            Dust.NewDustPerfect(player.Center + offset * 43, ModContent.DustType<Dusts.TSmokeDust>(), Vector2.UnitY * -2 + offset.RotatedByRandom(spread), 150, new Color(60, 55, 50) * 0.5f, Main.rand.NextFloat(0.5f, 1));
+            return base.Shoot(player, source, position, velocity, type, damage, knockback);
+        }
+
+    }
 }
