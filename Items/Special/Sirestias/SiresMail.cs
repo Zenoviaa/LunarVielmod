a447d24b88b04cd4c289825cd9fcadba68f94444
Zenovia <calamityjewelstudios@gmail.com>
2024-05-16 01:53:01 -0400

    jj

--- Items/Special/Sirestias/SiresMail.cs
+++ Items/Special/Sirestias/SiresMail.cs
@@ -242,6 +242,13 @@ namespace Stellamod.Items.Special.Sirestias
                         }
 
 
+                        if (DownedBossSystem.downedSOMBoss)
+                        {
+
+
+
+                        }
+
 
 
 
@@ -280,4 +287,77 @@ namespace Stellamod.Items.Special.Sirestias
                 return true;
         }
     }
+
+
+
+
+
+
+
+
+        internal class CallDialogue9 : Dialogue
+        {
+            //The number of steps in this dialogue
+            public override int Length => 2;
+
+            public override void Next(int index)
+            {
+                base.Next(index);
+
+                //This starts the dialogue
+                switch (index)
+                {
+                    case 0:
+                        //Set the texture of the portrait
+                        DialogueSystem.SetPortrait("Stellamod/UI/Dialogue/SirestiasDialoguePortrait");
+
+                        //Put your dialogue in Mods.Stellamod.Dialogue.hjson, then get it like this
+                        DialogueSystem.WriteText(GetLocalizedText("SirestiasTalk6"));
+                        break;
+
+                    case 1:
+                        //Set the texture of the portrait
+
+                        //Put your dialogue in Mods.Stellamod.Dialogue.hjson, then get it like this
+                        DialogueSystem.WriteText(GetLocalizedText("SirestiasTalk7"));
+                        break;
+
+                }
+            }
+
+            public override void Update(int index)
+            {
+                base.Update(index);
+                //If you want stuff to happen while they're talking you can do it here ig
+                //But that might not be a good idea since you can just speed through dialogues
+            }
+
+            public override void Complete()
+            {
+
+                //Do something when the dialogue is completely finished
+
+
+                base.Complete();
+            }
+
+
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
