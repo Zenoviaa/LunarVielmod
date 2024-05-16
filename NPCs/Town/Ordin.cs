40862ab34aa9f3c833e72810ca355f74435ff5c5
Zenovia <calamityjewelstudios@gmail.com>
2024-05-16 03:51:15 -0400

    miss

--- NPCs/Town/Ordin.cs
+++ NPCs/Town/Ordin.cs
@@ -400,6 +400,12 @@ namespace Stellamod.NPCs.Town
                          shopSpecialCurrency = Stellamod.MOLCurrencyID // omit this line if shopCustomPrice should be in regular coins.
                      })
 
+                      .Add(new Item(ModContent.ItemType<Incinerator>())
+                      {
+                           shopCustomPrice = 1,
+                           shopSpecialCurrency = Stellamod.MOLCurrencyID // omit this line if shopCustomPrice should be in regular coins.
+                      })
+
                       .Add(new Item(ModContent.ItemType<Vhel>())
                       {
                           shopCustomPrice = 3,
