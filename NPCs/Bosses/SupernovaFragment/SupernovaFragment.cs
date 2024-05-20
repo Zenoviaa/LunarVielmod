92c6fea4ebc77b91ee04328b44a8e2996fec159d
Zenovia <calamityjewelstudios@gmail.com>
2024-05-20 07:57:07 -0400

    real

--- NPCs/Bosses/SupernovaFragment/SupernovaFragment.cs
+++ NPCs/Bosses/SupernovaFragment/SupernovaFragment.cs
@@ -84,7 +84,7 @@ namespace Stellamod.NPCs.Bosses.SupernovaFragment
             NPC.npcSlots = 10f;
             Music = MusicLoader.GetMusicSlot(Mod, "Assets/Music/SupernovaFragment");
             NPC.HitSound = new SoundStyle("Stellamod/Assets/Sounds/VoidHit") with { PitchVariance = 0.1f };
-            NPC.BossBar = GetInstance<SInBossBar>();
+            NPC.BossBar = GetInstance<SUPBossBar>();
             NPC.aiStyle = 0;
         }
         public override void ApplyDifficultyAndPlayerScaling(int numPlayers, float balance, float bossAdjustment)
