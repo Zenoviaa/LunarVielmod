0cf0b04c52cbab6cd69a6d176a065f8e06a79175
Zenovia <calamityjewelstudios@gmail.com>
2024-05-20 11:32:42 -0400

    aaa

--- Items/Weapons/Mage/TheAurora.cs
+++ Items/Weapons/Mage/TheAurora.cs
@@ -11,6 +11,7 @@ using Terraria;
 using Microsoft.Xna.Framework;
 using Stellamod.Items.Materials;
 using Stellamod.Projectiles.Magic;
+using Terraria.DataStructures;
 
 namespace Stellamod.Items.Weapons.Mage
 { 
@@ -58,6 +59,28 @@ namespace Stellamod.Items.Weapons.Mage
             recipe.AddIngredient(ItemID.FlareGun, 1);
             recipe.AddTile(TileID.Anvils);
             recipe.Register();
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Main.LocalPlayer.GetModPlayer<MyPlayer>().ShakeAtPosition(player.Center, 1024f, 10f);
+
+            //Dust Burst Towards Mouse
+
+            float rot = velocity.ToRotation();
+            float spread = 0.4f;
+
+            Vector2 offset = new Vector2(1, -0.1f * player.direction).RotatedBy(rot);
+            for (int k = 0; k < 15; k++)
+            {
+                Vector2 direction = offset.RotatedByRandom(spread);
+
+                Dust.NewDustPerfect(position + offset * 43, ModContent.DustType<Dusts.GlowDust>(), direction * Main.rand.NextFloat(8), 125, new Color(150, 80, 240), Main.rand.NextFloat(0.2f, 0.5f));
+            }
+            Dust.NewDustPerfect(position + offset * 43, ModContent.DustType<Dusts.GlowDust>(), new Vector2(0, 0), 125, new Color(150, 80, 40), 1);
+            Dust.NewDustPerfect(player.Center + offset * 43, ModContent.DustType<Dusts.TSmokeDust>(), Vector2.UnitY * -2 + offset.RotatedByRandom(spread), 150, new Color(60, 55, 50) * 0.5f, Main.rand.NextFloat(0.5f, 1));
+            return base.Shoot(player, source, position, velocity, type, damage, knockback);
+
         }
     }
 }
--- Items/Weapons/Summon/TheBurningRod.cs
+++ Items/Weapons/Summon/TheBurningRod.cs
@@ -36,7 +36,7 @@ namespace Stellamod.Items.Weapons.Summon
 
 		public override void SetDefaults()
 		{
-			Item.damage = 32;
+			Item.damage = 22;
 			Item.knockBack = 3f;
 			Item.mana = 10;
 			Item.width = 32;
