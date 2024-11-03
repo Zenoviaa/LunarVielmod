7b7051e3b7d7e311144bf53f67f39c3576a24ef4
Scolon <scoloned@gmail.com>
2024-11-02 23:08:21 -0400

    heat glider rework

--- Items/Weapons/Ranged/HeatGlider.cs
+++ Items/Weapons/Ranged/HeatGlider.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
+using Stellamod.Projectiles.Ammo;
 using Stellamod.Projectiles.Bow;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -8,6 +10,7 @@ namespace Stellamod.Items.Weapons.Ranged
 {
     internal class HeatGlider : ModItem
     {
+        private int _comboIndex;
         public override void SetDefaults()
         {
             Item.damage = 19;
@@ -38,9 +41,13 @@ namespace Stellamod.Items.Weapons.Ranged
 
         public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
         {
-            if (type == ProjectileID.WoodenArrowFriendly)
+            base.ModifyShootStats(player, ref position, ref velocity, ref type, ref damage, ref knockback);
+            _comboIndex++;
+            if (_comboIndex >= 3)
             {
                 type = ModContent.ProjectileType<HuntrianArrow>();
+                _comboIndex = 0;
+                SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/HeatFeather"), player.position);
             }
         }
     }
