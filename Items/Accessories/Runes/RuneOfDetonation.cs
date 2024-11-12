dd5c65f224dc99906c926d0f23b923ffe1b57814
Scolon <scoloned@gmail.com>
2024-11-12 15:33:04 -0500

    reworked rune of detonation

--- Items/Accessories/Runes/RuneOfDetonation.cs
+++ Items/Accessories/Runes/RuneOfDetonation.cs
@@ -1,44 +1,117 @@
-﻿using Stellamod.Items.Materials;
-using Stellamod.Tiles;
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Common.Bases;
+using Stellamod.Helpers;
+using Stellamod.Projectiles.IgniterExplosions;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ModLoader;
-using Terraria.ID;
-using Stellamod.Items.Materials.Molds;
+using Terraria.WorldBuilding;
 
 namespace Stellamod.Items.Accessories.Runes
 {
-    internal class RuneOfDetonation : ModItem
+    internal class RuneOfDetonationBomb : ModProjectile
     {
-        public override void SetStaticDefaults()
-        {
-            // DisplayName.SetDefault("Rune of Detonation");
-            // Tooltip.SetDefault("Has a chance to explode enemies doing double damage");
-        }
+        private ref float Timer => ref Projectile.ai[0];
+        public override string Texture => TextureRegistry.EmptyTexture;
 
         public override void SetDefaults()
         {
-            Item.width = 20;
-            Item.height = 20;
-            Item.value = Item.sellPrice(silver: 75);
-            Item.rare = ItemRarityID.Blue;
-            Item.accessory = true;
+            base.SetDefaults();
+            Projectile.width = 1;
+            Projectile.height = 1;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 30;
         }
-        
-        public override void AddRecipes()
+
+        public override void AI()
         {
-            Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ModContent.ItemType<LostScrap>(), 15);
-            recipe.AddIngredient(ModContent.ItemType<RippedFabric>(), 5);
-            recipe.AddIngredient(ModContent.ItemType<ArnchaliteBar>(), 5);
-            recipe.AddIngredient(ModContent.ItemType<BlankRune>(), 1);
-            recipe.AddTile(ModContent.TileType<BroochesTable>());
-            recipe.Register();
+            base.AI();
+            Timer++;
+            if(Timer % 10 == 0)
+            {
+                Vector2 explosionPosition = Projectile.Center;
+                explosionPosition += Main.rand.NextVector2Circular(32, 32);
+
+                if(Main.myPlayer == Projectile.owner)
+                {
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), explosionPosition, Vector2.Zero,
+                        ModContent.ProjectileType<FableExSps>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                }
+
+
+                //Effects
+                SoundStyle explosionSoundStyle = new SoundStyle($"Stellamod/Assets/Sounds/HeatExplosion");
+                explosionSoundStyle.PitchVariance = 0.15f;
+                SoundEngine.PlaySound(explosionSoundStyle, Projectile.position);
+
+                MyPlayer myPlayer = Main.LocalPlayer.GetModPlayer<MyPlayer>();
+                myPlayer.ShakeAtPosition(Projectile.Center, 1024, 8);
+            }
+        }
+    }
+
+    internal class RuneOfDetonationPlayer : ModPlayer
+    {
+        private int _lastTargetIndex;
+        private int _hitCount;
+        public bool hasDetonationRune;
+        public override void ResetEffects()
+        {
+            base.ResetEffects();
+            hasDetonationRune = false;
+        }
+
+        public override void PostUpdateEquips()
+        {
+            base.PostUpdateEquips();
+            if (!hasDetonationRune)
+            {
+                _lastTargetIndex = -1;
+                _hitCount = 1;
+            }
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            base.OnHitNPC(target, hit, damageDone);
+            if (!hasDetonationRune)
+                return;
+            if (_lastTargetIndex != target.whoAmI)
+            {
+                _hitCount = 1;
+                _lastTargetIndex = target.whoAmI;
+            }
+            else
+            {
+                _hitCount++;
+            }
+
+            if (_hitCount >= 5)
+            {
+                float damage = damageDone;
+                damage *= 0.33f;
+                damage = MathHelper.Clamp(damage, 0, 33);
+
+                Projectile.NewProjectile(Player.GetSource_FromThis(), target.Center, Vector2.Zero,
+                    ModContent.ProjectileType<RuneOfDetonationBomb>(), (int)damage, hit.Knockback, Player.whoAmI);
+                _hitCount = 1;
+            }
+        }
+    }
+
+    internal class RuneOfDetonation : BaseRune
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults(); 
+            Item.value = Item.sellPrice(gold: 2);
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.GetModPlayer<MyPlayer>().DetonationRune = true;
-
+            RuneOfDetonationPlayer detonationPlayer = player.GetModPlayer<RuneOfDetonationPlayer>();
+            detonationPlayer.hasDetonationRune = true;
         }
     }
 }
