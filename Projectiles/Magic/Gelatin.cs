d4c2538e2cc40f7987a57d4fcfea3448c49df51e
Scolon <scoloned@gmail.com>
2024-11-12 22:10:06 -0500

    jelly bow explosion

--- Projectiles/Magic/Gelatin.cs
+++ Projectiles/Magic/Gelatin.cs
@@ -1,6 +1,8 @@
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Helpers;
+using Stellamod.Projectiles.IgniterExplosions;
 using Terraria;
 using Terraria.Audio;
 using Terraria.GameContent;
@@ -9,8 +11,58 @@ using Terraria.ModLoader;
 
 namespace Stellamod.Projectiles.Magic
 {
+    public class GelatinBoom : BaseIgniterExplosion
+    {
+        public override int FrameCount => 22;
+        public override void Start()
+        {
+            base.Start();
+            var circle = EffectsHelper.SimpleExplosionCircle(Projectile, Color.Aqua, endRadius: 80);
+            for (int i = 0; i < 2; i++)
+            {
+                int num1 = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.VenomStaff, 0f, -2f, 0, default(Color), .8f);
+                Main.dust[num1].noGravity = true;
+                Main.dust[num1].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                Main.dust[num1].position.Y += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                if (Main.dust[num1].position != Projectile.Center)
+                    Main.dust[num1].velocity = Projectile.DirectionTo(Main.dust[num1].position) * 6f;
+                int num = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.VenomStaff, 0f, -2f, 0, default(Color), .8f);
+                Main.dust[num].noGravity = true;
+                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                Main.dust[num].position.Y += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                if (Main.dust[num].position != Projectile.Center)
+                    Main.dust[num].velocity = Projectile.DirectionTo(Main.dust[num].position) * 6f;
+                SoundEngine.PlaySound(SoundID.Item14, Projectile.position);
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                int num = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.VenomStaff, 0f, -2f, 0, default(Color), 1f);
+                Main.dust[num].noGravity = true;
+                Main.dust[num].scale = 1.9f;
+                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                {
+                    Main.dust[num].velocity = Projectile.DirectionTo(Main.dust[num].position) * 10f;
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                int num = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.VenomStaff, 0f, -2f, 0, default(Color), 1.2f);
+                Main.dust[num].noGravity = true;
+                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
+                {
+                    Main.dust[num].velocity = Projectile.DirectionTo(Main.dust[num].position) * 10f;
+                }
+            }
+        }
+    }
+
     public class Gelatin : ModProjectile
 	{
+        private ref float Timer => ref Projectile.ai[0];
 		public override void SetStaticDefaults()
 		{
             // DisplayName.SetDefault("Gelatin");
@@ -20,11 +72,24 @@ namespace Stellamod.Projectiles.Magic
 
 		public override void SetDefaults()
 		{
-			Projectile.CloneDefaults(ProjectileID.Bullet);
-            AIType = ProjectileID.Bullet;
-			Projectile.width = 20;
-            Projectile.height = 20;
-            Projectile.DamageType = DamageClass.Throwing;
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.friendly = true;
+            Projectile.timeLeft = 180;
+        }
+
+        public override void AI()
+        {
+            base.AI();
+            Timer++;
+            if (Timer % 6 == 0)
+            {
+                int dust = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.VenomStaff);
+                int dust1 = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.VenomStaff);
+                Main.dust[dust].noGravity = true;
+                Main.dust[dust1].noGravity = true;
+            }
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.ToRadians(90);
         }
 
         public override bool PreDraw(ref Color lightColor)
@@ -44,59 +109,11 @@ namespace Stellamod.Projectiles.Magic
             return false;
         }
 
-        public override bool PreAI()
-        {
-            if (Main.rand.NextBool(6))
-            {
-                int dust = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.VenomStaff);
-                int dust1 = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.VenomStaff);
-                Main.dust[dust].noGravity = true;
-                Main.dust[dust1].noGravity = true;
-            }
-            return true;
-        }
 
         public override void OnKill(int timeLeft)
         {
-            for (int i = 0; i < 20; i++)
-            {
-                int num1 = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.VenomStaff, 0f, -2f, 0, default(Color), .8f);
-                Main.dust[num1].noGravity = true;
-                Main.dust[num1].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                Main.dust[num1].position.Y += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                if (Main.dust[num1].position != Projectile.Center)
-                    Main.dust[num1].velocity = Projectile.DirectionTo(Main.dust[num1].position) * 6f;
-                int num = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.VenomStaff, 0f, -2f, 0, default(Color), .8f);
-                Main.dust[num].noGravity = true;
-                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                Main.dust[num].position.Y += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                if (Main.dust[num].position != Projectile.Center)
-                    Main.dust[num].velocity = Projectile.DirectionTo(Main.dust[num].position) * 6f;
-                SoundEngine.PlaySound(SoundID.Item14, Projectile.position);
-            }
-
-            for (int i = 0; i < 180; i++)
-            {
-                int num = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.VenomStaff, 0f, -2f, 0, default(Color), 1.5f);
-                Main.dust[num].noGravity = true;
-                Main.dust[num].scale = 1.9f;
-                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                {
-                    Main.dust[num].velocity = Projectile.DirectionTo(Main.dust[num].position) * 10f;
-                }
-            }
-
-            for (int i = 0; i < 80; i++)
-            {
-                int num = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.VenomStaff, 0f, -2f, 0, default(Color), 2.5f);
-                Main.dust[num].noGravity = true;
-                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                {
-                    Main.dust[num].velocity = Projectile.DirectionTo(Main.dust[num].position) * 10f;
-                }
-            }
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero,
+                ModContent.ProjectileType<GelatinBoom>(), (int)(Projectile.damage * 0.5f), Projectile.knockBack, Projectile.owner);
         }
     }
 }
