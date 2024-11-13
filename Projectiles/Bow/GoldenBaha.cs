95ee0cdabc883c03c682b55d6faf548e6367be85
Scolon <scoloned@gmail.com>
2024-11-12 22:50:12 -0500

    adjust visuals on golden baha

--- Projectiles/Bow/GoldenBaha.cs
+++ Projectiles/Bow/GoldenBaha.cs
@@ -14,16 +14,16 @@ namespace Stellamod.Projectiles.Bow
 {
     internal class GoldenBaha : ModProjectile
     {
+        private ref float Timer => ref Projectile.ai[0];
         public override void SetStaticDefaults()
         {
             // DisplayName.SetDefault("Archarilite Arrow");
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 20;
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 10;
             ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
         }
 
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
-
             int gore1 = GoreHelper.TypeFallingLeafWhite;
             int gore2 = GoreHelper.TypeFallingLeafRed;
             for (int i = 0; i < 2; i++)
@@ -35,13 +35,13 @@ namespace Stellamod.Projectiles.Bow
                 Gore.NewGore(Projectile.GetSource_FromThis(), target.position, velocity, gore2);
             }
 
-
             for (int i = 0; i < 4; i++)
             {
-                Dust.NewDust(target.position, Projectile.width, Projectile.height, ModContent.DustType<GunFlash>(), Scale: 0.8f);
-                Dust.NewDustPerfect(target.position, ModContent.DustType<GlowDust>(), (Vector2.One * Main.rand.Next(1, 5)).RotatedByRandom(19.0), 0, Color.DarkGoldenrod, 1f).noGravity = true;
+                Dust.NewDust(target.position, Projectile.width, Projectile.height, 
+                    ModContent.DustType<GunFlash>(), Scale: 0.8f);
+                Dust.NewDustPerfect(target.Center, 
+                    ModContent.DustType<GlowDust>(), (Vector2.One * Main.rand.Next(1, 2)).RotatedByRandom(19.0), 0, Color.DarkGoldenrod, 1f).noGravity = true;
             }
-
 
             if (Main.rand.NextBool(2))
                 target.AddBuff(BuffID.OnFire3, 180);
@@ -49,42 +49,36 @@ namespace Stellamod.Projectiles.Bow
 
         public override void SetDefaults()
         {
-            Projectile.width = 15;
-            Projectile.height = 46;
-
+            Projectile.width = 8;
+            Projectile.height = 8;
             Projectile.knockBack = 12.9f;
-            Projectile.aiStyle = 1;
-            AIType = ProjectileID.Bullet;
-            Projectile.DamageType = DamageClass.Ranged;
             Projectile.friendly = true;
+            Projectile.timeLeft = 120;
+            Projectile.extraUpdates = 1;
         }
 
         public override void AI()
         {
-            Projectile.ai[1]++;
+            Timer++;
+            if (Timer % 5 == 0)
+            {
+                int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GoldCoin, 0f, 0f, 25, Color.White, 1f);
+                Main.dust[dustnumber].velocity *= 0.3f;
+                Main.dust[dustnumber].noGravity = true;
+            }
             Projectile.velocity *= 1.02f;
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.ToRadians(90);
         }
       
         public override void OnKill(int timeLeft)
         {
-
-
-           
-
-            Main.LocalPlayer.GetModPlayer<MyPlayer>().ShakeAtPosition(base.Projectile.Center, 512f, 32f);
-            for (int i = 0; i < 20; i++)
+            Main.LocalPlayer.GetModPlayer<MyPlayer>().ShakeAtPosition(Projectile.Center, 512f, 4);
+            for (int i = 0; i < 16; i++)
             {
-                Dust.NewDustPerfect(Projectile.Center, DustID.GoldCoin, (Vector2.One * Main.rand.Next(1, 5)).RotatedByRandom(25.0), 0, default, 1f).noGravity = false;
-            }
-            for (int i = 0; i < 50; i++)
-            {
-                int num = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GoldCoin, 0f, -2f, 0, default(Color), 1.5f);
-                Main.dust[num].noGravity = true;
-                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                Main.dust[num].position.X += Main.rand.Next(-50, 51) * .05f - 1.5f;
-                {
-                    Main.dust[num].velocity = Projectile.DirectionTo(Main.dust[num].position) * 6f;
-                }
+                float progress = (float)i / 16f;
+                float rot = progress * MathHelper.TwoPi;
+                Vector2 vel = rot.ToRotationVector2();
+                Dust.NewDustPerfect(Projectile.Center, DustID.GoldCoin, vel, 0, default, 1f).noGravity = false;
             }
         }
 
@@ -96,9 +90,10 @@ namespace Stellamod.Projectiles.Bow
         public PrimDrawer TrailDrawer { get; private set; } = null;
         public float WidthFunction(float completionRatio)
         {
-            float baseWidth = Projectile.scale * Projectile.width * 1.3f;
+            float baseWidth = Projectile.scale * Projectile.width * 0.7f;
             return MathHelper.SmoothStep(baseWidth, 3.5f, completionRatio);
         }
+
         public Color ColorFunction(float completionRatio)
         {
             return Color.Lerp(Color.LightGoldenrodYellow, Color.Transparent, completionRatio) * 0.7f;
@@ -106,43 +101,32 @@ namespace Stellamod.Projectiles.Bow
 
         public override bool PreDraw(ref Color lightColor)
         {
-            if (Main.rand.NextBool(5))
-            {
-                int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GoldCoin, 0f, 0f, 150, Color.White, 1f);
-                Main.dust[dustnumber].velocity *= 0.3f;
-                Main.dust[dustnumber].noGravity = true;
-            }
-            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, new Vector2(texture.Width / 2, texture.Height / 2), 1f, Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0f);
+
+            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+      
             TrailDrawer ??= new PrimDrawer(WidthFunction, ColorFunction, GameShaders.Misc["VampKnives:BasicTrail"]);
             GameShaders.Misc["VampKnives:BasicTrail"].SetShaderTexture(TrailRegistry.SmallWhispyTrail);
             TrailDrawer.DrawPrims(Projectile.oldPos, Projectile.Size * 0.5f - Main.screenPosition, 155);
 
-
+            SpriteBatch spriteBatch = Main.spriteBatch;
             int height = Main.player[Projectile.owner].height / 1; // 5 is frame count
             int y = height * Projectile.frame;
-            var rect = new Rectangle(0, y, Main.player[Projectile.owner].width, height);
-            var drawOrigin = new Vector2(Main.player[Projectile.owner].width / 2, Projectile.height / 2);
+            var drawOrigin = texture.Size() / 2;
 
             for (int k = 0; k < Projectile.oldPos.Length; k++)
             {
-                Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + Projectile.Size / 2 + new Vector2(0f, Projectile.gfxOffY);
                 Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
-                Main.EntitySpriteDraw((Texture2D)TextureAssets.Projectile[Projectile.type], drawPos, rect, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+                color *= 0.8f;
+                spriteBatch.Draw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
             }
-
-
+            spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, drawOrigin, 1f, Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0f);
             return false;
         }
 
         public override void PostDraw(Color lightColor)
         {
             Lighting.AddLight(Projectile.Center, Color.Orange.ToVector3() * 1.75f * Main.essScale);
-            if (Main.rand.NextBool(5))
-            {
-                int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.CopperCoin, 0f, 0f, 150, Color.White, 1f);
-                Main.dust[dustnumber].velocity *= 0.3f;
-            }
         }
     }
 }
