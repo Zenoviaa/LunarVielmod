7dedf3acdb09a378a4fff0b2a25711a29a0d86de
Scolon <scoloned@gmail.com>
2024-11-12 20:37:17 -0500

    comical tridenth itbox fix

--- Projectiles/IgniterExplosions/FunBoom.cs
+++ Projectiles/IgniterExplosions/FunBoom.cs
@@ -1,65 +1,57 @@
 ﻿using Microsoft.Xna.Framework;
-using System.Collections.Generic;
+using Stellamod.Helpers;
 using Terraria;
 using Terraria.ModLoader;
 
 namespace Stellamod.Projectiles.IgniterExplosions
 {
-	public class FunBoom : ModProjectile
-	{
-		public override void SetStaticDefaults()
-		{
-			// DisplayName.SetDefault("FrostShotIN");
-			Main.projFrames[Projectile.type] = 44;
-		}
+    public class FunBoom : ModProjectile
+    {
+        private ref float Timer => ref Projectile.ai[0];
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("FrostShotIN");
+            Main.projFrames[Projectile.type] = 44;
+        }
 
-		public override void SetDefaults()
-		{
-			Projectile.friendly = false;
-			Projectile.width = 194;
-			Projectile.height = 166;
-			Projectile.penetrate = -1;
-			Projectile.timeLeft = 44;
-			Projectile.scale = 1f;
+        public override void SetDefaults()
+        {
+            Projectile.friendly = false;
+            Projectile.width = 194;
+            Projectile.height = 166;
+            Projectile.scale = 1f;
+            Projectile.timeLeft = 44;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+        }
 
-		}
-		public float Timer
-		{
-			get => Projectile.ai[0];
-			set => Projectile.ai[0] = value;
-		}
-		public override void AI()
-		{
-			Projectile.rotation -= 0.01f;
-			Vector3 RGB = new(0.89f, 2.53f, 2.55f);
-			// The multiplication here wasn't doing anything
-			Lighting.AddLight(Projectile.position, RGB.X, RGB.Y, RGB.Z);
+        public override void AI()
+        {
+            Timer++;
+            if (Timer == 1)
+            {
+                var circle = EffectsHelper.SimpleExplosionCircle(Projectile, Color.OrangeRed);
+            }
 
-		}
+            Projectile.rotation -= 0.01f;
+            Vector3 RGB = new(0.89f, 2.53f, 2.55f);
+            // The multiplication here wasn't doing anything
+            Lighting.AddLight(Projectile.position, RGB.X, RGB.Y, RGB.Z);
+        }
 
-		public override bool PreAI()
-		{
-			Projectile.tileCollide = false;
-			if (++Projectile.frameCounter >= 1)
-			{
-				Projectile.frameCounter = 0;
-				if (++Projectile.frame >= 44)
-				{
-					Projectile.frame = 0;
-				}
-			}
-			return true;
-
-
-		}
-
-
-		public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
-		{
-			behindNPCs.Add(index);
-			behindProjectiles.Add(index);
-
-		}
-	}
-
+        public override bool PreAI()
+        {
+            Projectile.tileCollide = false;
+            if (++Projectile.frameCounter >= 1)
+            {
+                Projectile.frameCounter = 0;
+                if (++Projectile.frame >= 44)
+                {
+                    Projectile.frame = 0;
+                }
+            }
+            return true;
+        }
+    }
 }
--- Projectiles/Thrown/ComicalTridentP.cs
+++ Projectiles/Thrown/ComicalTridentP.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Stellamod.Projectiles.IgniterExplosions;
+
 using Terraria;
 using Terraria.Audio;
 using Terraria.GameContent;
@@ -12,22 +13,19 @@ namespace Stellamod.Projectiles.Thrown
 {
     public class ComicalTridentP : ModProjectile
     {
-        public bool OptionallySomeCondition { get; private set; }
-
         public override void SetStaticDefaults()
         {
-            // DisplayName.SetDefault("Cactius2");
             ProjectileID.Sets.TrailCacheLength[Projectile.type] = 10;
             ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
         }
 
         public override void SetDefaults()
         {
-            Projectile.CloneDefaults(ProjectileID.JavelinFriendly);
-            Projectile.width = 28;
-            Projectile.height = 98;
-            AIType = ProjectileID.JavelinFriendly;
-            Projectile.penetrate = 1;
+            Projectile.width = 8;
+            Projectile.height = 8;
+            Projectile.tileCollide = true;
+            Projectile.friendly = true;
+            Projectile.timeLeft = 180;
         }
 
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
@@ -38,52 +36,33 @@ namespace Stellamod.Projectiles.Thrown
                 target.AddBuff(BuffID.OnFire3, 280);
         }
 
-        public override bool OnTileCollide(Vector2 oldVelocity)
-        {
-            Projectile.penetrate--;
-            if (Projectile.penetrate <= 0)
-                Projectile.Kill();
-            else
-            {
-                if (Projectile.velocity.X != oldVelocity.X)
-                    Projectile.velocity.X = -oldVelocity.X;
-
-                if (Projectile.velocity.Y != oldVelocity.Y)
-                    Projectile.velocity.Y = -oldVelocity.Y;
-
-
-            }
-            SoundEngine.PlaySound(SoundID.Dig, Projectile.Center);
-            for (int i = 0; i < 7; i++)
-            {
-                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Torch);
-            }
-            return false;
-        }
-        public override bool PreAI()
-        {
-            if (Main.rand.NextBool(3))
-            {
-
-                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Torch);
-            }
-            return true;
-        }
         public override bool PreDraw(ref Color lightColor)
         {
-            Main.instance.LoadProjectile(Projectile.type);
-            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-
-            // Redraw the projectile with the color not influenced by light
-            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            Vector2 drawPos = Projectile.Center - Main.screenPosition;
+            Vector2 drawOrigin = texture.Size() / 2;
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
             for (int k = 0; k < Projectile.oldPos.Length; k++)
             {
-                Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Vector2 trailDrawPos = (Projectile.oldPos[k] - Main.screenPosition) + Projectile.Size / 2 + new Vector2(0f, Projectile.gfxOffY);
                 Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
-                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+                spriteBatch.Draw(texture, trailDrawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0f);
             }
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+            spriteBatch.Draw(texture, drawPos, null, Color.White.MultiplyRGB(lightColor), Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0f);
             return false;
         }
+
+        public override void AI()
+        {
+            base.AI();
+            Projectile.velocity.Y += 0.3f;
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        }
+
         public override void OnKill(int timeLeft)
         {
             for (int i = 0; i < 15; i++)
@@ -94,8 +73,7 @@ namespace Stellamod.Projectiles.Thrown
 
             Projectile.NewProjectile(Projectile.GetSource_FromThis(),Projectile.Center, Vector2.Zero, 
                 ModContent.ProjectileType<FunBoom>(), (int)(Projectile.damage * 1.5), 0f, Projectile.owner, 0f, 0f);
-            SoundEngine.PlaySound(new SoundStyle($"Stellamod/Assets/Sounds/ComicBoom"));
+            SoundEngine.PlaySound(new SoundStyle($"Stellamod/Assets/Sounds/ComicBoom"), Projectile.position);
         }
-
     }
 }
