b41d08fad63797d3bf099015fbacddf42d4490e5
Scolon <scoloned@gmail.com>
2024-11-21 23:32:48 -0500

    wave thing at top of the screen

--- NPCs/Colosseum/Common/BaseColosseumNPC.cs
+++ NPCs/Colosseum/Common/BaseColosseumNPC.cs
@@ -4,6 +4,12 @@ namespace Stellamod.NPCs.Colosseum.Common
 {
     public abstract class BaseColosseumNPC : ModNPC
     {
+        public override bool CheckActive()
+        {
+            ColosseumSystem colosseumSystem = ModContent.GetInstance<ColosseumSystem>();
+            return !colosseumSystem.IsActive();
+        }
+
         public override void OnKill()
         {
             base.OnKill();
