716dc24f19d85f03047981da95b4a510bc9f6354
Zenovia <calamityjewelstudios@gmail.com>
2024-11-21 21:25:55 -0500

    greenbean

--- Items/Weapons/Igniters/FrostCard.cs
+++ Items/Weapons/Igniters/FrostCard.cs
@@ -29,5 +29,11 @@ namespace Stellamod.Items.Weapons.Igniters
             base.SetDefaults();
             Item.damage = 2;
         }
+
+        public override int GetPowderSlotCount()
+        {
+
+            return 2;
+        }
     }
 }
