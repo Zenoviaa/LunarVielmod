6cadaea903f7003222fb309cb93272c65b388fac
Zenovia <calamityjewelstudios@gmail.com>
2024-11-30 14:38:41 -0500

    greenbean hopefully

---
+++ Items/Ores/GlisteningBar.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Items.Ores
+{
+    public class GlisteningBar : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// Tooltip.SetDefault("Frozen to the core, an essense of the moon and ice.");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 12;
+			Item.height = 12;
+			Item.maxStack = Item.CommonMaxStack;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 10;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.rare = ItemRarityID.Blue;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<GlisteningOre>(), 4);
+			recipe.AddTile(TileID.Furnaces);
+			recipe.Register();
+		}
+	}
+}
--- WorldG/StellaWorld.cs
+++ WorldG/StellaWorld.cs
@@ -5140,8 +5140,10 @@ namespace Stellamod.WorldG
 			for (int k = 0; k < (int)((Main.maxTilesX * Main.maxTilesY) * 6E-05); k++)
 			{
 				// 10. We randomly choose an x and y coordinate. The x coordinate is choosen from the far left to the far right coordinates. The y coordinate, however, is choosen from between WorldGen.worldSurfaceLow and the bottom of the map. We can use this technique to determine the depth that our ore should spawn at.
-				int x = WorldGen.genRand.Next(0, Main.maxTilesX);
-				int y = WorldGen.genRand.Next((int)GenVars.rockLayer, Main.maxTilesY);
+				
+
+                int x = WorldGen.genRand.Next(GenVars.snowOriginLeft - 100, GenVars.snowOriginRight + 100);
+				int y = WorldGen.genRand.Next((int)GenVars.rockLayerHigh, Main.maxTilesY);
 
 				// 11. Finally, we do the actual world generation code. In this example, we use the WorldGen.TileRunner method. This method spawns splotches of the Tile type we provide to the method. The behavior of TileRunner is detailed in the Useful Methods section below.
 				WorldGen.TileRunner(x, y, WorldGen.genRand.Next(3, 10), WorldGen.genRand.Next(2, 16), ModContent.TileType<FrileOreTile>());
