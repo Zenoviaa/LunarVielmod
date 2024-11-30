c13ffd74be55e3170aac01008f7f05836896ab86
Zenovia <calamityjewelstudios@gmail.com>
2024-11-30 14:28:27 -0500

    New Sprites and world generation stuff for the new ore

--- WorldG/StellaWorld.cs
+++ WorldG/StellaWorld.cs
@@ -139,6 +139,7 @@ namespace Stellamod.WorldG
 				tasks.Insert(MorrowGen + 11, new PassLegacy("World Gen Ice Ores", WorldGenFrileOre));
                 tasks.Insert(MorrowGen + 12, new PassLegacy("World Gen Dungeon Location", WorldGenDungeonLocation));
                 tasks.Insert(MorrowGen + 13, new PassLegacy("Icey Caverns", WorldGenIceCaverns));
+                tasks.Insert(MorrowGen + 14, new PassLegacy("World Gen Ice Ores", WorldGenGlisteningOre));
             }
 
 			int CathedralGen3 = tasks.FindIndex(genpass => genpass.Name.Equals("Buried Chests"));
@@ -5143,10 +5144,27 @@ namespace Stellamod.WorldG
 				int y = WorldGen.genRand.Next((int)GenVars.rockLayer, Main.maxTilesY);
 
 				// 11. Finally, we do the actual world generation code. In this example, we use the WorldGen.TileRunner method. This method spawns splotches of the Tile type we provide to the method. The behavior of TileRunner is detailed in the Useful Methods section below.
-				WorldGen.TileRunner(x, y, WorldGen.genRand.Next(3, 10), WorldGen.genRand.Next(2, 10), ModContent.TileType<FrileOreTile>());
+				WorldGen.TileRunner(x, y, WorldGen.genRand.Next(3, 10), WorldGen.genRand.Next(2, 16), ModContent.TileType<FrileOreTile>());
 			}
 		}
-		private void WorldGenDarkstone(GenerationProgress progress, GameConfiguration configuration)
+
+        private void WorldGenGlisteningOre(GenerationProgress progress, GameConfiguration configuration)
+        {
+            // 7. Setting a progress message is always a good idea. This is the message the user sees during world generation and can be useful for identifying infinite loops.      
+            progress.Message = "World Glistens with shines of the Glistening Moon";
+
+
+            for (int k = 0; k < (int)((Main.maxTilesX * Main.maxTilesY) * 6E-05); k++)
+            {
+                // 10. We randomly choose an x and y coordinate. The x coordinate is choosen from the far left to the far right coordinates. The y coordinate, however, is choosen from between WorldGen.worldSurfaceLow and the bottom of the map. We can use this technique to determine the depth that our ore should spawn at.
+                int x = WorldGen.genRand.Next(0, Main.maxTilesX);
+                int y = WorldGen.genRand.Next((int)GenVars.rockLayerLow, Main.maxTilesY);
+
+                // 11. Finally, we do the actual world generation code. In this example, we use the WorldGen.TileRunner method. This method spawns splotches of the Tile type we provide to the method. The behavior of TileRunner is detailed in the Useful Methods section below.
+                WorldGen.TileRunner(x, y, WorldGen.genRand.Next(3, 10), WorldGen.genRand.Next(2, 10), ModContent.TileType<GlisteningOreTile>());
+            }
+        }
+        private void WorldGenDarkstone(GenerationProgress progress, GameConfiguration configuration)
 		{
 			// 7. Setting a progress message is always a good idea. This is the message the user sees during world generation and can be useful for identifying infinite loops.      
 			progress.Message = "Blackening Stones for racist effect";
