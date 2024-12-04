dccd006098118e1b55964e8c742c342a4a1834d0
Scolon <scoloned@gmail.com>
2024-12-04 00:22:02 -0500

    okay

---
+++ Items/MoonlightMagic/Weapons/LeadWand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stellamod.Items.MoonlightMagic.Weapons
+{
+    internal class LeadWand : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 9;
+            Item.shootSpeed = 10;
+            Item.useTime = 18;
+            Item.useAnimation = 36;
+            Size = 8;
+            TrailLength = 16;
+        }
+
+        public override int GetNormalSlotCount()
+        {
+            return 4;
+        }
+
+        public override int GetTimedSlotCount()
+        {
+            return 4;
+        }
+    }
+}
