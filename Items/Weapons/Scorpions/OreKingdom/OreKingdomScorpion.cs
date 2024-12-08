bd350ebe029d5c5892b71e607e0c1280441c7a9f
Zenovia <calamityjewelstudios@gmail.com>
2024-12-08 13:51:58 -0500

    lmao

--- Items/Weapons/Scorpions/OreKingdom/OreKingdomScorpion.cs
+++ Items/Weapons/Scorpions/OreKingdom/OreKingdomScorpion.cs
@@ -1,4 +1,6 @@
 ﻿using Stellamod.Common.ScorpionMountSystem;
+using Stellamod.Items.Materials.Tech;
+using Stellamod.Items.Materials;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -31,6 +33,19 @@ namespace Stellamod.Items.Weapons.Scorpions.OreKingdom
         public override int GetRightHandedCount()
         {
             return 1;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.TungstenBar, 10);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+
+            Recipe recipe2 = CreateRecipe();
+            recipe2.AddIngredient(ItemID.SilverBar, 10);
+            recipe2.AddTile(TileID.Anvils);
+            recipe2.Register();
         }
     }
 }
--- Items/Weapons/Scorpions/RoyalPalace/RoyalPalaceScorpion.cs
+++ Items/Weapons/Scorpions/RoyalPalace/RoyalPalaceScorpion.cs
@@ -32,5 +32,18 @@ namespace Stellamod.Items.Weapons.Scorpions.RoyalPalace
         {
             return 2;
         }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.GoldBar, 10);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+
+            Recipe recipe2 = CreateRecipe();
+            recipe2.AddIngredient(ItemID.PlatinumBar, 10);
+            recipe2.AddTile(TileID.Anvils);
+            recipe2.Register();
+        }
     }
 }
