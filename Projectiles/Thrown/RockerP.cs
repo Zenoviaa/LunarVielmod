714a45a554099f558fc0d15710988c9a714994c7
Scolon <scoloned@gmail.com>
2024-12-13 00:21:29 -0500

    stonen fixes

--- Projectiles/Thrown/RockerP.cs
+++ Projectiles/Thrown/RockerP.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Helpers;
 using Stellamod.UI.Systems;
 using Terraria;
 using Terraria.Audio;
@@ -11,14 +12,15 @@ namespace Stellamod.Projectiles.Thrown
 {
     public class RockerP : ModProjectile
     {
+        private ref float Timer => ref Projectile.ai[0];
         public override void SetDefaults()
         {
             Projectile.penetrate = 1;
             Projectile.ignoreWater = true;
             Projectile.tileCollide = true;
             Projectile.DamageType = DamageClass.Throwing;
-            Projectile.height = 80;
-            Projectile.width = 80;
+            Projectile.height = 30;
+            Projectile.width = 30;
             Projectile.friendly = true;
             Projectile.scale = 1f;
             Projectile.usesLocalNPCImmunity = true;
@@ -33,12 +35,6 @@ namespace Stellamod.Projectiles.Thrown
             ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
         }
 
-        public float Timer
-        {
-            get => Projectile.ai[0];
-            set => Projectile.ai[0] = value;
-        }
-
         public override void AI()
         {
             Player player = Main.player[Projectile.owner];
@@ -46,17 +42,14 @@ namespace Stellamod.Projectiles.Thrown
             Timer++;
 
             player.RotatedRelativePoint(Projectile.Center);
-            Projectile.rotation -= 0.5f;
+            Projectile.rotation -= 0.5f - (Projectile.velocity.Length() * 0.015f);
             Projectile.velocity *= 0.97f;
-            if (Timer == 1)
-            {
-                ShakeModSystem.Shake = 1;
-            }
             if (Timer < 30)
             {
-                if (Main.mouseLeft)
+                if (Main.mouseLeft && Main.myPlayer == Projectile.owner)
                 {
                     Projectile.velocity = Projectile.DirectionTo(Main.MouseWorld) * Projectile.Distance(Main.MouseWorld) / 12;
+                    Projectile.netUpdate = true;
                 }
 
                 player.heldProj = Projectile.whoAmI;
@@ -69,10 +62,12 @@ namespace Stellamod.Projectiles.Thrown
             if (Timer == 99)
             {
                 ShakeModSystem.Shake = 4;
-                float speedXa = -Projectile.velocity.X * Main.rand.NextFloat(.4f, .7f) + Main.rand.NextFloat(-8f, 8f);
-                float speedYa = -Projectile.velocity.Y * Main.rand.Next(0, 0) * 0.01f + Main.rand.Next(-20, 21) * 0.0f;
-
-                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.position.X + speedXa, Projectile.position.Y + speedYa, speedXa * 0, speedYa * 0, ModContent.ProjectileType<AivanKaboom>(), (int)(Projectile.damage * 1.5f), 0f, Projectile.owner, 0f, 0f);
+                if(Main.myPlayer == Projectile.owner)
+                {
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(),Projectile.Center, Vector2.Zero,
+                        ModContent.ProjectileType<AivanKaboom>(), (int)(Projectile.damage * 1.5f), 0f, Projectile.owner, 0f, 0f);
+                }
+          
                 SoundEngine.PlaySound(SoundID.DD2_ExplosiveTrapExplode, Projectile.position);
                 Projectile.Kill();
             }
@@ -91,25 +86,25 @@ namespace Stellamod.Projectiles.Thrown
                 Main.dust[dustnumber].velocity *= 0.3f;
                 Main.dust[dustnumber].noGravity = true;
             }
-
             SpriteEffects Effects = Projectile.spriteDirection != 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
-            Main.spriteBatch.End();
-            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
-            Main.instance.LoadProjectile(Projectile.type);
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            spriteBatch.Restart(blendState: BlendState.Additive);
             Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
 
             // Redraw the projectile with the color not influenced by light
-            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            Vector2 drawOrigin = texture.Size() / 2f;
             for (int k = 0; k < Projectile.oldPos.Length; k++)
             {
-                Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+                Vector2 drawPos = Projectile.oldPos[k] - Main.screenPosition + Projectile.Size / 2f + new Vector2(0f, Projectile.gfxOffY);
                 Color color = Projectile.GetAlpha(Color.Lerp(new Color(254, 231, 255), new Color(100, 200, 255), 1f / Projectile.oldPos.Length * k) * (1f - 1f / Projectile.oldPos.Length * k));
-                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, Effects, 0);
+                spriteBatch.Draw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, Effects, 0);
             }
 
-            Main.spriteBatch.End();
-            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
-            return true;
+            spriteBatch.RestartDefaults();
+            Vector2 drawPosition = Projectile.Center - Main.screenPosition;
+            Color drawColor = Color.White.MultiplyRGB(lightColor);
+            spriteBatch.Draw(texture, drawPosition, null, drawColor, Projectile.rotation, drawOrigin, Projectile.scale, Effects, 0);
+            return false;
         }
 
 
@@ -123,6 +118,13 @@ namespace Stellamod.Projectiles.Thrown
             SoundEngine.PlaySound(SoundID.DD2_ExplosiveTrapExplode, Projectile.position);
             Projectile.Kill();
         }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            //Just rub it
+            return false;
+        }
+
     }
 }
 
