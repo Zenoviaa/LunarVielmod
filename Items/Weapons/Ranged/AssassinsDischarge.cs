dd15b92a02c8ef734f0f60af245086b91bf7ec9a
Scolon <scoloned@gmail.com>
2024-12-16 01:17:51 -0500

    assassassassasasasinate

--- Items/Weapons/Ranged/AssassinsDischarge.cs
+++ Items/Weapons/Ranged/AssassinsDischarge.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
-using Stellamod.Items.Materials;
+using Stellamod.Projectiles.Bow;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 
 namespace Stellamod.Items.Weapons.Ranged
 {
@@ -11,7 +10,7 @@ namespace Stellamod.Items.Weapons.Ranged
     {
 
         public override DamageClass AlternateClass => DamageClass.Magic;
-
+        public int combo;
         public override void SetClassSwappedDefaults()
         {
             Item.damage = 4;
@@ -24,19 +23,18 @@ namespace Stellamod.Items.Weapons.Ranged
             Item.height = 50;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.knockBack = 4;
-            Item.value = Item.buyPrice(0, 5, 0, 0);
+            Item.value = Item.sellPrice(0, 1, 1, 29);
             Item.rare = ItemRarityID.Blue;
-
-            Item.shootSpeed = 16;
+            Item.shootSpeed = 15;
             Item.autoReuse = true;
             Item.DamageType = DamageClass.Ranged;
             Item.shoot = ProjectileID.PurificationPowder;
-            Item.shootSpeed = 15f;
+            Item.shootSpeed = 16f;
             Item.useAmmo = AmmoID.Arrow;
             Item.UseSound = SoundID.Item5;
-            Item.useAnimation = 32;
-            Item.useTime = 32;
-            Item.consumeAmmoOnLastShotOnly = true;
+            Item.useAnimation = 12;
+            Item.useTime = 4; // one third of useAnimation
+            Item.reuseDelay = 28;
             Item.noMelee = true;
         }
         public override Vector2? HoldoutOffset()
@@ -45,5 +43,18 @@ namespace Stellamod.Items.Weapons.Ranged
         }
 
 
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            base.ModifyShootStats(player, ref position, ref velocity, ref type, ref damage, ref knockback);
+            combo++;
+            if (combo == 1)
+            {
+                type = ModContent.ProjectileType<AssassinsArrow>();
+            }
+            if (combo == 3)
+            {
+                combo = 0;
+            }
+        }
     }
 }
