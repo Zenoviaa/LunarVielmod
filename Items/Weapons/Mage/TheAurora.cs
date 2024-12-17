c372031185cebaaacddee4d804c00d0225039bf5
Scolon <scoloned@gmail.com>
2024-12-17 00:19:37 -0500

    i love this rework

--- Items/Weapons/Mage/TheAurora.cs
+++ Items/Weapons/Mage/TheAurora.cs
@@ -12,10 +12,18 @@ using Microsoft.Xna.Framework;
 using Stellamod.Items.Materials;
 using Stellamod.Projectiles.Magic;
 using Terraria.DataStructures;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Trails;
+using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
+using Stellamod.Helpers;
+using Stellamod.Dusts;
+using System.IO;
+using Terraria.Audio;
 
 namespace Stellamod.Items.Weapons.Mage
 { 
-    class TheAurora : ClassSwapItem
+    public class TheAurora : ClassSwapItem
     {
         //Alternate class you want it to change to
         public override DamageClass AlternateClass => DamageClass.Ranged;
@@ -40,8 +48,8 @@ namespace Stellamod.Items.Weapons.Mage
             Item.DamageType = DamageClass.Magic;
             Item.width = 40;
             Item.height = 40;
-            Item.useTime = 25;
-            Item.useAnimation = 25;
+            Item.useTime = 4;
+            Item.useAnimation = 12;
             Item.useStyle = 5;
             Item.knockBack = 4;
             Item.value = Item.sellPrice(0, 1, 20, 0);
@@ -50,36 +58,48 @@ namespace Stellamod.Items.Weapons.Mage
             Item.shoot = ProjectileType<AuroraStar>();
             Item.shootSpeed = 15f;
         }
-        public override void AddRecipes()
-        {
-            Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ItemID.FallenStar, 5);
-            recipe.AddIngredient(ItemType<DarkEssence>(), 10);
-            recipe.AddIngredient(ItemType<AuroreanStarI>(), 15);
-            recipe.AddIngredient(ItemID.FlareGun, 1);
-            recipe.AddTile(TileID.Anvils);
-            recipe.Register();
-        }
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Main.LocalPlayer.GetModPlayer<MyPlayer>().ShakeAtPosition(player.Center, 1024f, 10f);
-
             //Dust Burst Towards Mouse
 
+            int Sound = Main.rand.Next(1, 3);
+            SoundStyle shootSound = new SoundStyle("Stellamod/Assets/Sounds/MiniPistol");
+            if (Sound == 1)
+            {
+         
+
+            }
+            else
+            {
+                shootSound = new SoundStyle("Stellamod/Assets/Sounds/MiniPistol3");
+            }
+            shootSound.PitchVariance = 0.1f;
+            shootSound.Volume = 0.3f;
+            SoundEngine.PlaySound(shootSound, position);
+
+            shootSound = new SoundStyle("Stellamod/Assets/Sounds/Starblast");
+            shootSound.PitchVariance = 0.2f;
+            SoundEngine.PlaySound(shootSound, position);
             float rot = velocity.ToRotation();
             float spread = 0.4f;
 
-            Vector2 offset = new Vector2(1, -0.1f * player.direction).RotatedBy(rot);
-            for (int k = 0; k < 15; k++)
+            Vector2 offset = new Vector2(1,0f).RotatedBy(rot);
+            for (int k = 0; k < 5; k++)
             {
                 Vector2 direction = offset.RotatedByRandom(spread);
 
-                Dust.NewDustPerfect(position + offset * 43, ModContent.DustType<Dusts.GlowDust>(), direction * Main.rand.NextFloat(8), 125, new Color(150, 80, 240), Main.rand.NextFloat(0.2f, 0.5f));
+                Dust.NewDustPerfect(position + offset * 80, ModContent.DustType<Dusts.GlowDust>(), direction * Main.rand.NextFloat(8), 125, Color.Goldenrod, Main.rand.NextFloat(0.2f, 0.5f));
             }
-            Dust.NewDustPerfect(position + offset * 43, ModContent.DustType<Dusts.GlowDust>(), new Vector2(0, 0), 125, new Color(150, 80, 40), 1);
-            Dust.NewDustPerfect(player.Center + offset * 43, ModContent.DustType<Dusts.TSmokeDust>(), Vector2.UnitY * -2 + offset.RotatedByRandom(spread), 150, new Color(60, 55, 50) * 0.5f, Main.rand.NextFloat(0.5f, 1));
-            return base.Shoot(player, source, position, velocity, type, damage, knockback);
+            Dust.NewDustPerfect(position + offset * 80, ModContent.DustType<Dusts.GlowDust>(), new Vector2(0, 0), 125, Color.Goldenrod, 1);
+            Dust.NewDustPerfect(player.Center + offset * 80, ModContent.DustType<Dusts.TSmokeDust>(), Vector2.UnitY * -2 + offset.RotatedByRandom(spread), 150, new Color(60, 55, 50) * 0.5f, Main.rand.NextFloat(0.5f, 1));
+
+            for(int k = 0; k < 2; k++)
+            {
+                Projectile.NewProjectile(source, position, -velocity.RotatedByRandom(MathHelper.ToRadians(65)), type, damage, knockback, player.whoAmI);
+            }
+
+            return false;
 
         }
     }
--- Projectiles/Magic/AuroraStar.cs
+++ Projectiles/Magic/AuroraStar.cs
@@ -1,131 +1,136 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Dusts;
 using Stellamod.Helpers;
 using Stellamod.Trails;
-using System;
-using System.Diagnostics.Metrics;
-using System.IO.Pipelines;
+using System.IO;
 using Terraria;
-using Terraria.Audio;
 using Terraria.GameContent;
 using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 
 namespace Stellamod.Projectiles.Magic
 {
     public class AuroraStar : ModProjectile
     {
+        private ref float Timer => ref Projectile.ai[0];
+        private Player Owner => Main.player[Projectile.owner];
+        private Vector2 TargetPosition;
+        private Color MainColor
+        {
+            get
+            {
+                return Color.Goldenrod;
+            }
+        }
         public override void SetStaticDefaults()
         {
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 12;
-            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+            base.SetStaticDefaults();
+            ProjectileID.Sets.TrailCacheLength[Type] = 24;
+            ProjectileID.Sets.TrailingMode[Type] = 2;
         }
 
         public override void SetDefaults()
         {
-            Projectile.width = 18;
-            Projectile.height = 18;
+            base.SetDefaults();
+            Projectile.width = 4;
+            Projectile.height = 4;
             Projectile.friendly = true;
-            Projectile.hostile = false;
-            Projectile.penetrate = 10;
-            Projectile.timeLeft = 310;
-            Projectile.tileCollide = true;
-            Projectile.damage = 45;
-            Projectile.aiStyle = -1;
-            Projectile.scale = 1f;
-            Projectile.rotation = 1.5f;
+            Projectile.timeLeft = 360;
+            Projectile.tileCollide = false;
         }
-        public override Color? GetAlpha(Color lightColor)
+
+        public override void SendExtraAI(BinaryWriter writer)
         {
-            return Color.White;
+            base.SendExtraAI(writer);
+            writer.WriteVector2(TargetPosition);
         }
+
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            base.ReceiveExtraAI(reader);
+            TargetPosition = reader.ReadVector2();
+        }
+        public override void AI()
+        {
+            base.AI();
+
+            Timer++;
+            if(Timer > 120)
+            {
+                Projectile.tileCollide = true;
+            }
+
+            if (Timer % 36 == 0)
+            {
+              //  Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<GlyphDust>(), Projectile.velocity * 0.1f, 0, MainColor, Main.rand.NextFloat(1f, 3f)).noGravity = true;
+            }
+            if (Main.myPlayer == Projectile.owner && TargetPosition == Vector2.Zero)
+            {
+                TargetPosition = Main.MouseWorld;
+                Projectile.netUpdate = true;
+            }
+
+            float maxDegreesRotate = MathHelper.Lerp(0.2f, 16f, Timer / 30f);
+
+            Projectile.extraUpdates = (int)MathHelper.Lerp(1, 3f, Timer / 30f);
+            Projectile.velocity = ProjectileHelper.SimpleHomingVelocity(Projectile, TargetPosition, maxDegreesRotate);
+            Projectile.rotation = Projectile.velocity.ToRotation() + Timer * 0.05f;
+        }
+
         public PrimDrawer TrailDrawer { get; private set; } = null;
         public float WidthFunction(float completionRatio)
         {
-            float baseWidth = Projectile.scale * Projectile.width * 1.3f;
-            return MathHelper.SmoothStep(baseWidth, 0.1f, completionRatio);
+            float baseWidth = Projectile.scale * Projectile.width;
+            return MathHelper.SmoothStep(baseWidth, 0.5f, completionRatio);
         }
+
         public Color ColorFunction(float completionRatio)
         {
-            return Color.Lerp(Color.HotPink, Color.Orange, completionRatio) * 0.7f;
+            return Color.Lerp(Color.Goldenrod, Color.CadetBlue, completionRatio) * 0.7f;
         }
+
         public override bool PreDraw(ref Color lightColor)
         {
-            Vector3 huntrianColorXyz = DrawHelper.HuntrianColorOscillate(
-                Color.Orange.ToVector3(),
-                Color.HotPink.ToVector3(),
-                new Vector3(3, 3, 3), 0);
+            Texture2D texture2D4 = ModContent.Request<Texture2D>("Stellamod/Effects/Masks/DimLight").Value;
+            Color drawColor = MainColor;
+            drawColor.A = 0;
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            for (int i = 0; i < 2; i++)
+                spriteBatch.Draw(texture2D4, Projectile.Center - Main.screenPosition, null, drawColor, Projectile.rotation, new Vector2(32, 32), 0.5f, SpriteEffects.None, 0f);
 
-            DrawHelper.DrawDimLight(Projectile, huntrianColorXyz.X, huntrianColorXyz.Y, huntrianColorXyz.Z, Color.Orange, Color.White, 0);
-
-
-            if (Main.rand.NextBool(5))
-            {
-                int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.YellowStarDust, 0f, 0f, 150, Color.White, 1f);
-                Main.dust[dustnumber].velocity *= 0.3f;
-                Main.dust[dustnumber].noGravity = true;
-            }
-            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, new Vector2(texture.Width / 2, texture.Height / 2), 1f, Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0f);
             TrailDrawer ??= new PrimDrawer(WidthFunction, ColorFunction, GameShaders.Misc["VampKnives:BasicTrail"]);
-            GameShaders.Misc["VampKnives:BasicTrail"].SetShaderTexture(TrailRegistry.StarTrail);
+            GameShaders.Misc["VampKnives:BasicTrail"].SetShaderTexture(TrailRegistry.BeamTrail);
             TrailDrawer.DrawPrims(Projectile.oldPos, Projectile.Size * 0.5f - Main.screenPosition, 155);
 
+            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, new Vector2(texture.Width / 2, texture.Height / 2), Projectile.scale, Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0f);
             return false;
-        }
-
-        public override void AI()
-        {
-            Projectile.ai[1]++;
-            if (Projectile.ai[1] <= 1)
-            {
-                SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/Aurora"), Projectile.position);
-            }
-            Projectile.rotation += 0.3f;
         }
 
         public override void OnKill(int timeLeft)
         {
-
-            for (int i = 0; i < 15; i++)
+            base.OnKill(timeLeft);
+            for (float f = 0; f < 1; f++)
             {
-                Dust.NewDust(Projectile.Center, Projectile.width, Projectile.height, DustID.Pixie, 0f, -2f, 0, default(Color), .8f);
-                Dust.NewDust(Projectile.Center, Projectile.width, Projectile.height, DustID.Pixie, 0f, -2f, 0, default(Color), .8f);
+                Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<GlyphDust>(),
+                    (Vector2.One * Main.rand.NextFloat(0.2f, 5f)).RotatedByRandom(19.0), 0, MainColor, Main.rand.NextFloat(1f, 3f)).noGravity = true;
             }
-
-            for (int i = 0; i < 30; i++)
+            for (float i = 0; i < 4; i++)
             {
-                Dust.NewDustPerfect(base.Projectile.Center, DustID.InfernoFork, (Vector2.One * Main.rand.Next(1, 4)).RotatedByRandom(19.0), 0, default(Color), 1f).noGravity = true;
+                float progress = i / 4f;
+                float rot = progress * MathHelper.ToRadians(360);
+                rot += Main.rand.NextFloat(-0.5f, 0.5f);
+                Vector2 offset = rot.ToRotationVector2() * 24;
+                var particle = FXUtil.GlowCircleDetailedBoom1(Projectile.Center,
+                    innerColor: Color.White,
+                    glowColor: MainColor,
+                    outerGlowColor: Color.Black,
+                    duration: Main.rand.NextFloat(6, 12),
+                    baseSize: Main.rand.NextFloat(0.01f, 0.05f));
+                particle.Rotation = rot + MathHelper.ToRadians(45);
             }
-            float ScaleMult = 2.33f;
         }
-        public override bool PreAI()
-        {
-            if (Main.rand.Next(3) == 1)
-            {
-                int dust1 = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, 244, 0f, 0f, 0, default(Color), 1f);
-                Main.dust[dust1].scale = 2f;
-                Main.dust[dust1].noGravity = true;
-                int dust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.GoldCoin, 0f, 0f, 0, default(Color), 1f);
-                Main.dust[dust].scale = 2f;
-                Main.dust[dust].noGravity = true;
-            }
-            if (Main.rand.Next(3) == 1)
-            {
-                int dust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.GoldCoin, 0f, 0f, 0, default(Color), 1f);
-                Main.dust[dust].scale = 2f;
-                Main.dust[dust].noGravity = true;
-            }
-
-            Projectile.alpha -= 40;
-            if (Projectile.alpha < 0)
-                Projectile.alpha = 0;
-
-            Projectile.spriteDirection = Projectile.direction;
-            return true;
-        }
-
     }
 }
