a02f3401da7ec0d7b997ce8cf40a5ad5b64677d1
Scolon <scoloned@gmail.com>
2024-12-20 20:38:20 -0500

    moved over crystal moon wands

---
+++ Items/MoonlightMagic/Weapons/HexxingStaff.cs
@@ -0,0 +1,30 @@
+﻿using Stellamod.Items.MoonlightMagic.Forms;
+
+namespace Stellamod.Items.MoonlightMagic.Weapons
+{
+    internal class HexxingStaff : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 10;
+            Item.shootSpeed = 16;
+            Item.useTime = 23;
+            Item.useAnimation = 23;
+            Size = 8;
+            TrailLength = 30;
+            Form = FormRegistry.Snowglobe.Value;
+        }
+
+
+        public override int GetNormalSlotCount()
+        {
+            return 3;
+        }
+
+        public override int GetTimedSlotCount()
+        {
+            return 0;
+        }
+    }
+}
