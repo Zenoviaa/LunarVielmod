a27d2b8774be41b1cb4722a46a8331a875ccd7de
Scolon <scoloned@gmail.com>
2025-07-02 18:15:22 -0400

    title system back

---
+++ Core/TitleSystem/TitleCardUIState.cs
@@ -0,0 +1,20 @@
+﻿using Terraria.UI;
+
+
+namespace Stellamod.Core.TitleSystem
+{
+    internal class TitleCardUIState : UIState
+    {
+        public TitleCardUI titleCardUI;
+        public TitleCardUIState() : base()
+        {
+
+        }
+
+        public override void OnInitialize()
+        {
+            titleCardUI = new TitleCardUI();
+            Append(titleCardUI);
+        }
+    }
+}
