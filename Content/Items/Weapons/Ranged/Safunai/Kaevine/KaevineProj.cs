a5b24440e1555aa1ff686858a54aa60e6f242190
Scolon <scoloned@gmail.com>
2025-07-08 14:40:20 -0400

    damage rework

--- Content/Items/Weapons/Ranged/Safunai/Kaevine/KaevineProj.cs
+++ Content/Items/Weapons/Ranged/Safunai/Kaevine/KaevineProj.cs
@@ -63,30 +63,12 @@ namespace Stellamod.Content.Items.Weapons.Ranged.Safunai.Kaevine
             SoundEngine.PlaySound(SoundID.Item17, target.position);
             if (Slam)
             {
-                for (int i = 0; i < Main.rand.Next(1, 4); i++)
-                {
-                    Vector2 stingerVelocity = Vector2.One.RotatedBy(MathHelper.ToRadians(Main.rand.Next(0, 360))) * Main.rand.NextFloat(6, 8);
-                    int p = Projectile.NewProjectile(Projectile.GetSource_FromThis(),
-                        target.Center.X, target.Center.Y,
-                        stingerVelocity.X, stingerVelocity.Y,
-                        ProjectileID.QueenBeeStinger, Projectile.damage * 3, 0f, Projectile.owner);
-
-                    Projectile stingerProj = Main.projectile[p];
-                    stingerProj.hostile = false;
-                    stingerProj.friendly = true;
-                    stingerProj.usesLocalNPCImmunity = true;
-                    stingerProj.penetrate = -1;
-                    stingerProj.localNPCHitCooldown = -1;
-                    stingerProj.netUpdate = true;
-                }
                 FXUtil.GlowCircleBoom(target.Center,
                     innerColor: Color.White,
                     glowColor: Color.Yellow,
                     outerGlowColor: Color.Red, duration: 15, baseSize: 0.12f);
 
-
                 FXUtil.ShakeCamera(target.Center, 1024, 32);
-
                 SoundStyle hitSound = AssetRegistry.Sounds.Melee.Vinger2;
                 hitSound.PitchVariance = 0.2f;
                 SoundEngine.PlaySound(hitSound, target.position);
@@ -99,11 +81,11 @@ namespace Stellamod.Content.Items.Weapons.Ranged.Safunai.Kaevine
                 {
                     Dust.NewDustPerfect(target.Center, ModContent.DustType<TSmokeDust>(), (Vector2.One * Main.rand.Next(1, 5)).RotatedByRandom(19.0), 0, Color.DarkOliveGreen, 1f).noGravity = true;
                 }
+
                 FXUtil.GlowCircleBoom(target.Center,
                     innerColor: Color.Yellow,
                     glowColor: Color.Green,
                     outerGlowColor: Color.Black, duration: 25, baseSize: 0.24f);
-
             }
             else
             {
@@ -115,27 +97,7 @@ namespace Stellamod.Content.Items.Weapons.Ranged.Safunai.Kaevine
                 SoundStyle hitSound = AssetRegistry.Sounds.Melee.Vinger;
                 hitSound.PitchVariance = 0.2f;
                 SoundEngine.PlaySound(hitSound, target.position);
-                for (int i = 0; i < 1; i++)
-                {
-                    Vector2 stingerVelocity = Vector2.One.RotatedBy(MathHelper.ToRadians(Main.rand.Next(0, 360))) * Main.rand.NextFloat(4, 6);
-                    int p = Projectile.NewProjectile(Projectile.GetSource_FromThis(),
-                        target.Center.X, target.Center.Y,
-                        stingerVelocity.X, stingerVelocity.Y,
-                        ProjectileID.QueenBeeStinger, Projectile.damage * 2, 0f, Projectile.owner);
-
-                    Projectile stingerProj = Main.projectile[p];
-                    stingerProj.hostile = false;
-                    stingerProj.friendly = true;
-                    stingerProj.usesLocalNPCImmunity = true;
-                    stingerProj.penetrate = -1;
-                    stingerProj.localNPCHitCooldown = -1;
-                    stingerProj.netUpdate = true;
-
-                }
             }
-
         }
-
-
     }
 }
---
+++ Core/DamageRework/MoonPlayer.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Core.DamageRework
+{
+
+
+    public class MoonPlayer : ModPlayer
+    {
+        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+        {
+            //Disbale damage variation on everything
+            base.ModifyHitNPC(target, ref modifiers);
+            modifiers.DamageVariationScale *= 0;
+            if (ModContent.GetInstance<StellamodClientConfig>().RedDamageNumbersToggle)
+            {
+                modifiers.HideCombatText();
+            }
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            base.OnHitNPC(target, hit, damageDone);
+            if (ModContent.GetInstance<StellamodClientConfig>().RedDamageNumbersToggle)
+            {
+                CombatText.NewText(target.getRect(), Color.Lerp(Color.Red, Color.White, 0.25f), hit.Damage, dramatic: hit.Crit);
+            }
+        }
+
+        public override void PostUpdate()
+        {
+
+            if (Player.ZoneSnow)
+            {
+
+                //Update Rain
+                Main.raining = true;
+
+                //That way, if it is already raining, it won't be overriden
+                //And if it is not raining, it'll just be permanent until you leave the biome
+                if (Main.rainTime <= 2)
+                    Main.rainTime = 2;
+
+
+                Main.maxRaining = 0.8f;
+
+                Main.maxRain = 140;
+
+            }
+
+        }
+    }
+}
