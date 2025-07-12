eae29b898c4aee06703e929a80d94a5c491b1b8f
Scolon <scoloned@gmail.com>
2025-07-12 04:37:09 -0400

    g

---
+++ Content/Items/Accessories/HeartPiece.cs
@@ -0,0 +1,32 @@
+﻿using Stellamod.Core.DashSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Items.Accessories
+{
+    internal class HeartPiece : ModItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.width = 30;
+            Item.height = 40;
+            Item.rare = ItemRarityID.Green;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            base.UpdateAccessory(player, hideVisual);
+            DashPlayer dashPlayer = player.GetModPlayer<DashPlayer>();
+            dashPlayer.MaxDashCount += 2;
+        }
+    }
+}
