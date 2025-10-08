08959b0bbc68067a2ac0eef5ef635b37bb5f4be9
Scolon <scoloned@gmail.com>
2025-10-08 15:34:57 -0400

    add mystery insource

---
+++ Items/Insources/MysteryInsource.cs
@@ -0,0 +1,35 @@
+﻿using Stellamod.Core.XixianFlaskSystem;
+using Terraria;
+using Terraria.ID;
+
+namespace Stellamod.Items.Insources
+{
+    public class MysteryInsource : InsourceItem
+    {
+        public override int GetAddedTime()
+        {
+            return 60 * 15;
+        }
+        public override void UseInsource(FlaskPlayer flaskPlayer)
+        {
+            base.UseInsource(flaskPlayer);
+            Player player = flaskPlayer.Player;
+            int[] buffPool = new int[]
+            {
+                BuffID.AmmoReservation,
+                BuffID.Archery,
+                BuffID.Endurance,
+                BuffID.Regeneration,
+                BuffID.Swiftness,
+                BuffID.Ironskin,
+                BuffID.ManaRegeneration,
+                BuffID.MagicPower,
+                BuffID.Lifeforce,
+                BuffID.Lucky,
+                BuffID.WellFed3
+            };
+
+            player.AddBuff(buffPool[Main.rand.Next(0, buffPool.Length)], 60 * 15);
+        }
+    }
+}
