1cdb04b292122afae888f5dc620cd07aac8ab9c0
Scolon <scoloned@gmail.com>
2025-10-10 14:27:29 -0400

    add wiggle diggle

--- Content/Areas/WondrousDarkspace/WeaponsWD/WiggleDiggle.cs
+++ Content/Areas/WondrousDarkspace/WeaponsWD/WiggleDiggle.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
-
+using Microsoft.Xna.Framework.Graphics;
 using Stellamod.Gores;
-using Stellamod.Projectiles.Gun;
+using Stellamod.Helpers;
+using Stellamod.Items;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Materials.Molds;
+using Stellamod.Trails;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Stellamod.Items.Weapons.Ranged
+namespace Stellamod.Content.Areas.WondrousDarkspace.WeaponsWD
 {
     public class WiggleDiggle : ClassSwapItem
     {
@@ -34,6 +38,12 @@ namespace Stellamod.Items.Weapons.Ranged
             Item.shoot = ModContent.ProjectileType<WiggleDiggleProj>();
             Item.shootSpeed = 19;
 
+        }
+
+        public override void AddRecipes()
+        {
+            base.AddRecipes();
+            this.RegisterBrew<BlankGun, MiracleThread>();
         }
 
         public override Vector2? HoldoutOffset()
@@ -91,6 +101,111 @@ namespace Stellamod.Items.Weapons.Ranged
             }
 
             return base.Shoot(player, source, position, velocity, type, damage, knockback);
+        }
+    }
+
+    public class WiggleDiggleProj : ModProjectile,
+        IPixelPrimitiveDrawer
+    {
+        private int _color;
+        private ref float Timer => ref Projectile.ai[0];
+        public override string Texture => TextureRegistry.EmptyTexture;
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Type] = 6;
+            ProjectileID.Sets.TrailingMode[Type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.friendly = true;
+        }
+
+        public override void AI()
+        {
+            Timer++;
+            if (Timer == 1)
+            {
+                _color = Main.rand.Next(4);
+            }
+
+            Projectile.velocity.Y += 0.15f;
+            Projectile.rotation = Projectile.velocity.ToRotation();
+        }
+
+        public float WidthFunction(float completionRatio)
+        {
+            return 2;
+        }
+
+        public Color ColorFunction(float completionRatio)
+        {
+            switch (_color)
+            {
+                default:
+                case 0:
+                    return new Color(5, 7, 203);
+                case 1:
+                    return new Color(255, 51, 240);
+                case 2:
+                    return new Color(255, 229, 0);
+                case 3:
+                    return Color.White;
+            }
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            int goreType;
+            switch (_color)
+            {
+                default:
+                case 0:
+                    goreType = ModContent.GoreType<RibbonBlue>();
+                    break;
+                case 1:
+                    goreType = ModContent.GoreType<RibbonPink>();
+                    break;
+                case 2:
+                    goreType = ModContent.GoreType<RibbonYellow>();
+                    break;
+                case 3:
+                    goreType = ModContent.GoreType<RibbonWhite>();
+                    break;
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                Vector2 velocity = Main.rand.NextVector2Circular(4, 4);
+                Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, velocity,
+                  goreType);
+            }
+        }
+
+        public PrimitiveTrail BeamDrawer;
+        public Vector2[] TrailPos;
+        public void DrawPixelPrimitives(SpriteBatch spriteBatch)
+        {
+            BeamDrawer ??= new PrimitiveTrail(WidthFunction, ColorFunction, null, true, TrailRegistry.LaserShader);
+            BeamDrawer.SpecialShader = null;
+
+            if (TrailPos == null)
+            {
+                TrailPos = new Vector2[Projectile.oldPos.Length];
+                for (int i = 0; i < TrailPos.Length; i++)
+                {
+                    Projectile.oldPos[i] = Projectile.position;
+                }
+            }
+
+            for (int i = 0; i < TrailPos.Length; i++)
+            {
+                TrailPos[i] = Projectile.oldPos[i];
+                TrailPos[i] += new Vector2(VectorHelper.Osc(0, 16, offset: i));
+            }
+            BeamDrawer.DrawPixelated(TrailPos, -Main.screenPosition, 32);
+            Main.spriteBatch.ExitShaderRegion();
         }
     }
 }
