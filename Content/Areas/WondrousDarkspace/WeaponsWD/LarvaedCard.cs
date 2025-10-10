cadf929255a67dec0caf1d68d1c5a7a8b7d2db33
Scolon <scoloned@gmail.com>
2025-10-10 14:24:21 -0400

    add larvaed card

--- Content/Areas/WondrousDarkspace/WeaponsWD/LarvaedCard.cs
+++ Content/Areas/WondrousDarkspace/WeaponsWD/LarvaedCard.cs
@@ -1,9 +1,11 @@
-﻿using Stellamod.Items.Materials;
+﻿using Stellamod.Items;
+using Stellamod.Items.Materials;
 using Stellamod.Items.Materials.Molds;
+using Stellamod.Items.Weapons.Igniters;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace Stellamod.Items.Weapons.Igniters
+namespace Stellamod.Content.Areas.WondrousDarkspace.WeaponsWD
 {
     public class LarvaedCard : BaseIgniterCard
     {
@@ -19,10 +21,11 @@ namespace Stellamod.Items.Weapons.Igniters
             base.SetDefaults();
             Item.damage = 13;
         }
+
         public override void AddRecipes()
         {
             base.AddRecipes();
-            this.RegisterBrew(mold: ModContent.ItemType<BlankCard>(), material: ModContent.ItemType<ConvulgingMater>());
+            this.RegisterBrew(mold: ModContent.ItemType<BlankCard>(), material: ModContent.ItemType<HypnotizedSoul>());
         }
     }
 }
