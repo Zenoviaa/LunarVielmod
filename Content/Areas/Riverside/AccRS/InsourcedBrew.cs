7a9706932ced65a1531a35ddf9a657a13a094788
Scolon <scoloned@gmail.com>
2025-10-11 01:54:08 -0400

    insourced brew

---
+++ Content/Areas/Riverside/AccRS/InsourcedBrew.cs
@@ -0,0 +1,37 @@
+﻿using Stellamod.Core.XixianFlaskSystem;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Riverside.AccRS
+{
+    public class InsourcedBrew : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Book of Wooden Illusion");
+            /* Tooltip.SetDefault("Increased Regeneration!" +
+				"\n +3% damage" +
+				"\n Increases crit strike change by 5% "); */
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 28;
+            Item.value = Item.sellPrice(silver: 25);
+            Item.rare = ItemRarityID.Green;
+            Item.accessory = true;
+
+
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<FlaskPlayer>().maxInsourceCount += 1;
+        }
+    }
+}
