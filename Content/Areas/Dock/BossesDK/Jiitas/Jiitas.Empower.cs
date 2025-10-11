9b63c6bdf0678ac788bd630364abdd70c6c1b029
Scolon <scoloned@gmail.com>
2025-10-11 18:36:22 -0400

    add back jiitas

---
+++ Content/Areas/Dock/BossesDK/Jiitas/Jiitas.Empower.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Assets;
+using Terraria;
+using Terraria.Audio;
+
+namespace Stellamod.Content.Areas.Dock.BossesDK.Jiitas
+{
+    public partial class Jiitas
+    {
+        public float EmpowerStartupTime
+        {
+            get
+            {
+                return 120;
+            }
+        }
+        private void AI_Empower()
+        {
+            switch (ActionStep)
+            {
+                case 0:
+                    AI_EmpowerLaugh();
+                    break;
+            }
+        }
+
+        private void AI_EmpowerLaugh()
+        {
+            Timer++;
+            if (Timer == 1)
+            {
+                CombatText.NewText(NPC.getRect(), Color.Yellow, "Power Up!", true);
+                SoundStyle laughSound = AssetRegistry.Sounds.Jiitas.JiitasLaugh;
+                laughSound.PitchVariance = 0.2f;
+                SoundEngine.PlaySound(laughSound, NPC.position);
+            }
+            NPC.TargetClosest();
+            PlayAnimation(AnimationState.Laugh);
+            Empowered = true;
+            if (Timer >= EmpowerStartupTime)
+            {
+                SwitchState(ActionState.Idle);
+            }
+        }
+    }
+}
