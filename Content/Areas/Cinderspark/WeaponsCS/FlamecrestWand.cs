1a04063e15c0e9d890737a622c55ded5b3c1fa06
Zenovia <calamityjewelstudios@gmail.com>
2025-10-29 22:26:32 -0400

    Flamecrestwand

---
+++ Content/Areas/Cinderspark/WeaponsCS/FlamecrestWand.cs
@@ -0,0 +1,49 @@
+﻿using Stellamod.Content.Items.MoonlightMagic;
+using Stellamod.Content.Items.MoonlightMagic.Elements;
+using Stellamod.Content.Items.MoonlightMagic.Forms;
+using Stellamod.Items;
+using Stellamod.Items.Harvesting;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Materials.Molds;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Cinderspark.WeaponsCS
+{
+
+    public class FlamecrestWand : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Form = FormRegistry.Crescent.Value;
+            Item.damage = 125;
+            Item.mana = 50;
+        }
+
+        public override int GetNormalSlotCount()
+        {
+            return 1;
+        }
+
+        public override int GetTimedSlotCount()
+        {
+            return 4;
+        }
+
+        public override void ModifyElementPreferences(List<int> elements)
+        {
+            base.ModifyElementPreferences(elements);
+            elements.Add(ModContent.ItemType<PrimeMagicElement>());
+            elements.Add(ModContent.ItemType<RadianceElement>());
+            elements.Add(ModContent.ItemType<WindElement>());
+        }
+
+        public override void AddRecipes()
+        {
+            base.AddRecipes();
+            this.RegisterBrew<Cinderscrap, BlankStaff>();
+        }
+
+    }
+}
