63b9f46fa176cfb0e385b2ac1e936ab841cca21c
Scolon <scoloned@gmail.com>
2025-10-29 20:16:43 -0400

    ivyn sharprod

---
+++ Content/Areas/SpringHills/AccSH/IvynSharprod.cs
@@ -0,0 +1,30 @@
+﻿using Stellamod.Items;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Materials.Molds;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.SpringHills.AccSH
+{
+    public class IvynSharprod : ModItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.accessory = true;
+            Item.rare = ItemRarityID.Blue;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            base.UpdateAccessory(player, hideVisual);
+            player.GetArmorPenetration(DamageClass.Generic) += 3;
+        }
+        public override void AddRecipes()
+        {
+            base.AddRecipes();
+            this.RegisterBrew<Ivythorn, BlankAccessory>();
+        }
+    }
+}
