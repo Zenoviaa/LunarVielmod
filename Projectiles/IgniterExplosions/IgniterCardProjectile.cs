40f146af69f2a6f1e025c8513f3ca2be0d68ad87
Scolon <scoloned@gmail.com>
2025-10-31 15:47:42 -0400

    get out

--- Projectiles/IgniterExplosions/IgniterCardProjectile.cs
+++ Projectiles/IgniterExplosions/IgniterCardProjectile.cs
@@ -2,11 +2,9 @@
 using Microsoft.Xna.Framework.Graphics;
 using Stellamod.Core.IgnitersNPowders;
 using Stellamod.Helpers;
-using Stellamod.Trails;
 using System.IO;
 using Terraria;
 using Terraria.Audio;
-using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
 
