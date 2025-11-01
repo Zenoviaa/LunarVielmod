c1f13ddb9ce810b4366a00a35b7830103b9ddbac
Scolon <scoloned@gmail.com>
2025-11-01 16:55:41 -0400

    ice cube machine

--- Content/Areas/Snow/WeaponsSN/IceCubeMaker.cs
+++ Content/Areas/Snow/WeaponsSN/IceCubeMaker.cs
@@ -1,14 +1,80 @@
 ﻿using Microsoft.Xna.Framework;
-
+using Stellamod.Content.Items.Materials;
+using Stellamod.Core.GunSystem;
 using Stellamod.Helpers;
-using Stellamod.Trails;
+using Stellamod.Items;
+using Stellamod.Items.Materials.Molds;
 using Terraria;
 using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Stellamod.Projectiles.Gun
+namespace Stellamod.Content.Areas.Snow.WeaponsSN
 {
+    public class IceCubeMaker : BaseGun
+    {
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.width = 72;
+            Item.height = 40;
+            Item.rare = ItemRarityID.Green;
+
+            //Damage
+            Item.damage = 6;
+            Item.DamageType = DamageClass.Ranged;
+
+            Item.shoot = ModContent.ProjectileType<IceCubeMakerProj>();
+            Item.shootSpeed = 25;
+
+            //Use
+            Item.useTime = 50;
+            Item.useAnimation = 50;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.UseSound = SoundID.NPCHit11;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-16, -2);
+        }
+
+        public override bool GunShot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            type = ModContent.ProjectileType<IceCubeMakerProj>();
+            float rot = velocity.ToRotation();
+            float spread = 0.4f;
+
+            SoundStyle soundStyle = SoundRegistry.ExplosionCrystalShard;
+            soundStyle.PitchVariance = 0.33f;
+            SoundEngine.PlaySound(soundStyle, position);
+            Vector2 offset = new Vector2(2, -0f * player.direction).RotatedBy(rot);
+            float distance = 32;
+            int numProjectiles = 3;
+            for (int p = 0; p < numProjectiles; p++)
+            {
+                Dust.NewDustPerfect(position + offset * distance, ModContent.DustType<Dusts.GlowDust>(), new Vector2(0, 0), 125, Color.LightCyan, 1);
+                Dust.NewDustPerfect(player.Center + offset * distance, ModContent.DustType<Dusts.TSmokeDust>(), Vector2.UnitY * -2 + offset.RotatedByRandom(spread), 150, Color.LightCyan * 0.5f, Main.rand.NextFloat(0.5f, 1));
+
+
+
+                // Rotate the velocity randomly by 30 degrees at max.
+                Vector2 newVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(45));
+                newVelocity *= 1f - Main.rand.NextFloat(0.3f);
+                Projectile.NewProjectileDirect(source, position, newVelocity, type, damage, knockback, player.whoAmI);
+            }
+            return base.GunShot(player, source, position, velocity, type, damage, knockback);
+        }
+        public override void AddRecipes()
+        {
+            base.AddRecipes();
+            this.RegisterBrew(mold: ModContent.ItemType<BlankGun>(), material: ModContent.ItemType<WinterbornShard>());
+        }
+    }
+
+
     public class IceCubeMakerProj : ModProjectile
     {
         private ref float Timer => ref Projectile.ai[0];
@@ -39,7 +105,8 @@ namespace Stellamod.Projectiles.Gun
             {
                 DamageModifier = 1;
             }
-            Projectile.velocity *= 0.98f;
+            if(Projectile.velocity.Length() > 1f)
+                Projectile.velocity *= 0.94f;
             Projectile.rotation += Projectile.velocity.Length() * 0.05f;
             AI_Collide();
             Visuals();
@@ -47,18 +114,19 @@ namespace Stellamod.Projectiles.Gun
 
         private void AI_Collide()
         {
-            if (Timer < 30)
+            if (Timer < 45)
+                return;
+
+            if (!this.OwnedByLocalClient())
                 return;
             Rectangle myRect = Projectile.getRect();
-            for (int i = 0; i < Main.maxProjectiles; i++)
+            foreach (var p in Main.ActiveProjectiles)
             {
-                Projectile p = Main.projectile[i];
-                if (!p.active)
-                    continue;
                 if (p.type != Projectile.type)
                     continue;
                 if (p == Projectile)
                     continue;
+
                 Rectangle otherRect = p.getRect();
                 if (Projectile.Colliding(myRect, otherRect))
                 {
@@ -77,6 +145,8 @@ namespace Stellamod.Projectiles.Gun
                     Vector2 bounceVelocity = -Projectile.velocity * 1.5f;
                     Projectile.velocity = bounceVelocity.RotatedByRandom(MathHelper.PiOver4 / 4);
                     Projectile.ai[1] += 0.2f;
+                    Projectile.netUpdate = true;
+                    p.netUpdate = true;
                 }
             }
         }
@@ -98,21 +168,9 @@ namespace Stellamod.Projectiles.Gun
                 }
             }
         }
-
-        public float WidthFunction(float completionRatio)
-        {
-            float baseWidth = Projectile.scale * Projectile.width;
-            return MathHelper.SmoothStep(baseWidth, 3.5f, completionRatio);
-        }
-
-        public Color ColorFunction(float completionRatio)
-        {
-            return Color.Lerp(Color.LightCyan, Color.Transparent, completionRatio) * 0.2f;
-        }
-
         public override bool PreDraw(ref Color lightColor)
         {
-            DrawHelper.DrawSimpleTrail(Projectile, WidthFunction, ColorFunction, TrailRegistry.StarTrail);
+
             return base.PreDraw(ref lightColor);
         }
 
@@ -128,8 +186,9 @@ namespace Stellamod.Projectiles.Gun
             Vector2 bounceVelocity = -Projectile.velocity * 1.5f;
             Projectile.velocity = bounceVelocity.RotatedByRandom(MathHelper.PiOver4 / 16);
             Projectile.velocity += -Vector2.UnitY * 8;
-            DamageModifier += 0.1f;
             Projectile.netUpdate = true;
+            DamageModifier += 0.1f;
+
             if (Main.rand.NextBool(8))
             {
                 target.AddBuff(BuffID.Frostburn, 120);
