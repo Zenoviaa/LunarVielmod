59221ec364385b193dc7dd642873e188c2876382
Scolon <scoloned@gmail.com>
2025-11-01 01:03:54 -0400

    ivyakenwand

---
+++ Content/Areas/SpringHills/WeaponsSH/IvyakenWand.cs
@@ -0,0 +1,36 @@
+﻿using Stellamod.Content.Items.Materials;
+using Stellamod.Content.Items.MoonlightMagic;
+using Stellamod.Items;
+using Stellamod.Items.Materials.Molds;
+
+namespace Stellamod.Content.Areas.SpringHills.WeaponsSH
+{
+    public class IvyakenWand : BaseStaff
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.damage = 10;
+            Item.shootSpeed = 10;
+            Item.useTime = 18;
+            Item.useAnimation = 36;
+            Size = 8;
+            TrailLength = 16;
+        }
+
+        public override int GetNormalSlotCount()
+        {
+            return 2;
+        }
+
+        public override int GetTimedSlotCount()
+        {
+            return 1;
+        }
+        public override void AddRecipes()
+        {
+            base.AddRecipes();
+            this.RegisterBrew<Ivythorn, BlankStaff>();
+        }
+    }
+}
