bc752561d70af3aab5f12f8a45bbeb9b2ae68d1a
Scolon <scoloned@gmail.com>
2025-10-31 22:17:13 -0400

    leather glove rework

---
+++ Content/Areas/Fable/AccFB/LeatherGlove.cs
@@ -0,0 +1,28 @@
+using Stellamod.Items;
+using Stellamod.Items.Materials;
+using Stellamod.Items.Materials.Molds;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Fable.AccFB
+{
+    public class LeatherGlove : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToAccessory();
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetAttackSpeed(DamageClass.Melee) += 0.05f;
+        }
+
+        public override void AddRecipes()
+        {
+            base.AddRecipes();
+            this.RegisterBrew(mold: ModContent.ItemType<BlankAccessory>(),
+                material: ModContent.ItemType<AlcadizScrap>());
+        }
+    }
+}
