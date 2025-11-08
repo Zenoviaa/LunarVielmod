f64cb8fef3c59b2627091e084b16f124441983b4
Scolon <scoloned@gmail.com>
2025-11-07 23:24:02 -0500

    bruh

--- Core/LunarLightingSystem/LunarLighting.cs
+++ Core/LunarLightingSystem/LunarLighting.cs
@@ -4,6 +4,7 @@ using Microsoft.CodeAnalysis.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Threading;
+using Stellamod.Core.Backgrounds;
 using Stellamod.Core.Shaders;
 using Stellamod.Helpers;
 using System;
@@ -15,6 +16,26 @@ using Terraria.ModLoader;
 
 namespace Stellamod.Core.LunarLightingSystem
 {
+    public class GlobalLunarIllumination : GlobalWall
+    {
+        public override void ModifyLight(int i, int j, int type, ref float r, ref float g, ref float b)
+        {
+            var config = ModContent.GetInstance<LunarVeilClientConfig>();
+            if (!config.BeamingLights)
+                return;
+            if (!Main.tile[i, j].HasTile)
+            {
+                float lightStrength =1;
+                if (lightStrength > 0)
+                {
+                    r = MathHelper.Clamp(r + lightStrength, 0, 1);
+                    g = MathHelper.Clamp(g + lightStrength, 0, 1);
+                    b = MathHelper.Clamp(b + lightStrength, 0, 1);
+                }
+            }
+
+        }
+    }
     public class LunarLighting : ModSystem
     {
         private static Vector2 _previousScreenSize;
