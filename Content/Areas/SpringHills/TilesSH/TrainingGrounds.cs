98b6f8492119dfb0447e948ded74463327317c8e
Scolon <scoloned@gmail.com>
2025-11-11 17:41:57 -0500

    create training grounds tiles

---
+++ Content/Areas/SpringHills/TilesSH/TrainingGrounds.cs
@@ -0,0 +1,60 @@
+﻿using Stellamod.Tiles;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.SpringHills.TilesSH
+{
+    public class TargetBoard1Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<TargetBoard1>();
+        }
+    }
+
+    public class TargetBoard1 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+
+    public class FrontTargetBoardItem : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<FrontTargetBoard>();
+        }
+    }
+
+    public class FrontTargetBoard : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+
+    public class RedRoseItem : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<RedRose>();
+        }
+    }
+
+    public class RedRose : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+}
