975ad115864985c204c5aa6c23a9eaaaf153fa6c
Scolon <scoloned@gmail.com>
2025-11-11 17:37:58 -0500

    create illurian windows

---
+++ Content/Areas/Illuria/TilesIL/IlluriaWindows.cs
@@ -0,0 +1,83 @@
+﻿using Stellamod.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Illuria.TilesIL
+{
+    public class IlluriaWindows1Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<IlluriaWindows1>();
+        }
+    }
+
+    public class IlluriaWindows1 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+
+    public class IlluriaWindows2Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<IlluriaWindows2>();
+        }
+    }
+
+    public class IlluriaWindows2 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+
+
+    public class IlluriaWindows3Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<IlluriaWindows3>();
+        }
+    }
+
+    public class IlluriaWindows3 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+
+    public class IlluriaWindows4Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<IlluriaWindows4>();
+        }
+    }
+
+    public class IlluriaWindows4 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+}
