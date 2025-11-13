13a75b4e9fced3dc4b324fdc1491d134781e9cfc
Scolon <scoloned@gmail.com>
2025-11-13 02:29:37 -0500

    create dungeon save helper

--- Core/DungeonGeneration/DungeonSaveUtility.cs
+++ Core/DungeonGeneration/DungeonSaveUtility.cs
@@ -1,15 +1,52 @@
 ﻿using Microsoft.Xna.Framework;
 using Stellamod.Helpers;
+using Stellamod.NPCs.Colosseum.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 
 namespace Stellamod.Core.DungeonGeneration
 {
+    public class DungeonSaveHelper : ModItem
+    {
+        private int _useIndex;
+        public override void SetStaticDefaults()
+        {
+            /* Tooltip.SetDefault("Meatballs" +
+				"\nDo not be worried, this mushes reality into bit bits and then shoots it!" +
+				"\nYou can never miss :P"); */
+            // DisplayName.SetDefault("Teraciz");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 62;
+            Item.height = 32;
+            Item.scale = 0.9f;
+            Item.rare = ItemRarityID.Green;
+            Item.useTime = 2;
+            Item.useAnimation = 2;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = false;
+            Item.UseSound = new SoundStyle("Stellamod/Assets/Sounds/Balls");
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            DungeonSaveUtility.FindAndWriteDungeonWorldToPrefabFiles();
+            return true;
+        }
+    }
+
     /// <summary>
     /// Helper class for saving the new dungeon generation
     /// </summary>
