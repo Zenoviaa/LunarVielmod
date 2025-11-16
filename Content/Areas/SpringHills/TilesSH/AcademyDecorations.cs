29161b45c39f61f8a48a86de0f63252669159c38
Scolon <scoloned@gmail.com>
2025-11-16 18:03:26 -0500

    create witch academy items

---
+++ Content/Areas/SpringHills/TilesSH/AcademyDecorations.cs
@@ -0,0 +1,71 @@
+﻿using Stellamod.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.SpringHills.TilesSH
+{
+    public class WitchAcademyPosterItem : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<WitchAcademyPoster>();
+        }
+    }
+
+    public class WitchAcademyPoster : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+
+            Origin = DrawOrigin.Center;
+            StructureColor = BackgroundColor;
+        }
+    }
+
+    public class WitchAcademyBannerItem : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<WitchAcademyPoster>();
+        }
+    }
+
+    public class WitchAcademyBanner : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+
+            Origin = DrawOrigin.TopDown;
+            StructureColor = BackgroundColor;
+        }
+    }
+
+    public class WitchAcademyBookshelfItem : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<WitchAcademyBookshelf>();
+        }
+    }
+
+    public class WitchAcademyBookshelf : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+
+            Origin = DrawOrigin.BottomUp;
+            StructureColor = BackgroundColor;
+        }
+    }
+}
