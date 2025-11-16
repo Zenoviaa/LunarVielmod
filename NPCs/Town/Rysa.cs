daaff531902dfc719f37165b1aa6ac797706149b
Scolon <scoloned@gmail.com>
2025-11-16 18:06:20 -0500

    add rysa

---
+++ NPCs/Town/Rysa.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Core;
+using Stellamod.Helpers;
+using Stellamod.Items.Weapons.Melee.Swords;
+using Stellamod.Items.Weapons.Ranged;
+using Stellamod.Items.Weapons.Ranged.GunSwapping;
+using Stellamod.Items.Weapons.Thrown;
+
+using Stellamod.UI.ArmorReforgeSystem;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.NPCs.Town
+{
+    // [AutoloadHead] and NPC.townNPC are extremely important and absolutely both necessary for any Town NPC to work at all.
+
+    public class Rysa : VeilTownNPC
+    {
+        public int NumberOfTimesTalkedTo = 0;
+        public const string ShopName = "Shop";
+        public const string ShopName2 = "New Shop";
+        public override void SetStaticDefaults()
+        {
+            // DisplayName automatically assigned from localization files, but the commented line below is the normal approach.
+            // DisplayName.SetDefault("Example Person");
+            Main.npcFrameCount[Type] = 30; // The amount of frames the NPC has
+
+            NPCID.Sets.ActsLikeTownNPC[Type] = true;
+
+            //To reiterate, since this NPC isn't technically a town NPC, we need to tell the game that we still want this NPC to have a custom/randomized name when they spawn.
+            //In order to do this, we simply make this hook return true, which will make the game call the TownNPCName method when spawning the NPC to determine the NPC's name.
+            NPCID.Sets.SpawnsWithCustomName[Type] = true;
+            NPCID.Sets.NoTownNPCHappiness[Type] = true;
+
+            // Influences how the NPC looks in the Bestiary
+            NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers()
+            {
+                Velocity = 1f, // Draws the NPC in the bestiary as if its walking +1 tiles in the x direction
+                Direction = 1 // -1 is left and 1 is right. NPCs are drawn facing the left by default but ExamplePerson will be drawn facing the right
+                              // Rotation = MathHelper.ToRadians(180) // You can also change the rotation of an NPC. Rotation is measured in radians
+                              // If you want to see an example of manually modifying these when the NPC is drawn, see PreDraw
+            };
+
+
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+
+            // Set Example Person's biome and neighbor preferences with the NPCHappiness hook. You can add happiness text and remarks with localization (See an example in ExampleMod/Localization/en-US.lang).
+
+
+
+
+
+            ; // < Mind the semicolon!
+        }
+
+        public override void SetDefaults()
+        {
+            // Sets NPC to be a Town NPC
+            NPC.friendly = true; // NPC Will not attack player
+            NPC.width = 38;
+            NPC.height = 50;
+            NPC.aiStyle = 0;
+            NPC.damage = 90;
+            NPC.defense = 42;
+            NPC.lifeMax = 200;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0.5f;
+            NPC.dontTakeDamageFromHostiles = true;
+            SpawnAtPoint = true;
+            HasTownDialogue = true;
+        }
+
+        public override void SetPointSpawnerDefaults(ref NPCPointSpawner spawner)
+        {
+            spawner.structureToSpawnIn = "Struct/Overworld/WitchTown";
+            spawner.spawnTileOffset = new Point(90, -35);
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            NPC.frameCounter += 0.50f;
+            NPC.frameCounter %= Main.npcFrameCount[NPC.type];
+            int frame = (int)NPC.frameCounter;
+            NPC.frame.Y = frame * frameHeight;
+        }
+
+        public override bool CheckActive()
+        {
+            return false;
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            // We can use AddRange instead of calling Add multiple times in order to add multiple items at once
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+				// Sets the preferred biomes of this town NPC listed in the bestiary.
+				// With Town NPCs, you usually set this to what biome it likes the most in regards to NPC happiness.
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.VortexPillar,
+
+				// Sets your NPC's flavor text in the bestiary.
+				new FlavorTextBestiaryInfoElement(LangText.Bestiary(this, "Freezing to death")),
+
+				// You can add multiple elements if you really wanted to
+				// You can also use localization keys (see Localization/en-US.lang)
+				new FlavorTextBestiaryInfoElement(LangText.Bestiary(this, "Rysa", "2"))
+            });
+        }
+        public override List<string> SetNPCNameList()
+        {
+            return new List<string>() {
+                "Rysa",
+            };
+        }
+
+        public override void OpenTownDialogue(ref string text, ref string portrait, ref float timeBetweenTexts, ref SoundStyle? talkingSound, List<Tuple<string, Action>> buttons)
+        {
+            base.OpenTownDialogue(ref text, ref portrait, ref timeBetweenTexts, ref talkingSound, buttons);
+            //Set buttons
+            buttons.Add(new Tuple<string, Action>("Talk", Talk));
+            buttons.Add(new Tuple<string, Action>("Shop", OpenShop));
+
+            portrait = "RysaPortrait";
+            timeBetweenTexts = 0.015f;
+            talkingSound = SoundID.Item1;
+
+            //This pulls from the new Dialogue localization
+            text = "ZuiOpenDialogue1";
+        }
+
+        public override void IdleChat(ref string text, ref string portrait, ref float timeBetweenTexts, ref SoundStyle? talkingSound)
+        {
+            base.IdleChat(ref text, ref portrait, ref timeBetweenTexts, ref talkingSound);
+            portrait = "VeldrisPortrait";
+            timeBetweenTexts = 0.015f;
+            talkingSound = SoundID.Item1;
+
+            //This pulls from the new Dialogue localization
+            text = "ZuiIdleChat1";
+        }
+
+        public override void AddShops()
+        {
+            var npcShop = new NPCShop(Type, ShopName)
+            .Add(new Item(ItemID.Mace) { shopCustomPrice = Item.buyPrice(gold: 5) })
+            npcShop.Register(); // Name of this shop t
+        }
+    }
+}
