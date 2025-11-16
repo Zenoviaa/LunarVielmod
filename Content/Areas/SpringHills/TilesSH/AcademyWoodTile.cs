d7e6534cd4743326e30dbe2007572682a1be379e
Scolon <scoloned@gmail.com>
2025-11-16 17:44:36 -0500

    w

---
+++ Content/Areas/SpringHills/TilesSH/AcademyWoodTile.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.SpringHills.TilesSH
+{
+    public class AcademyWoodTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+
+            
+            DustType = Main.rand.Next(110, 113);
+            RegisterItemDrop(ModContent.ItemType<AcademyWood>());
+          
+            AddMapEntry(new Color(200, 200, 200));
+        }
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}
