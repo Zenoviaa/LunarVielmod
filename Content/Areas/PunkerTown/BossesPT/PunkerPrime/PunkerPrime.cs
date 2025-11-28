4afc574e8368a09003ea045f57d48520547bd77d
Scolon <scoloned@gmail.com>
2025-11-27 22:33:07 -0500

    create punker prime npc

---
+++ Content/Areas/PunkerTown/BossesPT/PunkerPrime/PunkerPrime.cs
@@ -0,0 +1,61 @@
+﻿using Stellamod.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.PunkerTown.BossesPT.PunkerPrime
+{
+    public class PunkerPrime : ScarletBoss
+    {
+        private enum AIState
+        {
+            Spawn,
+            Despawn,
+            Idle,
+            Death,
+        }
+
+        private ref float Timer => ref NPC.ai[0];
+        
+        private AIState State
+        {
+            get => (AIState)NPC.ai[1];
+            set => NPC.ai[1] = (float)value;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Main.npcFrameCount[NPC.type] = 1;
+            NPCID.Sets.MPAllowedEnemies[NPC.type] = true;
+            NPCID.Sets.BossBestiaryPriority.Add(Type);
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 128;
+            NPC.height = 128;
+            NPC.damage = 100;
+            NPC.defense = 14;
+            NPC.lifeMax = 6000;
+
+            NPC.value = Item.buyPrice(gold: 5);
+            NPC.knockBackResist = 0f;
+            NPC.boss = true;
+            NPC.noGravity = true;
+            NPC.noTileCollide = true;
+            NPC.npcSlots = 30f;
+
+            Music = MusicLoader.GetMusicSlot(Mod, "Assets/Music/PunkerPrime");
+            NPC.HitSound = new SoundStyle("Stellamod/Assets/Sounds/Gintze_Hit") with { PitchVariance = 0.1f, Pitch = -0.5f, Volume = 0.2f };
+            NPC.DeathSound = new SoundStyle("Stellamod/Assets/Sounds/Gintze_Death") with { PitchVariance = 0.1f, Pitch = -0.5f, Volume = 0.2f };
+        }
+    }
+}
