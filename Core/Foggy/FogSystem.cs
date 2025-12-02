83ef799cac754d9fb6ab0a50e2fce89c34e270d3
Scolon <scoloned@gmail.com>
2025-12-02 02:19:55 -0500

    w

--- Core/Foggy/FogSystem.cs
+++ Core/Foggy/FogSystem.cs
@@ -7,6 +7,7 @@ using Stellamod.Helpers;
 using System;
 using System.Collections.Generic;
 using Terraria;
+using Terraria.Graphics.Effects;
 using Terraria.ModLoader;
 
 namespace Stellamod.Core.Foggy
@@ -19,14 +20,76 @@ namespace Stellamod.Core.Foggy
         public override void Load()
         {
             base.Load();
-            On_Main.DrawDust += DrawFog;
+            On_OverlayManager.Draw += DrawFog;
         }
-
         public override void Unload()
         {
             base.Unload();
-            On_Main.DrawDust -= DrawFog;
+            On_OverlayManager.Draw -= DrawFog;
         }
+
+        private void DrawFog(On_OverlayManager.orig_Draw orig, OverlayManager self, SpriteBatch spriteBatch, RenderLayers layer, bool beginSpriteBatch)
+        {
+            orig(self, spriteBatch, layer, beginSpriteBatch);
+            if(layer == RenderLayers.ForegroundWater)
+            {
+                if (doDraws)
+                {
+
+                    if (_fogIndex.Count <= 0)
+                        return;
+
+                    var texture = TextureRegistry.Clouds6;
+                    //Apply Fog Shader
+                    var fogShader = Shaders.FogShader.Instance;
+                    fogShader.FogTexture = texture;
+                    fogShader.ProgressPower = 0.75f;
+                    fogShader.EdgePower = 1f;
+                    fogShader.Speed = 1f;
+                    fogShader.Apply();
+                    var currentTexture = texture;
+                    var blendState = BlendState.AlphaBlend;
+                    BaseShader currentShader = fogShader;
+
+                    spriteBatch.End();
+                    spriteBatch.Begin(SpriteSortMode.Immediate, blendState, SamplerState.PointClamp, DepthStencilState.None, Main.Rasterizer,
+                        currentShader.Effect, Main.GameViewMatrix.TransformationMatrix);
+
+                    foreach (var kvp in _fogIndex)
+                    {
+                        var fog = kvp.Value;
+                        BaseShader newShader = null;
+                        if (fog.shaderFunc != null)
+                        {
+                            newShader = fog.shaderFunc();
+                        }
+
+                        if (blendState != fog.blendState || newShader != currentShader)
+                        {
+                            currentTexture = fog.texture;
+                            currentShader = newShader;
+                            blendState = fog.blendState;
+
+                            Effect effect = null;
+                            if (currentShader != null)
+                                effect = currentShader.Effect;
+                            spriteBatch.End();
+                            spriteBatch.Begin(SpriteSortMode.Immediate, blendState, SamplerState.PointClamp, DepthStencilState.None, Main.Rasterizer,
+                                effect, Main.GameViewMatrix.TransformationMatrix);
+                        }
+
+                        Vector2 center = fog.position - Main.screenPosition;
+                        Vector2 scale = Vector2.One * fog.scale;
+                        Vector2 origin = fog.texture.Size() / 2;
+                        spriteBatch.Draw(currentTexture.Value, center, null, fog.color, fog.rotation, origin, scale, SpriteEffects.None, 0f);
+                    }
+
+                    spriteBatch.End();
+                    spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.Transform);
+                }
+            }
+        }
+
 
         public Fog SetupFog(Point position, Action<Fog> createFogFunc)
         {
@@ -71,64 +134,6 @@ namespace Stellamod.Core.Foggy
 
         }
 
-        private void DrawFog(On_Main.orig_DrawDust orig, Main self)
-        {
-            orig(self);
-            if (doDraws)
-            {
 
-                if (_fogIndex.Count <= 0)
-                    return;
-
-                var texture = TextureRegistry.Clouds6;
-                SpriteBatch spriteBatch = Main.spriteBatch;
-
-
-                //Apply Fog Shader
-                var fogShader = Shaders.FogShader.Instance;
-                fogShader.FogTexture = texture;
-                fogShader.ProgressPower = 0.75f;
-                fogShader.EdgePower = 1f;
-                fogShader.Speed = 1f;
-                fogShader.Apply();
-                var currentTexture = texture;
-                var blendState = BlendState.AlphaBlend;
-                BaseShader currentShader = fogShader;
-
-                spriteBatch.Begin(SpriteSortMode.Immediate, blendState, SamplerState.PointClamp, DepthStencilState.None, Main.Rasterizer,
-                    currentShader.Effect, Main.GameViewMatrix.TransformationMatrix);
-
-                foreach (var kvp in _fogIndex)
-                {
-                    var fog = kvp.Value;
-                    BaseShader newShader = null;
-                    if (fog.shaderFunc != null)
-                    {
-                        newShader = fog.shaderFunc();
-                    }
-
-                    if (blendState != fog.blendState || newShader != currentShader)
-                    {
-                        currentTexture = fog.texture;
-                        currentShader = newShader;
-                        blendState = fog.blendState;
-
-                        Effect effect = null;
-                        if (currentShader != null)
-                            effect = currentShader.Effect;
-                        spriteBatch.End();
-                        spriteBatch.Begin(SpriteSortMode.Immediate, blendState, SamplerState.PointClamp, DepthStencilState.None, Main.Rasterizer,
-                            effect, Main.GameViewMatrix.TransformationMatrix);
-                    }
-
-                    Vector2 center = fog.position - Main.screenPosition;
-                    Vector2 scale = Vector2.One * fog.scale;
-                    Vector2 origin = fog.texture.Size() / 2;
-                    spriteBatch.Draw(currentTexture.Value, center, null, fog.color, fog.rotation, origin, scale, SpriteEffects.None, 0f);
-                }
-
-                spriteBatch.End();
-            }
-        }
     }
 }
