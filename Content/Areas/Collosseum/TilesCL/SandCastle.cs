fbfc6e8c2a38e5230214c8958cd5b16de25df32c
Scolon <scoloned@gmail.com>
2025-12-04 02:04:36 -0500

    create sand castles

---
+++ Content/Areas/Collosseum/TilesCL/SandCastle.cs
@@ -0,0 +1,130 @@
+﻿using Stellamod.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Collosseum.TilesCL
+{
+    public class SandCastle1Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<SandCastle1>();
+        }
+    }
+
+    public class SandCastle1 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+    public class SandCastle2Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<SandCastle2>();
+        }
+    }
+
+    public class SandCastle2 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+    public class SandCastle3Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<SandCastle3>();
+        }
+    }
+
+    public class SandCastle3 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+    public class SandCastle4Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<SandCastle4>();
+        }
+    }
+
+    public class SandCastle4 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+    public class SandCastle5Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<SandCastle5>();
+        }
+    }
+
+    public class SandCastle5 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+    public class SandCastle6Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<SandCastle6>();
+        }
+    }
+
+    public class SandCastle6 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+    public class SandCastle7Item : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<SandCastle6>();
+        }
+    }
+
+    public class SandCastle7 : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            StructureColor = BackgroundColor;
+        }
+    }
+}
