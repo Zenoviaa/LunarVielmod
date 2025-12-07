4f0d8aa0e62a8d502c59af580d359e93dbb881cb
Scolon <scoloned@gmail.com>
2025-12-06 22:04:34 -0500

    eresh statue regular

---
+++ Content/Areas/SpringHills/TilesSH/EreshStatue.cs
@@ -0,0 +1,24 @@
+﻿using Stellamod.Tiles;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.SpringHills.TilesSH
+{
+    public class EreshStatueItem : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<EreshStatue>();
+        }
+    }
+
+    public class EreshStatue : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Origin = DrawOrigin.BottomUp;
+        }
+    }
+}
