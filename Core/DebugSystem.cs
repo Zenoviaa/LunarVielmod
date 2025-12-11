9c018eb53995515dfdccb8057acfd37cbb3c1c88
Scolon <scoloned@gmail.com>
2025-12-11 16:27:19 -0500

    debug for locking the camera

---
+++ Core/DebugSystem.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Stellamod.Core.Camera;
+using Stellamod.Helpers;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Core
+{
+    public class DebugSystem : ModSystem
+    {
+        private bool _pressed;
+        private bool _lock;
+        private Vector2 _lockPosition;
+        public override void PostUpdateEverything()
+        {
+            base.PostUpdateEverything();
+            if (InputHelper.KeyDown(Keys.OemComma))
+            {
+                _pressed = true;
+
+            }
+            if (InputHelper.KeyUp(Keys.OemComma) && !_pressed)
+            {
+                _pressed = false;
+                _lock = !_lock;
+                _lockPosition = Main.Camera.Center;
+            }
+
+            if (_lock)
+            {
+                RetargetCameraModifier.ReTargetPosition = _lockPosition;
+            }
+        }
+    }
+}
---
+++ Helpers/InputHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stellamod.Helpers
+{
+    public static class InputHelper
+    {
+        public static bool KeyUp(Keys keys)
+        {
+            KeyboardState keyboard = Keyboard.GetState();
+            return keyboard.IsKeyUp(keys);
+        }
+
+        public static bool KeyDown(Keys keys)
+        {
+            KeyboardState keyboard = Keyboard.GetState();
+            return keyboard.IsKeyDown(keys);
+        }
+
+
+    }
+}
