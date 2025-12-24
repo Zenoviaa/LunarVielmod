6c3789e286e349235efd42f8eeb7d704a2495cbe
Scolon <scoloned@gmail.com>
2025-12-24 14:35:35 -0500

    create dialogue classes

---
+++ Content/Dialogue/EFight.cs
@@ -0,0 +1,74 @@
+﻿using Stellamod.Core.DialogueSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stellamod.Content.Dialogue
+{
+    public class ZuiComeQuickDialogue : BaseDialogue
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            CloseOnComplete = true;
+        }
+        public override int GetLength()
+        {
+            return 4;
+        }
+    }
+
+    public class ZuiWhoAreYouDialogue : BaseDialogue
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            CloseOnComplete = true;
+        }
+        public override int GetLength()
+        {
+            return 2;
+        }
+    }
+
+    public class ZuiTalkingToYouDialogue : BaseDialogue
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            CloseOnComplete = true;
+        }
+        public override int GetLength()
+        {
+            return 3;
+        }
+    }
+
+    public class EFoundYouDialogue : BaseDialogue
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            CloseOnComplete = true;
+        }
+        public override int GetLength()
+        {
+            return 1;
+        }
+    }
+
+    public class ZuiGetOuttaHereDialogue : BaseDialogue
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            CloseOnComplete = true;
+        }
+        public override int GetLength()
+        {
+            return 2;
+        }
+    }
+}
