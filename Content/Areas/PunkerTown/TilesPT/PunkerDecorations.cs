3a6a541bf5c5f2173fe625068789efb0710ce459
Scolon <scoloned@gmail.com>
2025-12-27 20:17:55 -0500

    add extra decorations

---
+++ Content/Areas/PunkerTown/TilesPT/PunkerDecorations.cs
@@ -0,0 +1,23 @@
+﻿using Stellamod.Tiles;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.PunkerTown.TilesPT
+{
+    public class PunkerPillarVaseItem : DecorativeWallItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.createWall = ModContent.WallType<PunkerPillarVase>();
+        }
+    }
+
+    public class PunkerPillarVase : DecorativeWall
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Origin = DrawOrigin.BottomUp;
+        }
+    }
+}
