c4caf46f3c16a5e915d34c6c631f92befd0a522d
Scolon <scoloned@gmail.com>
2025-12-30 18:56:30 -0500

    added coaster enchantment

---
+++ Content/Items/MoonlightMagic/Enchantments/Uvilis/CoasterEnchantment.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Content.Items.MoonlightMagic.Elements;
+using Stellamod.Helpers;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Items.MoonlightMagic.Enchantments.Uvilis
+{
+    public class CoasterPlayer : ModPlayer
+    {
+        public Vector2? coastingTarget;
+        public override void PreUpdateMovement()
+        {
+            base.PreUpdateMovement();
+            if (coastingTarget.HasValue)
+            {
+                Vector2 targetPosition = coastingTarget.Value;
+                Vector2 velocityToPosition = targetPosition - Player.Center;
+                Player.velocity = velocityToPosition;
+                coastingTarget = null;
+            }
+        }
+    }
+
+    public class CoasterEnchantment : BaseEnchantment
+    {
+        public override void AI()
+        {
+            base.AI();
+            CoasterPlayer coasterPlayer = Owner.GetModPlayer<CoasterPlayer>();
+            if(coasterPlayer.coastingTarget == null)
+            {
+                coasterPlayer.coastingTarget = Projectile.Center;
+            }
+        }
+
+        public override float GetStaffManaModifier()
+        {
+            return 0.2f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<UvilisElement>();
+        }
+
+
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, ColorFunctions.UvilisLightBlue);
+        }
+    }
+}
