75b054d2b1bd177c0fcdc235af8ef5b1aac3fb42
Scolon <scoloned@gmail.com>
2025-12-30 18:42:31 -0500

    added sirestias's touch enchantment

---
+++ Content/Items/MoonlightMagic/Enchantments/Bloodlet/SirestiasTouchEnchantment.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Content.Items.MoonlightMagic.Elements;
+using Stellamod.Helpers;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Items.MoonlightMagic.Enchantments.Bloodlet
+{
+    public class SirestiasTouchEnchantment : BaseEnchantment
+    {
+        private int _extraHits;
+        private int _divisor;
+        public override void SetMagicDefaults()
+        {
+            base.SetMagicDefaults();
+            _extraHits = 3;
+        }
+
+        public override void AI()
+        {
+            base.AI();
+            Countertimer++;
+            if(Countertimer == 1)
+            {
+                _divisor = Projectile.timeLeft / _extraHits;
+            }
+            if (Countertimer % _divisor == 0)
+            {
+                Projectile.ResetLocalNPCHitImmunity();
+            }
+        }
+
+        public override float GetStaffManaModifier()
+        {
+            return 1f;
+        }
+
+        public override int GetElementType()
+        {
+            return ModContent.ItemType<BloodletElement>();
+        }
+
+        public override void SpecialInventoryDraw(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.SpecialInventoryDraw(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            DrawHelper.DrawGlowInInventory(item, spriteBatch, position, ColorFunctions.DreadRed);
+        }
+    }
+}
