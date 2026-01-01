4e1a3cacb7f24d873c1351491fc3f054cef7793d
Scolon <scoloned@gmail.com>
2025-12-31 21:37:55 -0500

    add marsh reeds

---
+++ Backgrounds/MarshReeds.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Stellamod.Content.Biomes;
+using Stellamod.Core.Foreground;
+using Terraria;
+
+namespace Stellamod.Backgrounds
+{
+    public class MarshReeds : ForegroundLayer
+    {
+        public override bool IsActive()
+        {
+            return Main.LocalPlayer.GetModPlayer<BiomePlayer>().ZoneMarsh;
+        }
+        public override void SetLayering(ref float zLayer, ref Vector2 parallax)
+        {
+            base.SetLayering(ref zLayer, ref parallax);
+            parallax.X = 1.2f;
+            parallax.Y = 1;
+        }
+    }
+}
