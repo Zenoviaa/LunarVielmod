a3bcaf5e031052e5166224896cf880859b6a95d2
Scolon <scoloned@gmail.com>
2026-01-07 22:41:42 -0500

    testing

---
+++ Content/Areas/Snow/ArmorsSN/JacklerArmor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Snow.ArmorsSN
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class JacklerHat : ModItem
+    {
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+
+        }
+    }
+    [AutoloadEquip(EquipType.Body)]
+    public class JacklerCoat : ModItem
+    {
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+     
+        }
+    }
+
+    [AutoloadEquip(EquipType.Legs)]
+    public class JacklerPants : ModItem
+    {
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+
+        }
+    }
+}
