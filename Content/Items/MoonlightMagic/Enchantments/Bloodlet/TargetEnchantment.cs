919df7f9ab2820fce9629a1f77cfb318264c34c8
Scolon <scoloned@gmail.com>
2026-01-09 15:35:29 -0500

    add target enchantment

---
+++ Content/Items/MoonlightMagic/Enchantments/Bloodlet/TargetEnchantment.cs
@@ -0,0 +1,13 @@
+﻿using Terraria;
+
+namespace Stellamod.Content.Items.MoonlightMagic.Enchantments.Bloodlet
+{
+    public class TargetEnchantment : BaseEnchantment
+    {
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            base.OnHitNPC(target, hit, damageDone);
+            Main.player[Projectile.owner].MinionAttackTargetNPC = target.whoAmI;
+        }
+    }
+}
