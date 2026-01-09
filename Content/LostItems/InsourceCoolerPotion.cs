77a7bbbbc9a200de85a0af298e705bcb12945c17
Scolon <scoloned@gmail.com>
2026-01-09 15:14:49 -0500

    add insource cooler potion

---
+++ Content/LostItems/InsourceCoolerPotion.cs
@@ -0,0 +1,21 @@
+﻿using Stellamod.Common.XixianFlaskSystem;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.LostItems
+{
+    public class InsourceCoolerPotion : ModItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.DefaultToAccessory();
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            base.UpdateAccessory(player, hideVisual);
+            player.GetModPlayer<FlaskPlayer>().insourceSecondsBonusPerInsource += 2;
+        }
+    }
+}
