fdb68485a6e0bc0fb821a53dcbb0959a56363a06
Scolon <scoloned@gmail.com>
2026-01-09 14:48:39 -0500

    add summoner's brew

---
+++ Content/LostItems/SummonersBrew.cs
@@ -0,0 +1,21 @@
+﻿using Stellamod.Common.ArmorRework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.LostItems
+{
+    public class SummonersBrew : ModItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.DefaultToAccessory();
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            base.UpdateAccessory(player, hideVisual);
+            player.GetModPlayer<ArmorStatsPlayer>().minionSummonHealth += 0.25f;
+        }
+    }
+
+}
