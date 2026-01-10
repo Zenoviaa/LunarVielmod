f70534b35b4c95a7cf827ac8110b803c0d8352ac
Scolon <scoloned@gmail.com>
2026-01-09 23:33:06 -0500

    Update NaturalWitch.cs

--- NPCs/Town/NaturalWitch.cs
+++ NPCs/Town/NaturalWitch.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Stellamod.Buffs;
+using Stellamod.Content.Biomes;
 using Stellamod.Helpers;
 using Stellamod.Items.Accessories.Foods;
 using Stellamod.Items.Armors.Pieces.RareMetals;
