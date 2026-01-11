7a7545eddd01ef52379e58dcc265858cc1ed8739
Scolon <scoloned@gmail.com>
2026-01-11 16:33:42 -0500

    woas

--- Content/Areas/Dungeon/BossesDG/Bisinine/Bishinine.cs
+++ Content/Areas/Dungeon/BossesDG/Bisinine/Bishinine.cs
@@ -194,7 +194,7 @@ namespace Stellamod.Content.Areas.Dungeon.BossesDG.Bisinine
             NPC.npcSlots = 10f;
             NPC.takenDamageMultiplier = 0.9f;
             NPC.aiStyle = -1;
-            Music = MusicMod.GetMusicSlot( "Assets/Music/Bishinine");
+            Music = MusicLoader.GetMusicSlot( "Assets/Music/Bishinine");
         }
 
         public override bool? CanFallThroughPlatforms()
