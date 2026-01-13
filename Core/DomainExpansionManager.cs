a97ee650f445c9e5494ee7e84dc720616b6e1b10
Scolon <scoloned@gmail.com>
2026-01-13 02:54:12 -0500

    collision check in the domain expansion only applies to the player and the platform has improved collision

--- Core/DomainExpansionManager.cs
+++ Core/DomainExpansionManager.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Stellamod.Helpers;
+using System;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -6,6 +7,7 @@ namespace Stellamod.Core
 {
     public class DomainExpansionManager : ModSystem
     {
+        private bool[] _prevTileSolid;
         public bool inSpace;
         public bool noWings;
         public bool hoveringPlatform;
@@ -13,126 +15,74 @@ namespace Stellamod.Core
         public override void OnModLoad()
         {
             base.OnModLoad();
-            On_Collision.TileCollision += NoCollision;
-            On_Collision.AdvancedTileCollision += NoAdvancedCollision;
-            On_Collision.WetCollision += NoWetCollision;
-            On_Collision.AnyCollision += NoAnyCollision;
-            On_Collision.SlopeCollision += NoSlopeCollision;
-            On_Player.SlopingCollision += NoSlopingCollision;
-            On_Player.DryCollision += NoDryCollision;
-            On_Collision.SolidCollision_Vector2_int_int += NoSolidCollision;
-            On_Collision.EmptyTile += AllEmptyTiles;
-            On_Collision.SolidCollision_Vector2_int_int_bool += NoSolidCollision2;
-            On_Collision.IsWorldPointSolid += NoSolid;
-            On_Collision.StepDown += NoStepDown;
-            On_Collision.StepUp += NoStepUp;
-            On_Player.SlopeDownMovement += NoSlopeDown;
-            On_Collision.CanHitLine += AlwaysHitLine;
+            On_Player.SlopingCollision += HoverPlatformCollisionCheck;
         }
+
         public override void OnModUnload()
         {
             base.OnModUnload();
-            On_Collision.TileCollision -= NoCollision;
-            On_Collision.AdvancedTileCollision -= NoAdvancedCollision;
-            On_Collision.WetCollision -= NoWetCollision;
-            On_Collision.AnyCollision -= NoAnyCollision;
-            On_Collision.SlopeCollision -= NoSlopeCollision;
-            On_Player.SlopingCollision -= NoSlopingCollision;
-            On_Player.DryCollision -= NoDryCollision;
- 
-            On_Player.SlopeDownMovement -= NoSlopeDown;
-            On_Collision.CanHitLine -= AlwaysHitLine;
+            On_Player.SlopingCollision -= HoverPlatformCollisionCheck;
         }
 
-        private bool AlwaysHitLine(On_Collision.orig_CanHitLine orig, Vector2 Position1, int Width1, int Height1, Vector2 Position2, int Width2, int Height2)
+        private void HoverPlatformCollisionCheck(On_Player.orig_SlopingCollision orig, Player self, bool fallThrough, bool ignorePlats)
         {
-            if (inSpace)
-                return true;
-            return orig(Position1, Width1, Height1, Position2, Width2, Height2);
+       
+            if (hoveringPlatform)
+            {
+                float y = hoverPlatformY + 36;
+                Rectangle playerRectangle = self.getRect();
+                int width = 128;
+                Rectangle nextPlatformRectangle = new Rectangle(playerRectangle.X - width / 2, (int)y, width, 1);
+                if (nextPlatformRectangle.Intersects(playerRectangle) || nextPlatformRectangle.Contains(playerRectangle))
+                {
+                    if (!self.justJumped && self.velocity.Y >= 0)
+                    {
+                        self.velocity.Y = 0;
+
+                        int inside = 4;
+                        //We had it a little bit into the elevator so it doesn't stop colliding with it
+                        self.position.Y = (nextPlatformRectangle.TopLeft().Y) - self.height + inside;
+                        self.position.Y -= self.gfxOffY;
+                    }
+                } else if (self.Bottom.Y > y)
+                {
+                    float dist = MathF.Abs(y - self.Bottom.Y);
+                    float strength = dist / 1000;
+                    strength = MathHelper.Clamp(strength, 0, 1);
+                    strength = EasingFunction.InOutSine(strength);
+                    self.position.Y -= MathHelper.Lerp(12, 48, strength);
+                    self.velocity.Y = 0;
+                }
+            }
+
+            orig(self, fallThrough, ignorePlats);
         }
 
-        private void NoSlopeDown(On_Player.orig_SlopeDownMovement orig, Player self)
+        public override void PreUpdatePlayers()
         {
-            if (inSpace)
+            base.PreUpdatePlayers();
+            if (!inSpace)
                 return;
-            orig(self);
+
+            _prevTileSolid ??= new bool[Main.tileSolid.Length];
+            for (int t = 0; t < _prevTileSolid.Length; t++)
+            {
+                _prevTileSolid[t] = Main.tileSolid[t];
+                Main.tileSolid[t] = false;
+            }
         }
 
-        private void NoStepUp(On_Collision.orig_StepUp orig, ref Vector2 position, ref Vector2 velocity, int width, int height, ref float stepSpeed, ref float gfxOffY, int gravDir, bool holdsMatching, int specialChecksMode)
+        public override void PostUpdatePlayers()
         {
-            if (inSpace)
+            base.PostUpdatePlayers();
+            if (!inSpace)
                 return;
-            orig(ref position, ref velocity, width, height, ref stepSpeed, ref gfxOffY, gravDir, holdsMatching, specialChecksMode);
-        }
 
-        private void NoStepDown(On_Collision.orig_StepDown orig, ref Vector2 position, ref Vector2 velocity, int width, int height, ref float stepSpeed, ref float gfxOffY, int gravDir, bool waterWalk)
-        {
-            if (inSpace)
-                return;
-            orig(ref position, ref velocity, width, height, ref stepSpeed, ref gfxOffY, gravDir, waterWalk);
-        }
-
-
-        private bool NoSolid(On_Collision.orig_IsWorldPointSolid orig, Vector2 pos, bool treatPlatformsAsNonSolid)
-        {
-
-            if (!inSpace)
+            for (int t = 0; t < Main.tileSolid.Length; t++)
             {
-                return orig(pos, treatPlatformsAsNonSolid);
+                Main.tileSolid[t] = _prevTileSolid[t];
             }
-            return false;
         }
-
-        private bool NoSolidCollision2(On_Collision.orig_SolidCollision_Vector2_int_int_bool orig, Vector2 Position, int Width, int Height, bool acceptTopSurfaces)
-        {
-           
-            if (!inSpace)
-            {
-                return orig(Position, Width, Height, acceptTopSurfaces);
-            }
-            return false;
-        }
-
-
-        private bool NoSolidCollision(On_Collision.orig_SolidCollision_Vector2_int_int orig, Vector2 Position, int Width, int Height)
-        {
-          
-            if (!inSpace)
-            {
-                return orig(Position, Width, Height);
-            }
-            return false;
-        }
-        private bool AllEmptyTiles(On_Collision.orig_EmptyTile orig, int i, int j, bool ignoreTiles)
-        {
-        
-            if (!inSpace)
-            {
-                return orig(i, j, ignoreTiles);
-            }
-            return true;
-        }
-
-        private void NoDryCollision(On_Player.orig_DryCollision orig, Player self, bool fallThrough, bool ignorePlats)
-        {
-            if (!inSpace)
-            {
-                orig(self, fallThrough, ignorePlats);
-                return;
-            }
-            orig(self, true, true);
-        }
-
-        private void NoSlopingCollision(On_Player.orig_SlopingCollision orig, Player self, bool fallThrough, bool ignorePlats)
-        {
-            if (!inSpace)
-            {
-                orig(self, fallThrough, ignorePlats);
-                return;
-            }
-            orig(self, true, true);
-        }
-
 
         public override void PreUpdateNPCs()
         {
@@ -140,49 +90,6 @@ namespace Stellamod.Core
             inSpace = false;
             noWings = false;
             hoveringPlatform = false;
-        }
-        private Vector4 NoSlopeCollision(On_Collision.orig_SlopeCollision orig, Vector2 Position, Vector2 Velocity, int Width, int Height, float gravity, bool fall)
-        {
-            if (!inSpace)
-                return orig(Position, Velocity, Width, Height, gravity, fall);
-            return new Vector4(Position.X, Position.Y, Velocity.X, Velocity.Y);
-        }
-
-        private Vector2 NoAnyCollision(On_Collision.orig_AnyCollision orig, Vector2 Position, Vector2 Velocity, int Width, int Height, bool evenActuated)
-        {
-            if (!inSpace)
-                return orig(Position, Velocity, Width, Height, evenActuated);
-            return Velocity;
-        }
-
-        private Vector2 NoCollision(On_Collision.orig_TileCollision orig, Vector2 Position, Vector2 Velocity, int Width, int Height, bool fallThrough, bool fall2, int gravDir)
-        {
-            if (!inSpace)
-                return orig(Position, Velocity, Width, Height, fallThrough, fall2, gravDir);
-            if (hoveringPlatform)
-            {
-                Vector2 vel = Velocity;
-                if (Position.Y > hoverPlatformY)
-                    vel.Y = -16;
-                else if (Position.Y + Velocity.Y > hoverPlatformY)
-                    vel.Y = 0;
-                return vel;
-            }
-            return Velocity;
-        }
-
-        private Vector2 NoAdvancedCollision(On_Collision.orig_AdvancedTileCollision orig, bool[] forcedIgnoredTiles, Vector2 Position, Vector2 Velocity, int Width, int Height, bool fallThrough, bool fall2, int gravDir)
-        {
-            if (!inSpace)
-                return orig(forcedIgnoredTiles, Position, Velocity, Width, Height, fallThrough, fall2, gravDir);
-            return Velocity;
-        }
-
-        private bool NoWetCollision(On_Collision.orig_WetCollision orig, Vector2 Position, int Width, int Height)
-        {
-            if (!inSpace)
-                return orig(Position, Width, Height);
-            return false;
         }
     }
 }
