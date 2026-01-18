c0411d379d46164d3fe5b60163477944bef49fe6
Scolon <scoloned@gmail.com>
2026-01-17 21:12:20 -0500

    cindered lantern is a little bit better than radiating

---
+++ Content/Areas/Cinderspark/AccCS/CinderedLantern.cs
@@ -0,0 +1,49 @@
+﻿using Stellamod.Core.Bases;
+using Stellamod.Core.LunarLightingSystem;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Cinderspark.AccCS
+{
+    public class CinderedLantern : BaseLanternItem
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.buffType = ModContent.BuffType<CinderediatingLantern>();
+            Item.shoot = ModContent.ProjectileType<CinderedLanternProjectile>();
+        }
+    }
+
+    public class CinderedLanternProjectile : BaseLanternProjectile<CinderediatingLantern>
+    {
+        private ConeLight _coneLight;
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            FlashlightDegrees = 30;
+            FlashlightLength = 800;
+            FlashlightWidth = 860;
+        }
+        protected override ILight GetLight()
+        {
+            _coneLight ??= new ConeLight();
+            return _coneLight;
+        }
+    }
+
+    public class CinderediatingLantern : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.lightPet[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            bool unused = false;
+            player.BuffHandle_SpawnPetIfNeededAndSetTime(buffIndex, ref unused, ModContent.ProjectileType<CinderedLanternProjectile>());
+        }
+    }
+}
