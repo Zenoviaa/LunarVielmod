9cf14c54cab5ea99591e85518caf53503c4359aa
Scolon <scoloned@gmail.com>
2026-01-22 12:59:59 -0500

    make jackler armor

---
+++ Content/Armors/Jackler/JacklerArmor.cs
@@ -0,0 +1,177 @@
+﻿using Stellamod.Assets;
+using Stellamod.Common.ArmorRework;
+using Stellamod.Common.SummonerSystem;
+using Stellamod.Core.Particles;
+using Stellamod.Helpers;
+using Stellamod.Visual.Particles;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Armors.Jackler
+{
+    public class JacklerBoom : ModProjectile
+    {
+        private ref float Timer => ref Projectile.ai[0];
+        public override string Texture => TextureRegistry.EmptyTexture;
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Projectile.width = 80;
+            Projectile.height = 80;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.timeLeft = 24;
+            Projectile.tileCollide = false;
+        }
+
+        public override void AI()
+        {
+            base.AI();
+            Timer++;
+            if(Timer == 1)
+            {
+                FXUtil.ShakeCamera(Projectile.Center, 1024, 16);
+                FXUtil.GlowCircleBoom(Projectile.Center,
+                    innerColor: Color.Red,
+                    glowColor: Color.DarkRed,
+                    outerGlowColor: Color.Black, duration: 25, baseSize: 0.28f);
+
+                SoundEngine.PlaySound(SoundID.DD2_ExplosiveTrapExplode, Projectile.position);
+                for (float f = 0; f < 4; f++)
+                {
+                    Particle<DustParticle>.Spawn(Projectile.Center, 
+                        Vector2.UnitY.RotatedByRandom(MathHelper.TwoPi) * Main.rand.NextFloat(6, 8f), Scale: Main.rand.NextFloat(0.5f, 1f));
+                }
+
+                for (float f = 0; f < 6; f++)
+                {
+                    var smoke = Particle<SmokeParticle>.SpawnInAlphaLayer(Projectile.Center, 
+                        -Vector2.UnitY.RotatedByRandom(MathHelper.TwoPi) * Main.rand.NextFloat(1, 1f), Color.White, Scale: Main.rand.NextFloat(0.5f, 1f));
+                    smoke.initialColor = Color.DarkGray;
+                }
+
+                for(float f = 0; f < 8; f++)
+                {
+                    Vector2 velocity = Main.rand.NextVector2Circular(16, 16);
+                    var rose = Particle<RosePetalParticle>.SpawnInAlphaLayer(Projectile.Center, velocity, Scale: Main.rand.NextFloat(0.8f, 1.6f));
+                }
+
+                for (float i = 0; i < 8; i++)
+                {
+                    float progress = i / 4f;
+                    float rot = progress * MathHelper.ToRadians(360);
+                    rot += Main.rand.NextFloat(-0.5f, 0.5f);
+                    Vector2 offset = rot.ToRotationVector2() * 24;
+                    var particle = FXUtil.GlowCircleDetailedBoom1(Projectile.Center,
+                        innerColor: Color.Red,
+                        glowColor: Color.DarkRed,
+                        outerGlowColor: Color.Black,
+                        baseSize: Main.rand.NextFloat(0.1f, 0.2f),
+                        duration: Main.rand.NextFloat(15, 25));
+                    particle.Rotation = rot + MathHelper.ToRadians(45);
+                }
+            }
+        }
+    }
+    public class JacklerGlobalProjectile : GlobalProjectile
+    {
+        public override void OnKill(Projectile projectile, int timeLeft)
+        {
+            base.OnKill(projectile, timeLeft);
+            if (!projectile.minion)
+                return;
+
+            Player owner = Main.player[projectile.owner];
+            JacklerPlayer jacklerPlayer = owner.GetModPlayer<JacklerPlayer>();
+            if (!jacklerPlayer.hasJacklerSetBonus)
+                return;
+
+            if (Main.myPlayer != projectile.owner)
+                return;
+            Projectile.NewProjectile(projectile.GetSource_FromThis(), projectile.Center, Vector2.Zero, 
+                ModContent.ProjectileType<JacklerBoom>(), projectile.damage * 2, 1, projectile.owner);
+        }
+    }
+    public class JacklerPlayer : ModPlayer
+    {
+        public bool hasJacklerSetBonus;
+        public override void ResetEffects()
+        {
+            base.ResetEffects();
+            hasJacklerSetBonus = false;
+        }
+    }
+    [AutoloadEquip(EquipType.Head)]
+    public class JacklerHat : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            ArmorSetSystem.RegisterArmorSet<JacklerHat, JacklerCoat, JacklerPants>();
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            ArmorStatsPlayer stats = player.GetModPlayer<ArmorStatsPlayer>();
+            stats.accessorySlots++;
+            stats.summonCastTime += 0.25f;
+            stats.defenseBonus += 2;
+
+        }
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return head.type == Type && body.type == ModContent.ItemType<JacklerCoat>() && legs.type == ModContent.ItemType<JacklerPants>();
+        }
+        public override void UpdateArmorSet(Player player)
+        {
+            base.UpdateArmorSet(player);
+            player.GetModPlayer<JacklerPlayer>().hasJacklerSetBonus = true;
+        }
+    }
+
+    [AutoloadEquip(EquipType.Body)]
+    public class JacklerCoat : ModItem
+    {
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            ArmorStatsPlayer stats = player.GetModPlayer<ArmorStatsPlayer>();
+            stats.accessorySlots++;
+            stats.minionSlots++;
+            stats.defenseBonus += 4;
+
+        }
+    }
+
+    [AutoloadEquip(EquipType.Legs)]
+    public class JacklerPants : ModItem
+    {
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            ArmorStatsPlayer stats = player.GetModPlayer<ArmorStatsPlayer>();
+            stats.accessorySlots++;
+            stats.minionSummonHealth+= -0.20f;
+            stats.defenseBonus += 3;
+        }
+    }
+}
---
+++ Visual/Particles/RosePetalParticle.cs
@@ -0,0 +1,57 @@
+﻿using Stellamod.Core.Particles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace Stellamod.Visual.Particles
+{
+
+    public class RosePetalParticle : Particle<RosePetalParticle>
+    {
+        public int FrameWidth = 18;
+        public int FrameHeight = 16;
+        public int MaxFrameCount = 3;
+        public float gravity;
+        public Vector2 stretchScale;
+        public float dampening;
+        public bool fast;
+        public override void OnSpawn()
+        {
+            gravity = 0.2f;
+            Frame = new Rectangle(0, FrameHeight * Main.rand.Next(MaxFrameCount), FrameWidth, FrameHeight);
+        }
+
+        public override void Update()
+        {
+            Velocity.Y += gravity;
+            Velocity *= 1.0f - dampening;
+            Rotation = Velocity.ToRotation();
+            Scale *= 0.97f;
+            if (fast)
+                Scale *= 0.98f;
+
+            fadeIn++;
+            if (fadeIn > 180 || Scale < 0.1f)
+                active = false;
+
+            //Bouncing
+            Vector2 collisionVelocity = Collision.TileCollision(Center, Velocity, 2, 2);
+            if (Velocity.X != collisionVelocity.X)
+                Velocity.X = -collisionVelocity.X;
+            if (Velocity.Y != collisionVelocity.Y)
+                Velocity.Y = -collisionVelocity.Y;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 centerPos = Center - Main.screenPosition;
+            var textureAsset = GetTexture();
+
+            Color drawColor = color;
+            spriteBatch.Draw(textureAsset.Value, centerPos, Frame, drawColor, Rotation + MathHelper.PiOver2, Frame.Size() / 2f, Scale, SpriteEffects.None, 0);
+        }
+    }
+}
