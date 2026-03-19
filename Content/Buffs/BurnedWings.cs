5b0b3a625a36549ff01a30b9776380ef9f89c7b5
Scolon <scoloned@gmail.com>
2026-03-19 15:27:20 -0400

    feat: burned wings debuff goes away after STARBOMBER is down, and also removes your flying mounts

--- Content/Buffs/BurnedWings.cs
+++ Content/Buffs/BurnedWings.cs
@@ -1,29 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Stellamod.Helpers;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace Stellamod.Content.Buffs
+namespace Stellamod.Content.Buffs;
+
+public class BurnedWings : ModBuff
 {
-    public class BurnedWings : ModBuff
+    public override void SetStaticDefaults()
     {
-        public override void SetStaticDefaults()
+        Main.pvpBuff[Type] = true;
+        Main.buffNoTimeDisplay[Type] = true;
+    }
+
+
+    public override void Update(Player player, ref int buffIndex)
+    {
+        base.Update(player, ref buffIndex);
+        player.wingTimeMax = 0;
+        player.wingTime = 0;
+        if (player.mount.Active && player.mount.CanFly())
         {
-            // DisplayName.SetDefault("Charm Buff!");
-            // Description.SetDefault("Icy Frileness!");
-            Main.pvpBuff[Type] = true;
-            Main.buffNoTimeDisplay[Type] = true;
+            player.mount.Dismount(player);
+            CombatText.NewText(player.getRect(), Color.White, LangText.Common("NoMount"));
         }
 
-
-        public override void Update(Player player, ref int buffIndex)
+        if (DownedBossTracker.IsDowned(DownedBossFlag.StarBomber))
         {
-            base.Update(player, ref buffIndex);
-            player.wingTimeMax = 0;
-            player.wingTime = 0;
+            player.ClearBuff(Type);
         }
     }
 }
