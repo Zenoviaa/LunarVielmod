21c4d3efa936862505456384e45c373336f457be
Scolon <scoloned@gmail.com>
2026-04-21 23:04:19 -0400

    Update MaskingWall.cs

--- Core/WallBackgroundSystem/MaskingWall.cs
+++ Core/WallBackgroundSystem/MaskingWall.cs
@@ -4,6 +4,14 @@ namespace Stellamod.Core.WallBackgroundSystem
 {
     public class MaskingWall : ModWall
     {
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            base.ModifyLight(i, j, ref r, ref g, ref b);
+            float light = 0.5f;
+            r += light;
+            g += light;
+            b += light;
+        }
         public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
         {
            // MaskedWallRenderer.QueueDraw(new Point(i, j));
