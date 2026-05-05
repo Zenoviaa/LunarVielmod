00cb20027ae0636e5593dc89abf40d2234db76f0
Zenovia <calamityjewelstudios@gmail.com>
2026-05-04 22:15:43 -0400

    Useless wand

---
+++ Content/Items/MoonlightMagic/Weapons/MiracleWand.cs
@@ -0,0 +1,42 @@
+﻿using Stellamod.Content.CommonMaterials;
+using Stellamod.Content.Items.MoonlightMagic.Elements;
+using Stellamod.Content.Items.MoonlightMagic.Forms;
+using Stellamod.Items;
+using Stellamod.Items.Materials;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Items.MoonlightMagic.Weapons
+{
+    public class MiracleWand : AbstractMagicWand
+    {
+        public override void SetDefaults2()
+        {
+            base.SetDefaults2();
+            Item.damage = 1650;
+            Item.shootSpeed = 16;
+            Size = 8;
+            TrailLength = 16;
+            Form = FormRegistry.Runic.Value;
+            normalSlotCount = 5;
+            timedSlotCount = 4;
+            Item.mana = 20;
+        }
+
+        public override void AddRecipes()
+        {
+            base.AddRecipes();
+            this.RegisterBrew(mold: ModContent.ItemType<BlankStaff>(), material: ModContent.ItemType<MiracleThread>());
+        }
+
+        public override void ModifyElementPreferences(List<int> elements)
+        {
+            base.ModifyElementPreferences(elements);
+            elements.Add(ModContent.ItemType<PrimeMagicElement>());
+            elements.Add(ModContent.ItemType<DeeyaElement>());
+            elements.Add(ModContent.ItemType<BloodletElement>());
+            elements.Add(ModContent.ItemType<GuutElement>());
+            elements.Add(ModContent.ItemType<HexElement>());
+        }
+    }
+}
