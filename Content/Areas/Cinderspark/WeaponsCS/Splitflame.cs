bc9cee4159c5f221865d51173d129fdfce91ee9a
Scolon <scoloned@gmail.com>
2026-05-15 19:25:59 -0400

    feat: reworked split flame artifact

--- Content/Areas/Cinderspark/WeaponsCS/Splitflame.cs
+++ Content/Areas/Cinderspark/WeaponsCS/Splitflame.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Stellamod.Assets;
 using Stellamod.Common.Shaders;
 using Stellamod.Content.CommonMaterials;
 using Stellamod.Core.Bases;
+using Stellamod.Core.Particles;
+using Stellamod.Core.Pixelation;
 using Stellamod.Dusts;
 using Stellamod.Helpers;
 using Stellamod.Items;
@@ -10,6 +14,7 @@ using Stellamod.Items.Harvesting;
 using Stellamod.Visual.Particles;
 using Terraria;
 using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -20,23 +25,41 @@ namespace Stellamod.Content.Areas.Cinderspark.WeaponsCS
         public override void SetDefaults()
         {
             Item.DefaultToArtifact();
-            Item.damage = 50;
-            Item.mana = 45;
+            Item.damage = 18;
+            Item.mana = 180;
             Item.width = 18;
             Item.height = 21;
             Item.useTime = 40;
             Item.useAnimation = 40;
             Item.useStyle = ItemUseStyleID.HoldUp;
-            Item.noMelee = true;
+      
             Item.knockBack = 4f;
             Item.DamageType = DamageClass.Magic;
             Item.value = 10000;
             Item.rare = ItemRarityID.Orange;
             Item.UseSound = SoundID.DD2_BookStaffCast;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
             Item.autoReuse = true;
             Item.shoot = ModContent.ProjectileType<SplitFlameBlast>();
             Item.shootSpeed = 4f;
             Item.autoReuse = true;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            return base.CanUseItem(player);
+        }
+        public override bool CanShoot(Player player)
+        {
+            return !player.HasBuff(ModContent.BuffType<ManaFire>());
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            base.ModifyShootStats(player, ref position, ref velocity, ref type, ref damage, ref knockback);
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            return base.Shoot(player, source, position, velocity, type, damage, knockback);
         }
         public override void AddRecipes()
         {
@@ -45,106 +68,173 @@ namespace Stellamod.Content.Areas.Cinderspark.WeaponsCS
         }
     }
 
+    public class ManaFire : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.pvpBuff[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+  
+        }
+    }
+
+    public class ManaFirePlayer : ModPlayer
+    {
+        public override void ResetEffects()
+        {
+            base.ResetEffects();
+
+        }
+        public override void ModifyManaCost(Item item, ref float reduce, ref float mult)
+        {
+            base.ModifyManaCost(item, ref reduce, ref mult);
+            if (Player.HasBuff<ManaFire>())
+                mult *= 0.5f;
+        }
+    }
+
     public class SplitFlameBlast : ModProjectile
     {
         private ref float Timer => ref Projectile.ai[0];
         public override string Texture => TextureRegistry.EmptyTexture;
+        private Player Owner => Main.player[Projectile.owner];
         public override void SetDefaults()
         {
             base.SetDefaults();
-            Projectile.width = 8;
-            Projectile.height = 8;
-            Projectile.tileCollide = true;
-            Projectile.penetrate = 1;
+            Projectile.width = 128;
+            Projectile.height = 128;
             Projectile.friendly = true;
-            Projectile.timeLeft = 140;
+            Projectile.timeLeft = 600;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 20;
         }
 
         public override void AI()
         {
             base.AI();
             Timer++;
-            if (Timer % 6 == 0)
+            if(Timer == 1)
             {
-                Color glyphColor = Color.Red;
-                switch (Main.rand.Next(3))
+                for (int i = 0; i < 32; i++)
                 {
-                    case 0:
-                        glyphColor = Color.Red;
-                        break;
-                    case 1:
-                        glyphColor = Color.OrangeRed;
-                        break;
-                    case 2:
-                        glyphColor = Color.Yellow;
-                        break;
+                    Vector2 pos = Projectile.Center;
+                    pos += Main.rand.NextVector2Circular(32, 32);
+                    Vector2 vel = Main.rand.NextVector2Circular(16, 16);
+                    Dust.NewDustPerfect(pos, DustID.Torch, vel, Scale: Main.rand.NextFloat(1f, 3f));
                 }
-
-                if (Main.rand.NextBool(3))
-                {
-                    DustParticleSpawnParams spawnParams = new DustParticleSpawnParams
-                    {
-                        innerColor = Color.Yellow,
-                        outerColor = Color.Red,
-                        gravity=0f,
-                        scaleRange = new Vector2(0.2f, 0.6f)
-                    };
-                    var dp = DustParticle.Spawn(Projectile.Center, -Vector2.UnitY, spawnParams);
-                    dp.dampening = 0.1f;
-                }
-                    
-                Dust.NewDustPerfect(Projectile.Center, DustID.Torch, Projectile.velocity * 0.1f, 0, glyphColor, 1f).noGravity = true;
-                if (Main.rand.NextBool(3))
-                    Dust.NewDustPerfect(Projectile.Center, DustID.Torch, Projectile.velocity * 0.1f, 0, glyphColor, 1f).noGravity = true;
-                Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<GlyphDust>(), Projectile.velocity * 0.1f, 0, glyphColor, 1f).noGravity = true;
-            }
-            if (Timer < 10)
-            {
-                Projectile.velocity -= Projectile.velocity.SafeNormalize(Vector2.Zero);
-                Projectile.velocity *= 0.99f;
+                SoundEngine.PlaySound(SoundID.Item74 with { PitchVariance = 0.5f }, Projectile.position);
             }
 
-            if (Timer == 20 && Main.myPlayer == Projectile.owner)
+            Owner.AddBuff(ModContent.BuffType<ManaFire>(), 10);
+
+            Vector2 center = Owner.Center;
+            center.X -= 14;
+            Projectile.velocity = (center - Projectile.Center);
+
+            if (Main.rand.NextBool(16))
             {
-                Projectile.velocity = ProjectileHelper.SimpleHomingVelocity(Projectile, Main.MouseWorld, 360);
-                Projectile.netUpdate = true;
+                var fs = FaintSmokeParticle.SpawnInAlphaLayer(Projectile.Center + Main.rand.NextVector2Circular(64, 32), -Vector2.UnitY.SafeNormalize(Vector2.Zero) * Main.rand.NextFloat(1f, 3f));
+                fs.Center -= Vector2.UnitY.SafeNormalize(Vector2.Zero) * 64;
+                fs.fadeToColor = Color.Black * 0.35f;
+                fs.color = Color.RosyBrown * 0.35f;
+                fs.Scale *= 0.25f;
+            }
+            //  Vector2.c
+            // Projectile.rotation = _initialVelocity.ToRotation() + MathHelper.PiOver2;
+            if (Main.rand.NextBool(8))
+            {
+                DustParticle sp = Particle<DustParticle>.Spawn(Projectile.Center + Main.rand.NextVector2Circular(32, 32) - new Vector2(0, 12),
+                    -Vector2.UnitY * Main.rand.NextFloat(2, 16), Scale: Main.rand.NextFloat(0.5f, 1.5f));
+                sp.innerColor = Color.Yellow;
+                sp.outerColor = Color.Red;
+                sp.gravity = 0f;
+                sp.fast = true;
+                sp.dampening = 0.1f;
+                sp.Scale *= 0.6f;
             }
 
-            if (Timer > 20)
+            foreach(var npc in Main.ActiveNPCs)
             {
-                if (Projectile.velocity.Length() < 15)
-                    Projectile.velocity *= 1.5f;
+                if (npc.friendly)
+                    continue;
+                if (npc.townNPC)
+                    continue;
+                float dist = Vector2.Distance(Projectile.Center, npc.Center);
+                if (dist <= 256)
+                    npc.AddBuff(BuffID.OnFire, 80);
             }
-            Projectile.rotation = Projectile.velocity.X * 0.025f;
+            //   Projectile.rotation = Projectile.velocity.X * 0.025f;
             Lighting.AddLight(Projectile.Center, Color.OrangeRed.ToVector3() * 1.75f * Main.essScale);
         }
+
+        private void DrawPixelatedFlames(SpriteBatch sb, Vector2 screenPos)
+        {
+            // var sb = Main.spriteBatch;
+            float fade = MathHelper.Lerp(0f, 1f, EasingFunction.InOutSine((float)Projectile.timeLeft / 30f));
+            float inScale = EasingFunction.OutExpo(Timer / 30f);
+            Asset<Texture2D> waveTexture = AssetManager.GlowMask.Wave;
+            WaveShader waveShader = ShaderContent.GetInstance<WaveShader>();
+            waveShader.Time = Main.GlobalTimeWrappedHourly * 0.5f + Projectile.whoAmI;
+            waveShader.Amplitude = 0.3f;
+            waveShader.Frequency = 8;
+            waveShader.XStrength = 6;
+            waveShader.NoiseTexture = AssetManager.Noise.Whirly.Value;
+            sb.Restart(effect: waveShader.Effect);
+            SpritebatchDrawer drawer = SpritebatchDrawer.FromTextureAsset(waveTexture, Projectile.Center);
+            drawer.rotation = Projectile.rotation;
+            drawer.BottomCenterOrigin();
+            drawer.color = Color.OrangeRed * fade * ExtraMath.Osc(0.6f, 1f, speed: 32, offset: Projectile.whoAmI);
+            drawer.color.A = 0;
+            drawer.scale *= 0.5f * inScale;
+            drawer.scale.Y *= ExtraMath.Osc(1f, 1.1f, offset: Projectile.whoAmI);
+            sb.Draw(drawer);
+
+            drawer.TopCenterOrigin();
+            drawer.scale.Y *= 0.4f;
+            drawer.spriteEffects |= SpriteEffects.FlipVertically;
+            drawer.rotation = Projectile.rotation;
+            sb.Draw(drawer);
+
+            sb.RestartDefaults();
+
+            Asset<Texture2D> bloomLine = AssetManager.GlowMask.SimpleGlowCircle;
+            SpritebatchDrawer drawer2 = SpritebatchDrawer.FromTextureAsset(bloomLine, Projectile.Center + new Vector2(0f, 12));
+            //      drawer2.BottomCenterOrigin();
+            drawer2.scale *= new Vector2(0.55f, 0.55f) * ExtraMath.Osc(0.8f, 1f, speed: 3) * inScale;
+            drawer2.color = Color.Yellow * fade * 0.5f; ;
+            drawer2.color.A = 0;
+            drawer2.rotation = Projectile.rotation;
+            sb.Draw(drawer2);
+
+            drawer2.scale *= 2;
+            drawer2.color = Color.Red * fade * 0.5f; ;
+            drawer2.color.A = 0;
+            sb.Draw(drawer2);
+
+            drawer2.scale *= 2;
+            drawer2.color = Color.Red * fade * 0.15f; ;
+            drawer2.color.A = 0;
+            sb.Draw(drawer2);
+
+            SpritebatchDrawer blastPillar = SpritebatchDrawer.FromTextureAsset(AssetManager.GlowMask.BlastPillar, Projectile.Center + new Vector2(0f, 12));
+            blastPillar.BottomCenterOrigin();
+            blastPillar.color = Color.Red * 0.5f * ExtraMath.Osc(0.6f, 1f, speed: 32, offset: Projectile.whoAmI) * fade;
+            blastPillar.color.A = 0;
+            blastPillar.scale *= 0.6f;
+            blastPillar.rotation = Projectile.rotation;
+            sb.Draw(blastPillar);
+        }
+
         public override bool PreDraw(ref Color lightColor)
         {
-            SpriteBatch spriteBatch = Main.spriteBatch;
-            Texture2D texture = ModContent.Request<Texture2D>(TextureRegistry.CandleFlame).Value;
-            Vector2 drawOrigin = texture.Size() / 2f;
-            Color drawColor = Color.White.MultiplyRGB(lightColor);
-            float drawRotation = Projectile.rotation;
-            float drawScale = Projectile.scale * 0.1f;
-
-            Vector2 drawPos = Projectile.Center - Main.screenPosition;
-            var shader = FableFireShader.Instance;
-
-
-            spriteBatch.Restart(blendState: BlendState.Additive, effect: shader.Effect);
-            spriteBatch.Draw(texture, drawPos, null, Color.White, drawRotation, drawOrigin, drawScale, SpriteEffects.None, 0);
-
-            Texture2D dimLight = ModContent.Request<Texture2D>("Stellamod/Assets/NoiseTextures/DimLight").Value;
-
-            spriteBatch.RestartDefaults();
-            Color dimLightColor = Color.Red;
-            dimLightColor.A = 0;
-            spriteBatch.Draw(dimLight, drawPos, null, dimLightColor, drawRotation, dimLight.Size() / 2f, drawScale * 10, SpriteEffects.None, 0);
-
-            dimLightColor = Color.Yellow;
-            dimLightColor.A = 0;
-            if (Main.rand.NextBool(3))
-                spriteBatch.Draw(dimLight, drawPos, null, dimLightColor, drawRotation, dimLight.Size() / 2f, drawScale * 10, SpriteEffects.None, 0);
+            PixelationManager.QueueSpritebatchDrawAction(DrawPixelatedFlames, DrawLayer.OverPlayers);
             return false;
         }
 
@@ -158,80 +248,6 @@ namespace Stellamod.Content.Areas.Cinderspark.WeaponsCS
         public override void OnKill(int timeLeft)
         {
             base.OnKill(timeLeft);
-            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero,
-                ModContent.ProjectileType<SplitflameBoom>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
-        }
-    }
-
-    public class SplitflameBoom : ModProjectile
-    {
-        private ref float Timer => ref Projectile.ai[0];
-        public override string Texture => TextureRegistry.EmptyTexture;
-        public override void SetDefaults()
-        {
-            base.SetDefaults();
-            Projectile.width = 80;
-            Projectile.height = 80;
-            Projectile.penetrate = -1;
-            Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = -1;
-            Projectile.friendly = true;
-        }
-        public override void AI()
-        {
-            base.AI();
-            Timer++;
-            if (Timer == 1)
-            {
-                FXUtil.ShakeCamera(Projectile.Center, 1024, 8);
-                SoundEngine.PlaySound(SoundRegistry.CombusterBoom, Projectile.position);
-                for (float f = 0; f < 15; f++)
-                {
-                    Color glyphColor = Color.Red;
-                    switch (Main.rand.Next(3))
-                    {
-                        case 0:
-                            glyphColor = Color.Red;
-                            break;
-                        case 1:
-                            glyphColor = Color.OrangeRed;
-                            break;
-                        case 2:
-                            glyphColor = Color.Yellow;
-                            break;
-                    }
-                    Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<GlyphDust>(),
-                        (Vector2.One * Main.rand.NextFloat(0.2f, 5f)).RotatedByRandom(19.0), 0, glyphColor, Main.rand.NextFloat(1f, 3f)).noGravity = true;
-                }
-
-                SoundStyle morrowExp = new SoundStyle($"Stellamod/Assets/Sounds/MorrowExp");
-                morrowExp.PitchVariance = 0.3f;
-                SoundEngine.PlaySound(morrowExp, Projectile.position);
-                for (float i = 0; i < 4; i++)
-                {
-                    float progress = i / 4f;
-                    float rot = progress * MathHelper.ToRadians(360);
-                    Vector2 offset = rot.ToRotationVector2() * 24;
-                    var particle = FXUtil.GlowCircleDetailedBoom1(Projectile.Center,
-                        innerColor: Color.White,
-                        glowColor: Color.Yellow,
-                        outerGlowColor: Color.Red,
-                        baseSize: 0.2f);
-                    particle.Rotation = rot + MathHelper.ToRadians(45);
-                }
-
-                for (int i = 0; i < 8; i++)
-                {
-                    Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<TSmokeDust>(), (Vector2.One * Main.rand.Next(1, 5)).RotatedByRandom(19.0), 0, Color.DarkGray, 1f).noGravity = true;
-                }
-            }
-        }
-
-        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-        {
-            base.OnHitNPC(target, hit, damageDone);
-            if (Main.rand.NextBool(3))
-                target.AddBuff(BuffID.OnFire, 180);
         }
     }
 }
