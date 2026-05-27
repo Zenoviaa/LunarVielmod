b8892df3c79f2874cc54d54f31665863683d5870
Scolon <scoloned@gmail.com>
2026-05-27 13:48:39 -0400

    feat: pearlinator should be brighter?

--- Content/Areas/Snow/WeaponsSN/Pearlinator.cs
+++ Content/Areas/Snow/WeaponsSN/Pearlinator.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Stellamod.Common.GunSystem;
+﻿using Stellamod.Common.GunSystem;
 using Stellamod.Common.Shaders;
 using Stellamod.Content.CommonMaterials;
 using Stellamod.Core.Particles;
@@ -16,190 +14,185 @@ using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Stellamod.Content.Areas.Snow.WeaponsSN
+namespace Stellamod.Content.Areas.Snow.WeaponsSN;
+
+public class Pearlinator : BaseGun
 {
-    public class Pearlinator : BaseGun
+    public override void SetDefaults()
     {
-        public override void SetDefaults()
-        {
-            base.SetDefaults();
-            Item.width = 62;
-            Item.height = 32;
-            Item.useTime = 4;
-            Item.useAnimation = 4;
-            Item.useStyle = ItemUseStyleID.Shoot;
-            Item.autoReuse = true;
-            Item.UseSound = SoundID.Item34;
+        base.SetDefaults();
+        Item.width = 62;
+        Item.height = 32;
+        Item.useTime = 4;
+        Item.useAnimation = 4;
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.autoReuse = true;
+        Item.UseSound = SoundID.Item34;
 
-            Item.DamageType = DamageClass.Ranged;
-            Item.damage = 4;
-            Item.knockBack = 4;
-            Item.noMelee = true;
+        Item.DamageType = DamageClass.Ranged;
+        Item.damage = 4;
+        Item.knockBack = 4;
+        Item.noMelee = true;
 
-            Item.shoot = ModContent.ProjectileType<PearlinatorFlame>();
-            Item.useTime = 8;
-            Item.useAnimation = 8;
-            Item.shootSpeed = 1;
-        }
+        Item.shoot = ModContent.ProjectileType<PearlinatorFlame>();
+        Item.useTime = 8;
+        Item.useAnimation = 8;
+        Item.shootSpeed = 1;
+    }
 
-        public override void SetMagazine(ref GunReloadParams fireParams)
-        {
-            base.SetMagazine(ref fireParams);
-            fireParams.maxAmmo = 32;
-            fireParams.reloadWindow = 120;
-        }
-        public override bool ShootProjectile(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-        {
-  
-            type = ModContent.ProjectileType<PearlinatorFlame>();
-            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
-            return false;
-        }
+    public override void SetMagazine(ref GunReloadParams fireParams)
+    {
+        base.SetMagazine(ref fireParams);
+        fireParams.maxAmmo = 32;
+        fireParams.reloadWindow = 120;
+    }
 
-        public override void ShootEffects(Vector2 position, Vector2 velocity)
-        {
-            for (int i = 0; i < 2; i++)
-            {
-                DustParticle dp = Particle<DustParticle>.Spawn(position, velocity.RotatedByRandom(MathHelper.ToRadians(22)) * Main.rand.NextFloat(3f, 8f), Color.White, Scale: Main.rand.NextFloat(0.3f, 0.5f));
-                dp.gravity = 0;
-                dp.dampening = 0.1f;
-                dp.outerColor = Color.Blue;
-            }
-        }
+    public override bool ShootProjectile(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    {
+        type = ModContent.ProjectileType<PearlinatorFlame>();
+        Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+        return false;
+    }
 
-        public override void AddRecipes()
+    public override void ShootEffects(Vector2 position, Vector2 velocity)
+    {
+        for (int i = 0; i < 2; i++)
         {
-            base.AddRecipes();
-            this.RegisterBrew<WinterbornShard, BlankGun>();
+            DustParticle dp = Particle<DustParticle>.Spawn(position, velocity.RotatedByRandom(MathHelper.ToRadians(22)) * Main.rand.NextFloat(3f, 8f), Color.White, Scale: Main.rand.NextFloat(0.3f, 0.5f));
+            dp.gravity = 0;
+            dp.dampening = 0.1f;
+            dp.outerColor = Color.Blue;
         }
     }
 
-    public class PearlinatorFlame : ModProjectile
+    public override void AddRecipes()
     {
-        public override string Texture => TextureRegistry.EmptyTexture;
+        base.AddRecipes();
+        this.RegisterBrew<WinterbornShard, BlankGun>();
+    }
+}
 
-        private ref float Timer => ref Projectile.ai[0];
-        private Vector2[] IncineratorPos;
-        private float LifeTime => 32;
-        private int NumPoints => 64;
-        public override void SetDefaults()
+public class PearlinatorFlame : ModProjectile,
+    IDrawToRenderTarget
+{
+    public override string Texture => TextureRegistry.EmptyTexture;
+    private ref float Timer => ref Projectile.ai[0];
+    private Vector2[] IncineratorPos;
+    private float LifeTime => 32;
+    private int NumPoints => 64;
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+        IncineratorPos = new Vector2[NumPoints];
+        Projectile.width = 150;
+        Projectile.height = 150;
+        Projectile.friendly = true;
+        Projectile.hostile = false;
+        Projectile.penetrate = -1;
+        Projectile.idStaticNPCHitCooldown = 14;
+        Projectile.usesIDStaticNPCImmunity = true;
+        Projectile.tileCollide = false;
+        Projectile.timeLeft = (int)LifeTime;
+    }
+
+    public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+    {
+        return ProjectileHelper.OldPosColliding(IncineratorPos, projHitbox, targetHitbox, 48);
+    }
+    public override bool ShouldUpdatePosition()
+    {
+        return true;
+    }
+
+    public override void AI()
+    {
+        float numPoints = NumPoints;
+        Vector2 start = Projectile.Center;
+        Vector2 end = start + Projectile.velocity * 64;
+
+        float progress = Timer / LifeTime;
+        float easeOut = EasingFunction.InOutSine(progress);
+        start = Vector2.Lerp(start, end, easeOut * 0.5f);
+        for (int i = 0; i < numPoints; i++)
         {
-            base.SetDefaults();
-            IncineratorPos = new Vector2[NumPoints];
-            Projectile.width = 150;
-            Projectile.height = 150;
-            Projectile.friendly = true;
-            Projectile.hostile = false;
-            Projectile.penetrate = -1;
-            Projectile.idStaticNPCHitCooldown = 14;
-            Projectile.usesIDStaticNPCImmunity = true;
-            Projectile.tileCollide = false;
-            Projectile.timeLeft = (int)LifeTime;
+            float f = i;
+            float ratio = f / numPoints;
+            Vector2 point = Vector2.Lerp(start, end, ratio);
+            IncineratorPos[i] = point;
         }
 
-        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        Timer++;
+        if (Timer == 1 && Main.rand.NextBool(8))
         {
-            return ProjectileHelper.OldPosColliding(IncineratorPos, projHitbox, targetHitbox, 48);
-        }
-        public override bool ShouldUpdatePosition()
-        {
-            return true;
+            SoundEngine.PlaySound(SoundID.DD2_BetsyFireballShot, Projectile.position);
+            SoundEngine.PlaySound(SoundID.DD2_EtherianPortalSpawnEnemy, Projectile.position);
         }
 
-        public override void AI()
-        {
-            float numPoints = NumPoints;
-            Vector2 start = Projectile.Center;
-            Vector2 end = start + Projectile.velocity * 64;
 
-            float progress = Timer / LifeTime;
-            float easeOut = EasingFunction.InOutSine(progress);
-            start = Vector2.Lerp(start, end, easeOut * 0.5f);
-            for (int i = 0; i < numPoints; i++)
-            {
-                float f = (float)i;
-                float ratio = f / numPoints;
-                Vector2 point = Vector2.Lerp(start, end, ratio);
-                IncineratorPos[i] = point;
-            }
+        Lighting.AddLight(Projectile.Center + Projectile.velocity * 32, TorchID.Ice);
+        Projectile.rotation += 0.05f;
+    }
 
-            Timer++;
-            if (Timer == 1 && Main.rand.NextBool(8))
-            {
-                SoundEngine.PlaySound(SoundID.DD2_BetsyFireballShot, Projectile.position);
-                SoundEngine.PlaySound(SoundID.DD2_EtherianPortalSpawnEnemy, Projectile.position);
-            }
+    public override bool OnTileCollide(Vector2 oldVelocity)
+    {
+        return false;
+    }
 
-            //   Lighting.AddLight(Projectile.Center + Projectile.velocity * 64, TorchID.Torch);
-            Projectile.rotation += 0.05f;
-        }
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        base.OnHitNPC(target, hit, damageDone);
+        target.AddBuff(BuffID.Frostburn, 120);
+    }
 
-        public override bool OnTileCollide(Vector2 oldVelocity)
-        {
-            return false;
-        }
+    private float WidthFunction(float completionRatio)
+    {
+        float width = 192;
+        float w = MathHelper.SmoothStep(16, width, completionRatio);
+        float o = MathHelper.Lerp(1f, 0f, EasingFunction.InCirc(completionRatio));
+        float progress = Timer / LifeTime;
+        float o2 = MathHelper.Lerp(1f, 2f, progress);
+        float i = MathHelper.Lerp(0f, 1f, EasingFunction.OutExpo(progress));
+        return w * o * o2 * i;
+    }
 
-        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-        {
-            base.OnHitNPC(target, hit, damageDone);
-            target.AddBuff(BuffID.Frostburn, 120);
-        }
+    private Color ColorFunction(float completionRatio)
+    {
+        Color tipColor = Color.Lerp(Color.White, Color.Pink, completionRatio);
+        Color finalColor = Color.Lerp(Color.Cyan, tipColor, EasingFunction.QuadraticBump(MathF.Pow(completionRatio, 0.5f)));
+        Color finalColor2 = Color.Lerp(Color.White, finalColor, EasingFunction.QuadraticBump(completionRatio));
+        finalColor2 *= EasingFunction.QuadraticBump(completionRatio);
+        float progress = Timer / LifeTime;
+        float o2 = MathHelper.Lerp(1f, 0f, progress);
+        finalColor2 *= o2;
+        finalColor2 *= MathHelper.Lerp(1f, 0f, EasingFunction.InExpo(completionRatio));
+        return finalColor2;
+    }
 
-        private float WidthFunction(float completionRatio)
-        {
-            float width = 192;
-            float w = MathHelper.SmoothStep(16, width, completionRatio);
-            float o = MathHelper.Lerp(1f, 0f, EasingFunction.InCirc(completionRatio));
-            float progress = Timer / LifeTime;
-            float o2 = MathHelper.Lerp(1f, 2f, progress);
-            float i = MathHelper.Lerp(0f, 1f, EasingFunction.OutExpo(progress));
-            return w * o * o2 * i;
-        }
+    private void DrawMainShader(Vector2[] oldPos)
+    {
+        BlackFireShader blackFireShader = BlackFireShader.Instance;
+        blackFireShader.InnerColor = Color.LightCyan;
+        blackFireShader.OuterColor = Color.Blue;
+        blackFireShader.PrimaryTexture2 = TrailRegistry.StarTrail;
+        blackFireShader.BackColor = Color.Violet;
+        blackFireShader.InnerEmitColor = Color.White;
+        blackFireShader.OuterEmiteColor = Color.LightGray;
+        TrailDrawer.Draw(Main.spriteBatch, oldPos, null, ColorFunction, WidthFunction, blackFireShader, Vector2.Zero);
+    }
 
-        private Color ColorFunction(float completionRatio)
-        {
-            Color tipColor = Color.Lerp(Color.White, Color.Pink, completionRatio);
-            Color finalColor = Color.Lerp(Color.Cyan, tipColor, EasingFunction.QuadraticBump(MathF.Pow(completionRatio, 0.5f)));
-            Color finalColor2 = Color.Lerp(Color.White, finalColor, EasingFunction.QuadraticBump(completionRatio));
-            finalColor2 *= EasingFunction.QuadraticBump(completionRatio);
-            float progress = Timer / LifeTime;
-            float o2 = MathHelper.Lerp(1f, 0f, progress);
-            finalColor2 *= o2;
-            finalColor2 *= MathHelper.Lerp(1f, 0f, EasingFunction.InExpo(completionRatio));
-            return finalColor2;
-        }
-        public float SmokeWidthFunction(float completionRatio)
-        {
-            return WidthFunction(completionRatio) * 0.85f;
-        }
+    private void DrawPixelatedFlames(GraphicsDevice graphicsDevice)
+    {
+        DrawMainShader(IncineratorPos);
+    }
 
-        public Color SmokeColorFunction(float completionRatio)
-        {
-            return ColorFunction(completionRatio);
-        }
-        private void DrawMainShader(Vector2[] oldPos)
-        {
-            BlackFireSmokeShader blackSmokeShader = BlackFireSmokeShader.Instance;
-          //  TrailDrawer.Draw(Main.spriteBatch, oldPos, null, SmokeColorFunction, SmokeWidthFunction, blackSmokeShader, Vector2.Zero);
+    public override bool PreDraw(ref Color lightColor)
+    {
 
-            BlackFireShader blackFireShader = BlackFireShader.Instance;
-            blackFireShader.InnerColor = Color.LightCyan;
-            blackFireShader.OuterColor = Color.Blue;
-            blackFireShader.PrimaryTexture2 = TrailRegistry.StarTrail;
-            //   InnerEmitColor = Color.Yellow;
-            // OuterEmiteColor = Color.Red;
-            blackFireShader.BackColor = Color.Violet;
-            TrailDrawer.Draw(Main.spriteBatch, oldPos, null, ColorFunction, WidthFunction, blackFireShader, Vector2.Zero);
-        }
+        return false;
+    }
 
-        private void DrawPixelatedFlames(GraphicsDevice graphicsDevice)
-        {
-            DrawMainShader(IncineratorPos);
-        }
-        public override bool PreDraw(ref Color lightColor)
-        {
-            PixelationManager.QueuePrimitivesDrawAction(DrawPixelatedFlames, DrawLayer.OverNPCsWithOutline);
-            return false;
-        }
+    public void DrawToRenderTargets()
+    {
+        PixelationManager.QueuePrimitivesDrawAction(DrawPixelatedFlames, DrawLayer.OverNPCsWithOutline);
     }
 }
