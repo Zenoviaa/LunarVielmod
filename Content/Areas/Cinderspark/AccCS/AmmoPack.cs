0814430b8315262fa49d5faa1402989a8bf0d4aa
Scolon <scoloned@gmail.com>
2026-06-21 13:23:33 -0400

    feat: ammo pack mechanics

---
+++ Content/Areas/Cinderspark/AccCS/AmmoPack.cs
@@ -0,0 +1,26 @@
+﻿using Stellamod.Common.ArmorRework;
+using Stellamod.Content.CommonMaterials;
+using Stellamod.Items;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Cinderspark.AccCS;
+
+public class AmmoPack : ModItem
+{
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+        Item.DefaultToAccessory();
+    }
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        base.UpdateAccessory(player, hideVisual);
+        player.GetStats().rangedGunAmmoAmountPct += 0.5f;
+    }
+    public override void AddRecipes()
+    {
+        base.AddRecipes();
+        this.RegisterBrew<Cinderscrap, BlankAccessory>();
+    }
+}
