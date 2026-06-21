cff123dfeda430ebb3a3ad720a9091df0347f8dc
Scolon <scoloned@gmail.com>
2026-06-21 13:20:45 -0400

    feat: piercer mechanic

---
+++ Content/Areas/RoyalCapital/AccRC/ThePiercer.cs
@@ -0,0 +1,27 @@
+﻿using Stellamod.Common.ArmorRework;
+using Stellamod.Content.CommonMaterials;
+using Stellamod.Items;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.RoyalCapital.AccRC;
+
+public class ThePiercer : ModItem
+{
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+        Item.DefaultToAccessory();
+    }
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        base.UpdateAccessory(player, hideVisual);
+        player.GetStats().rangedPiercing += 2;
+    }
+
+    public override void AddRecipes()
+    {
+        base.AddRecipes();
+        this.RegisterBrew<AlcaricMush, BlankAccessory>();
+    }
+}
