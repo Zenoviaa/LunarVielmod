4da0a31b29a8a992dc70839e6f5cbd896a896368
Scolon <scoloned@gmail.com>
2026-08-06 21:50:51 -0400

    govheil protector

--- NPCs/Govheil/GovheilGear.cs
+++ NPCs/Govheil/GovheilGear.cs
@@ -1,29 +1,105 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Stellamod.Dusts;
+using Stellamod.Helpers;
+using Stellamod.Helpers.Mathin;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Stellamod.NPCs.Govheil
 {
     public class GovheilGear : ModProjectile
     {
+        private ref float Timer => ref Projectile.ai[0];
+        private ref float Direction => ref Projectile.ai[1];
+        private ref float Step => ref Projectile.ai[2];
         public override void SetStaticDefaults()
         {
             base.SetStaticDefaults();
+            ProjectileID.Sets.TrailCacheLength[Type] = 16;
+            ProjectileID.Sets.TrailingMode[Type] = 2;
+
         }
         public override void SetDefaults()
         {
             base.SetDefaults();
+            Projectile.tileCollide = true;
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.hostile = true;
+            Projectile.timeLeft = 180;
+            Projectile.penetrate = -1;
         }
         public override void AI()
         {
             base.AI();
+
+            Timer++;
+            if(Timer == 1)
+            {
+
+                SoundEngine.PlaySound(new SoundStyle("Stellamod/Assets/Sounds/GhostExcalibur1") with { PitchVariance = 0.3f}, Projectile.position);
+                for (int i = 0; i < 16; i++)
+                {
+                    Vector2 speed = Main.rand.NextVector2CircularEdge(4f, 4f);
+                    var d = Dust.NewDustPerfect(Projectile.Center, DustID.GemTopaz, speed, Scale: 1.5f);
+                    d.noGravity = true;
+                }
+            }
+
+
+            Projectile.rotation +=  Projectile.velocity.Length() * MathF.Sign(Projectile.velocity.X) * 0.05f;
+            Projectile.velocity *= 1.01f;
+            Projectile.velocity.Y += 0.25f;
         }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.velocity = Projectile.velocity.RotatedBy(-0.145f * MathF.Sign(Projectile.velocity.X));
+
+            return false;
+        }
+
         public override bool PreDraw(ref Color lightColor)
         {
-            return base.PreDraw(ref lightColor);
+            float scale = Easing.InOutSine((float)Projectile.timeLeft / 30f);
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+            Vector2 origin = new Vector2(texture.Width, texture.Height) * 0.5f;
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            Vector2 position = Projectile.Center - Main.screenPosition;
+
+            var projectile = Projectile;
+            int projFrames = Main.projFrames[projectile.type];
+            int frameHeight = texture.Height / projFrames;
+            int startY = frameHeight * projectile.frame;
+
+            SpriteEffects direction = projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            Rectangle sourceRectangle = new Rectangle(0, startY, texture.Width, frameHeight);
+            Vector2 drawOrigin = sourceRectangle.Size() / 2f;
+            //drawOrigin.X = projectile.spriteDirection == 1 ? sourceRectangle.Width - offsetX : offsetX;
+            for (int k = 0; k < projectile.oldPos.Length; k++)
+            {
+                Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + Projectile.Size * 0.5F;// + new Vector2(0f, projectile.gfxOffY);
+                Color color = projectile.GetAlpha(Color.Lerp(Color.Brown, Color.Transparent, 1f / projectile.oldPos.Length * k) * (1f - 1f / projectile.oldPos.Length * k));
+                color *= 0.45f;
+                color.A = 0;
+                Main.spriteBatch.Draw(texture, drawPos, sourceRectangle, color, projectile.oldRot[k], drawOrigin, projectile.scale  *scale, direction, 0f);
+            }
+
+            spriteBatch.Draw(texture, position, null, lightColor, Projectile.rotation, origin, scale, SpriteEffects.None, 0);
+            return false;
+            //return base.PreDraw(ref lightColor);
         }
         public override void OnKill(int timeLeft)
         {
             base.OnKill(timeLeft);
+            for (int i = 0; i < 14; i++)
+            {
+                Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<TSmokeDust>(), (Vector2.One * Main.rand.Next(1, 5)).RotatedByRandom(19.0), 0, Color.DarkGoldenrod, 1f).noGravity = true;
+            }
         }
     }
 }
--- NPCs/Govheil/GovheilProtector.cs
+++ NPCs/Govheil/GovheilProtector.cs
@@ -15,6 +15,7 @@ namespace Stellamod.NPCs.Govheil
 {
     public class GovheilProtector : ModNPC
     {
+        private ref float Timer => ref NPC.ai[1];
         public int moveSpeed = 0;
         public int moveSpeedY = 0;
         public int counter;
@@ -35,7 +36,10 @@ namespace Stellamod.NPCs.Govheil
                 new FlavorTextBestiaryInfoElement(LangText.Bestiary(this, "The last protectors of the govheil, and they are pretty formidable..."))
             });
         }
-
+        public override bool CanHitPlayer(Player target, ref int cooldownSlot)
+        {
+            return false;
+        }
         public override void SetDefaults()
         {
             NPC.width = 98;
@@ -54,103 +58,90 @@ namespace Stellamod.NPCs.Govheil
         }
 
         int frame = 0;
-        int games = 0;
+
         public override void AI()
         {
-            games++;
-            if (games <= 760)
+            NPC.aiStyle = -1;
+            if (!NPC.HasValidTarget || Timer == 1)
+                NPC.TargetClosest();
+            if (counter == 0)
             {
-                NPC.velocity *= 0.99f;
-                NPC.aiStyle = 22;
-                AIType = NPCID.IchorSticker;
-
+                if (npcCounter >= 4)
+                {
+                    npcCounter = 0;
+                    NPC.ai[0] = 150;
+                }
             }
 
-            if (games >= 1280)
+            Timer++;
+            Player player = Main.player[NPC.target];
+            NPC.spriteDirection = player.Center.X < NPC.Center.X ? 1 : -1;
+            NPC.rotation = NPC.velocity.X * 0.1f;
+            if (NPC.Center.X >= player.Center.X && moveSpeed >= -60)
             {
-                games = 0;
+                moveSpeed--;
             }
 
-            if (games >= 1200)
+            if (NPC.Center.X <= player.Center.X && moveSpeed <= 60)
             {
+                moveSpeed++;
+            }
 
-                /*
-                float speedXB = NPC.velocity.X * Main.rand.NextFloat(-1f, 1f);
-                float speedY = NPC.velocity.Y * Main.rand.Next(0, 0) * 0.0f + Main.rand.Next(-4, 4) * 0f;
+
+
+            if (NPC.Center.Y >= player.Center.Y - NPC.ai[0] && moveSpeedY >= -50)
+            {
+                moveSpeedY--;
+                NPC.ai[0] = 150f;
+            }
+
+            if (NPC.Center.Y <= player.Center.Y - NPC.ai[0] && moveSpeedY <= 50)
+            {
+                moveSpeedY++;
+            }
+
+
+            if (Timer >= 110 && Timer < 140)
+            {
+                dash = true;
+                NPC.velocity *= 0.5f;
+            }
+
+            if (Timer == 140)
+            {
+                Vector2 direction = player.Center - NPC.Center;
+                direction = direction.SafeNormalize(Vector2.Zero);
+                direction.X *= 4f;
+                direction.Y *= 4f;
+                NPC.velocity = direction;
+            }
+            if (Timer >= 180)
+            {
+                float distance = 128;
+                float particleSpeed = 8;
+
+                Vector2 position = NPC.Center + Main.rand.NextVector2CircularEdge(distance, distance);
+                Vector2 speed = (NPC.Center - position).SafeNormalize(Vector2.Zero) * particleSpeed;
+                var d = Dust.NewDustPerfect(position, DustID.GemTopaz, speed, Scale: 1f);
+                d.noGravity = true;
+                NPC.velocity *= 0.98f;
+            }
+            else
+            {
+                NPC.velocity.X = moveSpeed * 0.07f;
+                NPC.velocity.Y = moveSpeedY * 0.07f;
+            }
+            if (Timer == 240)
+            {
                 if (StellaMultiplayer.IsHost)
                 {
-                    Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.position.X, NPC.position.Y, speedXB * 3, speedY,
-                        ProjectileID.GreekFire3, 12, 0f, Owner: Main.myPlayer);
+                    Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, (player.Center - NPC.Center).SafeNormalize(Vector2.Zero) * 7, 
+                        ModContent.ProjectileType<GovheilGear>(), 16, 1, Main.myPlayer);
                 }
-
-                */
-            }
-            if (games >= 761)
-            {
-                NPC.aiStyle = -1;
-
-
-                if (NPC.target < 0 || NPC.target == 255 || Main.player[NPC.target].dead || !Main.player[NPC.target].active)
-                {
-                    NPC.TargetClosest();
-                }
-                if (counter == 0)
-                {
-                    if (npcCounter >= 4)
-                    {
-                        npcCounter = 0;
-                        NPC.ai[0] = 150;
-                    }
-                }
-                counter++;
-                Player player = Main.player[NPC.target];
-                NPC.rotation = NPC.velocity.X * 0.1f;
-                if (NPC.Center.X >= player.Center.X && moveSpeed >= -60)
-                {
-                    moveSpeed--;
-                }
-
-                if (NPC.Center.X <= player.Center.X && moveSpeed <= 60)
-                {
-                    moveSpeed++;
-                }
-
-                NPC.velocity.X = moveSpeed * 0.07f;
-
-                if (NPC.Center.Y >= player.Center.Y - NPC.ai[0] && moveSpeedY >= -50)
-                {
-                    moveSpeedY--;
-                    NPC.ai[0] = 150f;
-                }
-
-                if (NPC.Center.Y <= player.Center.Y - NPC.ai[0] && moveSpeedY <= 50)
-                {
-                    moveSpeedY++;
-                }
-
-                NPC.velocity.Y = moveSpeedY * 0.07f;
-                if (counter >= 110 && counter < 140)
-                {
-                    dash = true;
-                    NPC.velocity *= 0.5f;
-                }
-
-                if (counter == 140)
-                {
-                    Vector2 direction = player.Center - NPC.Center;
-                    direction = direction.SafeNormalize(Vector2.Zero);
-                    direction.X *= 4f;
-                    direction.Y *= 4f;
-                    NPC.velocity = direction;
-                }
-                if (counter == 180)
-                {
-                    NPC.ai[0] += -25f;
-                    NPC.velocity = Vector2.Zero;
-                    counter = 0;
-                    dash = false;
-                }
-
+                NPC.ai[0] += -25f;
+                NPC.velocity = Vector2.Zero;
+                Timer = 0;
+                dash = false;
             }
         }
 
