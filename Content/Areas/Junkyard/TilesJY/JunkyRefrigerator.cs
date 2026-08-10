8d73380058a354f1838e94d8e8f444cdb6c4470a
Scolon <scoloned@gmail.com>
2026-08-10 00:46:58 -0400

    fridge

---
+++ Content/Areas/Junkyard/TilesJY/JunkyRefrigerator.cs
@@ -0,0 +1,13 @@
+﻿using Stellamod.Core.ZTileSystem;
+
+namespace Stellamod.Content.Areas.Junkyard.TilesJY;
+
+public class JunkyRefrigerator : ZTile
+{
+    public override void SetStaticDefaults()
+    {
+        base.SetStaticDefaults();
+        frameCount = 1;
+        drawOrigin = TileDrawOrigin.BottomUp;
+    }
+}
--- Content/Areas/Junkyard/TilesJY/JunkyTile.cs
+++ Content/Areas/Junkyard/TilesJY/JunkyTile.cs
@@ -1,6 +1,7 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static Stellamod.Tiles.SpecialDecorativeWall;
 
 namespace Stellamod.Content.Areas.Junkyard.TilesJY;
 
