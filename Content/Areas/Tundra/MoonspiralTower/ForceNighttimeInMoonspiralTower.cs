3c9faf74963e196dc509605a7f4794ca45532779
Scolon <scoloned@gmail.com>
2026-08-23 16:25:08 -0400

    feat: fast forward time to peak night in moon spiral tower

---
+++ Content/Areas/Tundra/MoonspiralTower/ForceNighttimeInMoonspiralTower.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Tundra.MoonspiralTower;
+
+public class ForceNighttimeInMoonspiralTower : ModSystem
+{
+    public override void PostUpdateTime()
+    {
+        base.PostUpdateTime();
+        bool fastForward = false;
+        foreach (var player in Main.ActivePlayers)
+        {
+            if (player.InModBiome<MoonspiralTowerBiome>())
+            {
+                fastForward = true;
+                break;
+            }
+        }
+        if (Main.dayTime && fastForward)
+        {
+            Main.time += 128;
+            return;
+        }
+
+
+        Main.time = (double)MathHelper.Lerp((float)Main.time, (float)Main.nightLength * 0.5f, 0.1f);
+    }
+}
