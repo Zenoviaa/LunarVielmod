ab197c7f114e5ac8866cd046480a14640c7ae423
Scolon <scoloned@gmail.com>
2026-08-23 15:42:51 -0400

    wrong folder

--- Content/Areas/Snow/AccsSN/NighttimeSigil.cs
+++ Content/Areas/Snow/AccsSN/NighttimeSigil.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Stellamod.Content.Areas.Abyss.AccAB;
+namespace Stellamod.Content.Areas.Snow.AccsSN;
 
 public class NighttimeSigilPlayer : ModPlayer
 {
