eeb92dea5097567f7a05d1a2f6fe0584a9dfbcd0
Scolon <scoloned@gmail.com>
2026-09-18 19:03:30 -0400

    feat: add royal tile

---
+++ Content/Areas/Tundra/Abyss/TilesAB/RoyalTile.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Stellamod.Content.Areas.Tundra.Abyss.TilesAB;
+
+public class RoyalTileBlock : ModItem
+{
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+        Item.DefaultToPlaceableTile(ModContent.TileType<RoyalTile>());
+    }
+}
+
+public class RoyalTile : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        base.SetStaticDefaults();
+        Main.tileSolid[Type] = true;
+        Main.tileBlockLight[Type] = true;
+        Main.tileLargeFrames[Type] = 2;
+        TileID.Sets.ChecksForMerge[Type] = true;
+
+        MineResist = 2f;
+        MinPick = 225;
+        RegisterItemDrop(ModContent.ItemType<RoyalTileBlock>());
+        AddMapEntry(new Color(175, 175, 40));
+    }
+}
