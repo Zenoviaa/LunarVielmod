ff415c1290e3dcbc5d8edb73670f6e2a62640829
Scolon <scoloned@gmail.com>
2026-09-19 21:08:37 -0400

    ga

--- Common/ConsoleMenu/WorldGenCommand.cs
+++ Common/ConsoleMenu/WorldGenCommand.cs
@@ -16,7 +16,8 @@ public class WorldGenCommand : ConsoleCommand
         Arguments arguments0 = new Arguments();
         arguments0.potentialArguments = new()
         {
-            "abyss"
+            "abyss",
+            "square"
         };
 
         return arguments0;
@@ -31,6 +32,21 @@ public class WorldGenCommand : ConsoleCommand
             case "abyss":
                 VeilGen.GenerateAbyss();
                 Main.LocalPlayer.GetModPlayer<BiomePlayer>().justEnteredAbyss = true;
+                return true;
+            case "square":
+                Point center = Main.LocalPlayer.Center.ToTileCoordinates();
+                int width = 170;
+                int height = 300;
+                Rectangle rect = new Rectangle(center.X - width / 2, center.Y - height / 2, width, height);
+                for(int x = rect.Left; x < rect.Right; x++)
+                {
+                    for(int y = rect.Top; y < rect.Bottom; y++)
+                    {
+                        Tile tile = Main.tile[x, y];
+                        tile.ClearEverything();
+                    }
+                }
+                TileUtilities.UpdateMap(rect, 255);
                 return true;
         }
 
